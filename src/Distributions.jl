49efdcfa244d2821ce1cfeaad8bea2a057ba345a
Dan Merl <dmerl@fb.com>
2013-03-20 13:31:40 -0700

    Fixing unit tests so they pass, and replacing some matrix inversions with \ operators

--- src/Distributions.jl
+++ src/Distributions.jl
@@ -1187,7 +1187,7 @@ function logpdf(W::Wishart, X::Matrix{Float64})
     p = size(X,1)
     logd::Float64 = - (W.nu * p / 2. * log(2) + W.nu / 2. * log(det(W.Schol)) + lpgamma(p, W.nu/2.))
     logd += 0.5 * (W.nu - p - 1.) * logdet(X)
-    logd -= 0.5 * trace(inv(W.Schol)*X)
+    logd -= 0.5 * trace(W.Schol \ X) #logd -= 0.5 * trace(inv(W.Schol)*X)
     return logd
   end
 end
@@ -1260,14 +1260,21 @@ function logpdf(IW::InverseWishart, X::Matrix{Float64})
     p = size(X,1)
     logd::Float64 = - ( IW.nu * p / 2. * log(2) + lpgamma(p, IW.nu / 2.) - IW.nu / 2. * log(det(IW.Psichol)))
     logd -= 0.5 * (IW.nu + p + 1) * logdet(X)
-    logd -= 0.5 * trace(IW.Psichol[:U]' * IW.Psichol[:U] * inv(X))
+##    logd -= 0.5 * trace(IW.Psichol[:U]' * IW.Psichol[:U] * inv(X))
+    logd -= 0.5 * trace(X \ (IW.Psichol[:U]' * IW.Psichol[:U]))
     return logd
   end
 end
 
 ## because X==X' keeps failing due to floating point nonsense
 function isApproxSymmmetric(a::Matrix{Float64})
-  return max(abs(a - a')) < 1e-8
+  tmp = true
+  for j in 2:size(a,1)
+    for i in 1:j-1
+      tmp &= abs(a[i,j] - a[j,i]) < 1e-8
+    end
+  end
+  return tmp
 end
 
 ## because isposdef keeps giving the wrong answer for samples from Wishart and InverseWisharts
