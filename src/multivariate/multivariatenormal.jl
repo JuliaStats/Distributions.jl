62aff6974ad55139d8e8ea3a2377c6acb5110757
Dahua Lin <lindahua@gmail.com>
2013-07-16 17:28:40 -0500

    add types and stats functions for multivariate normal

--- src/multivariate/multivariatenormal.jl
+++ src/multivariate/multivariatenormal.jl
@@ -1,1 +1,41 @@
+# Multivariate Normal distribution
+
+immutable MultivariateNormal{Cov<:AbstractPDMat} <: ContinuousMultivariateDistribution
+    dim::Int
+    μ::Vector{Float64}
+    Σ::Cov
+end
+
+function MultivariateNormal{Cov<:AbstractPDMat}(μ::Vector{Float64}, Σ::Cov)
+    d = length(μ)
+    if dim(Σ) != (d, d)
+        throw(ArgumentError("The dimensions of μ and Σ are inconsistent."))
+    end
+    MultivariateNormal{Cov}(d, μ, Σ)
+end
+
+function MultivariateNormal{Cov<:AbstractPDMat}(Σ::Cov)
+    d = dim(Σ)
+    MultivariateNormal{Cov}(d, zeros(d), Σ)    
+end
+
+const MvNormal = MultivariateNormal
+
+# Basic statistics
+
+dim(d::MvNormal) = d.dim
+
+mean(d::MvNormal) = d.μ
+
+var(d::MvNormal) = diag(d.Σ)
+
+cov(d::MvNormal) = full(d.Σ)
+
+logdet_cov(d::MvNormal) = logdet(d.Σ)
+
+mode(d::MvNormal) = d.μ
+
+modes(d::MvNormal) = [mode(d)]
+
+entropy(d::MvNormal) = 0.5 * ()
 
