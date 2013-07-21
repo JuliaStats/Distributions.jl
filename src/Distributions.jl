d3b513da2dfc00d126b034293cc873f3769f8ea3
Dahua Lin <lindahua@gmail.com>
2013-07-20 20:21:54 -0500

    export SufficientStats

--- src/Distributions.jl
+++ src/Distributions.jl
@@ -19,6 +19,7 @@ export                                  # types
     ContinuousUnivariateDistribution,
     ContinuousMultivariateDistribution,
     ContinuousMatrixDistribution,
+    SufficientStats,
     Arcsine,
     Bernoulli,
     Beta,
