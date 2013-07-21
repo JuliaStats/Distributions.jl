53dc3844f03036db8cc9c272e6adbaad45489e46
Dahua Lin <lindahua@gmail.com>
2013-07-20 21:01:18 -0500

    new MLE for Normal

--- test/fit.jl
+++ test/fit.jl
@@ -9,8 +9,8 @@ d = fit(Bernoulli, rand(Bernoulli(0.7), N))
 
 d = fit(Beta, rand(Beta(1.3, 3.7), N))
 @test isa(d, Beta)
-@test_approx_eq_eps d.alpha 1.3 0.01
-@test_approx_eq_eps d.beta  3.7 0.01
+@test_approx_eq_eps d.alpha 1.3 0.1
+@test_approx_eq_eps d.beta  3.7 0.1
 
 d = fit(Binomial, 100, rand(Binomial(100, 0.3), N))
 @test isa(d, Binomial)
@@ -20,6 +20,18 @@ d = fit(Binomial, 100, rand(Binomial(100, 0.3), N))
 d = fit(Exponential, rand(Exponential(0.5), N))
 @test isa(d, Exponential)
 @test_approx_eq_eps mean(d) 0.5 0.01
+
+x = rand(Normal(11.3, 5.7), N)
+d = fit(Normal, x)
+@test isa(d, Normal)
+@test_approx_eq_eps mean(d) 11.3 0.1
+@test_approx_eq_eps std(d) 5.7 0.1
+
+d = fit_mle(Normal, suffstats(Normal, x))
+@test isa(d, Normal)
+@test_approx_eq_eps mean(d) 11.3 0.1
+@test_approx_eq_eps std(d) 5.7 0.1
+
 
 # fit(DiscreteUniform, rand(DiscreteUniform(300_000, 700_000), N))
 
@@ -32,7 +44,7 @@ d = fit(Exponential, rand(Exponential(0.5), N))
 
 # fit(Laplace, rand(Laplace(10.0, 3.0), N))
 
-# fit(Normal, rand(Normal(11.3, 5.7), N))
+# 
 
 # fit(Poisson, rand(Poisson(19.0), N))
 
