3660dd32fd9dfb36e5f5d688699175476c426fbe
Simon Byrne <simonbyrne@gmail.com>
2013-07-26 17:11:53 +0100

    use exact distns for edgeworth tests

--- test/edgeworth.jl
+++ test/edgeworth.jl
@@ -3,16 +3,33 @@
 using Distributions
 using Base.Test
 
-d = EdgeworthSum(Gamma(1,1),10)
-X = vec(sum(rand(d.dist,1_000_000,int(d.n)),2))
-de = EmpiricalUnivariateDistribution(X)
+dg = Gamma(1,1)
+
+d_s = EdgeworthSum(dg,10)
+d_m = EdgeworthMean(dg,10)
+d_z = EdgeworthZ(dg,10)
+
+dg_s = Gamma(10,1)
+dg_m = Gamma(10,0.1)
 
 for i = 0.01:0.01:0.99
-    @test_approx_eq quantile(d,i) cquantile(d,1-i) 
-    @test_approx_eq_eps cdf(d,quantile(d,i))/i 1.0 0.2
-    @test_approx_eq_eps ccdf(d,cquantile(d,i))/i 1.0 0.2
+    @test_approx_eq quantile(d_s,i) cquantile(d_s,1-i) 
+    @test_approx_eq_eps cdf(d_s,quantile(d_s,i))/i 1.0 0.2
+    @test_approx_eq_eps ccdf(d_s,cquantile(d_s,i))/i 1.0 0.2
 
-    @test_approx_eq_eps quantile(d,i)/quantile(de,i) 1.0 0.02
+    @test_approx_eq_eps quantile(d_s,i) quantile(dg_s,i) 0.02
+
+    @test_approx_eq quantile(d_m,i) cquantile(d_m,1-i) 
+    @test_approx_eq_eps cdf(d_m,quantile(d_m,i))/i 1.0 0.2
+    @test_approx_eq_eps ccdf(d_m,cquantile(d_m,i))/i 1.0 0.2
+
+    @test_approx_eq_eps quantile(d_m,i) quantile(dg_m,i) 0.002
+
+    @test_approx_eq quantile(d_z,i) cquantile(d_z,1-i) 
+    @test_approx_eq_eps cdf(d_z,quantile(d_z,i))/i 1.0 0.2
+    @test_approx_eq_eps ccdf(d_z,cquantile(d_z,i))/i 1.0 0.2
+
+    @test_approx_eq_eps quantile(d_z,i) (quantile(dg_s,i)-mean(dg_s))/std(dg_s) 0.007
 end
 
 
