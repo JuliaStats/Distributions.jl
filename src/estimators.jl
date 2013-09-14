837c1b3f5275866ee638fdb8ffbcbe9d8374c61f
Dahua Lin <lindahua@gmail.com>
2013-09-14 10:12:51 -0500

    add Estimator type and interface
    This is a nonintrusive modification. By delegating estimate(e::MLEstimator, x) to fit_mle, we no longer need to make any change to original codes.

---
+++ src/estimators.jl
@@ -0,0 +1,19 @@
+# uniform interface for model estimation
+
+export Estimator, MLEstimator
+export nsamples, estimate, prior_score
+
+abstract Estimator{D<:Distribution}
+
+nsamples{D<:UnivariateDistribution}(e::Estimator{D}, x::Array) = length(x)
+nsamples{D<:MultivariateDistribution}(e::Estimator{D}, x::Matrix) = size(x, 2)
+
+type MLEstimator{D<:Distribution} <: Estimator{D} end
+
+estimate{D<:Distribution}(e::MLEstimator{D}, x) = fit_mle(D, x)
+estimate{D<:Distribution}(e::MLEstimator{D}, x, w) = fit_mle(D, x, w)
+
+prior_score{D<:Distribution}(e::MLEstimator{D}, d::D) = 0.
+
+# TODO: add MAPEstimator
+
