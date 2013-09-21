674b5434374dc1b02c55192ecd631df0f2e3ea90
Dahua Lin <lindahua@gmail.com>
2013-09-21 06:54:04 -0500

    new conjugate API for beta-bernoulli/binomial

--- src/conjugates/beta_binom.jl
+++ src/conjugates/beta_binom.jl
@@ -4,16 +4,28 @@
 #   Beta - Binomial
 #
 
-posterior(prior::Beta, ss::BernoulliStats) = Beta(prior.alpha + ss.cnt1, prior.beta + ss.cnt0)
+posterior_canon(pri::Beta, ss::BernoulliStats) = Beta(pri.alpha + ss.cnt1, pri.beta + ss.cnt0)
+posterior_canon(pri::Beta, ss::BinomialStats) = Beta(pri.alpha + ss.ns, pri.beta + (ss.ne * ss.n - ss.ns))
 
-posterior(prior::Beta, ss::BinomialStats) = Beta(prior.alpha + ss.ns, prior.beta + (ss.ne * ss.n - ss.ns))
+complete(G::Type{Bernoulli}, pri::Beta, p::Float64) = Bernoulli(p)
 
-function posterior{T<:Real}(prior::Beta, ::Type{Binomial}, n::Integer, x::Array{T})
-	posterior(prior, suffstats(Binomial, n, x))
+# specialized fit_map and posterior_randmodel methods for Binomial 
+#
+# n is needed to create a Binomial distribution (which can not be provided through complete)
+#
+
+fit_map(pri::Beta, ss::BinomialStats) = Binomial(ss.n, posterior_mode(pri, ss))
+fit_map(pri::Beta, G::Type{Binomial}, data::(Int, Array)) = fit_map(pri, suffstats(G, data))
+fit_map(pri::Beta, G::Type{Binomial}, data::(Int, Array), w::Array) = fit_map(pri, suffstats(G, data, w))
+
+posterior_randmodel(pri::Beta, ss::BinomialStats) = Binomial(ss.n, posterior_rand(pri, ss))
+
+function posterior_randmodel(pri::Beta, G::Type{Binomial}, data::(Int, Array))
+	posterior_randmodel(pri, suffstats(G, data))
 end
 
-function posterior{T<:Real}(prior::Beta, ::Type{Binomial}, n::Integer, x::Array{T}, w::Array{Float64})
-	posterior(prior, suffstats(Binomial, n, x, w))
+function posterior_randmodel(pri::Beta, G::Type{Binomial}, data::(Int, Array), w::Array)
+	posterior_randmodel(pri, suffstats(G, data, w))
 end
 
 
