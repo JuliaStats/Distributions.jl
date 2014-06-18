e2eca04303dc4f9a1fe13a92da11eb5c05e9b51a
Simon Byrne <simonbyrne@gmail.com>
2014-06-18 16:25:23 +0100

    improve Laplace rand, rearrange methods

--- src/specialfuns.jl
+++ src/specialfuns.jl
@@ -13,6 +13,8 @@ log1pexp(x::Real) = x > zero(x) ? x + log1p(exp(-x)) : log1p(exp(x))
 log1pexp(x::Float64) = x <= 18.0 ? log1p(exp(x)) : x <= 33.3 ? x + exp(-x) : x
 log1pexp(x::Float32) = x <= 9f0 ? log1p(exp(x)) : x <= 16f0 ? x + exp(-x) : x
 log1pexp(x::Integer) = log1pexp(float(x))
+
+log2mexp(x::Real) = log1p(-expm1(x))
 # log(exp(x)-1)
 logexpm1(x::Real) = x + log1mexp(-x)
 logexpm1(x::Float64) = x <= 18.0 ? log(expm1(x)) : x <= 33.3 ? x - exp(-x) : x
