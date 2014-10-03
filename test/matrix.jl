5756640820257ccfb45ea1c2a4cdd0c4ab5c515c
Simon Byrne <simonbyrne@gmail.com>
2014-10-03 14:11:49 +0100

    Fix #287

--- test/matrix.jl
+++ test/matrix.jl
@@ -1,20 +1,22 @@
 using Distributions
 using Base.Test
 
-v = 3.0
+v = 7.0
 S = eye(2)
 S[1, 2] = S[2, 1] = 0.5
 
 W = Wishart(v,S)
-IW = InverseWishart(v+1,S)
+IW = InverseWishart(v,S)
 
 for d in [W,IW]
     @test size(d) == size(rand(d))
     @test length(d) == length(rand(d))
 end
 
-@test_approx_eq_eps mean(rand(W,100000)) mean(W) 0.3
-@test_approx_eq_eps mean(rand(IW,100000)) mean(IW) 0.3
+@test_approx_eq_eps mean(rand(W,100000)) mean(W) 0.1
+@test_approx_eq_eps mean(rand(IW,100000)) mean(IW) 0.1
+
+v = 3.0
 
 @test_approx_eq_eps pdf(Wishart(v,S), S) 0.04507168 1e-8
 @test_approx_eq_eps pdf(Wishart(v,S), inv(S)) 0.01327698 1e-8
