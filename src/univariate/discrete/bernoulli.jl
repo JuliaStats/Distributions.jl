8184f0dee4bd432583624b4376c64c1b1d10b93e
Dahua Lin <lindahua@gmail.com>
2014-11-16 22:11:46 +0800

    restructure the file bernoulli.jl

--- src/univariate/discrete/bernoulli.jl
+++ src/univariate/discrete/bernoulli.jl
@@ -1,3 +1,8 @@
+# Bernoulli distribution
+
+
+#### Type and Constructor
+
 immutable Bernoulli <: DiscreteUnivariateDistribution
     p0::Float64
     p1::Float64
@@ -11,44 +16,7 @@ end
 Bernoulli() = Bernoulli(0.5)
 
 
-function entropy(d::Bernoulli) 
-    p0 = d.p0
-    p1 = d.p1
-    p0 == 0. || p0 == 1. ? 0. : -(p0 * log(p0) + p1 * log(p1))
-end
-
-mean(d::Bernoulli) = d.p1
-
-var(d::Bernoulli) = d.p0 * d.p1
-
-skewness(d::Bernoulli) = (d.p0 - d.p1) / sqrt(d.p0 * d.p1)
-
-kurtosis(d::Bernoulli) = 1.0 / (d.p0 * d.p1) - 6.0
-
-median(d::Bernoulli) = d.p1 < 0.5 ? 0.0 : 1.0
-
-mgf(d::Bernoulli, t::Real) = d.p0 + d.p1 * exp(t)
-
-cf(d::Bernoulli, t::Real) = d.p0 + d.p1 * exp(im * t)
-
-mode(d::Bernoulli) = d.p1 > 0.5 ? 1 : 0
-
-function modes(d::Bernoulli)
-    d.p1 < 0.5 ? [0] : 
-    d.p1 > 0.5 ? [1] : [0, 1]
-end
-
-pdf(d::Bernoulli, x::Real) = x == zero(x) ? d.p0 : x == one(x) ? d.p1 : 0.0
-
-pdf(d::Bernoulli) = [d.p0, d.p1]
-
-cdf(d::Bernoulli, q::Real) = q >= zero(q) ? (q >= one(q) ? 1.0 : d.p0) : 0.
-
-quantile(d::Bernoulli, p::Real) = zero(p) <= p <= one(p) ? (p <= d.p0 ? 0 : 1) : NaN
-
-rand(d::Bernoulli) = rand() > d.p1 ? 0 : 1
-
-### handling support
+#### Suppport
 
 isupperbounded(::Union(Bernoulli, Type{Bernoulli})) = true
 islowerbounded(::Union(Bernoulli, Type{Bernoulli})) = true
@@ -61,7 +29,49 @@ support(::Union(Bernoulli, Type{Bernoulli})) = 0:1
 insupport(::Union(Bernoulli, Type{Bernoulli}), x::Real) = (x == 0 || x == 1)
 
 
-## MLE fitting
+#### Properties
+
+mean(d::Bernoulli) = d.p1
+var(d::Bernoulli) = d.p0 * d.p1
+skewness(d::Bernoulli) = (d.p0 - d.p1) / sqrt(d.p0 * d.p1)
+kurtosis(d::Bernoulli) = 1.0 / (d.p0 * d.p1) - 6.0
+
+
+mode(d::Bernoulli) = ifelse(d.p1 > 0.5, 1, 0)
+
+modes(d::Bernoulli) = d.p1 < 0.5 ? [0] :
+                      d.p1 > 0.5 ? [1] : [0, 1]
+
+median(d::Bernoulli) = ifelse(d.p1 > 0.5, 1.0, 0.0)
+
+function entropy(d::Bernoulli) 
+    p0 = d.p0
+    p1 = d.p1
+    p0 == 0. || p0 == 1. ? 0. : -(p0 * log(p0) + p1 * log(p1))
+end
+
+
+#### Evaluation
+
+pdf(d::Bernoulli, x::Real) = x == zero(x) ? d.p0 : x == one(x) ? d.p1 : 0.0
+
+pdf(d::Bernoulli) = [d.p0, d.p1]
+
+cdf(d::Bernoulli, q::Real) = q >= zero(q) ? (q >= one(q) ? 1.0 : d.p0) : 0.
+
+quantile(d::Bernoulli, p::Real) = zero(p) <= p <= one(p) ? (p <= d.p0 ? 0 : 1) : NaN
+
+mgf(d::Bernoulli, t::Real) = d.p0 + d.p1 * exp(t)
+
+cf(d::Bernoulli, t::Real) = d.p0 + d.p1 * exp(im * t)
+
+
+#### Sampling
+
+rand(d::Bernoulli) = rand() > d.p1 ? 0 : 1
+
+
+#### MLE fitting
 
 immutable BernoulliStats <: SufficientStats
     cnt0::Float64
