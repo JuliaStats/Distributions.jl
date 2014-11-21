e00bd14a7714e1f5c468002fab33790923c97f11
Dahua Lin <lindahua@gmail.com>
2014-11-21 17:03:55 +0800

    new testing framework with tests on discrete univariates

--- test/discrete.jl
+++ test/discrete.jl
@@ -5,7 +5,7 @@ import JSON
 using Base.Test
 
 
-function verify_and_test_discretes(jsonfile)
+function verify_and_test_discretes(jsonfile, n_tsamples::Int)
     R = JSON.parsefile(jsonfile)
     for (ex, dct) in R
         println("    testing $(ex)")
@@ -17,27 +17,33 @@ function verify_and_test_discretes(jsonfile)
         @test isa(d, dtype)
 
         # verification and testing
-        verify_and_test(d, dct)
+        verify_and_test(d, dct, n_tsamples)
     end
 end
 
-function verify_and_test(d::DiscreteUnivariateDistribution, dct::Dict)
+function verify_and_test(d::DiscreteUnivariateDistribution, dct::Dict, n_tsamples::Int)
     # verify parameters
     pdct = dct["params"]
-    for fname, val in pdct
+    for (fname, val) in pdct
         f = eval(symbol(fname))
         @assert isa(f, Function)
         Base.Test.test_approx_eq(f(d), val, "$fname(d)", "val")
+    end
 
     # verify stats
-    @test_approx_eq minimum(d) dct["minimum"]
-    @test_approx_eq maximum(d) dct["maximum"]
+    r_min = dct["minimum"]
+    r_max = dct["maximum"]
+    r_min = r_min == "-inf" ? -Inf : r_min
+    r_max = r_max == "inf" ? Inf : r_max
+
+    @test_approx_eq minimum(d) r_min
+    @test_approx_eq maximum(d) r_max
     @test_approx_eq mean(d) dct["mean"]
     @test_approx_eq var(d) dct["var"]
-    @test_approx_eq median(d) dct["median"]
+    @test_approx_eq_eps median(d) dct["median"] 1.0
 
     if applicable(entropy, d)
-        @test_approx_eq entropy(d) dct["entropy"]
+        @test_approx_eq_eps entropy(d) dct["entropy"] 1.0e-7
     end
 
     # verify quantiles
@@ -62,155 +68,8 @@ function verify_and_test(d::DiscreteUnivariateDistribution, dct::Dict)
 end
 
 
+## main
 
-### load reference data
-#
-#   Note
-#   -------
-#   To generate the reference data:
-#   (1) make sure that python, numpy, and scipy are installed in your system
-#   (2) enter the sub-directory test
-#   (3) run: python discrete_ref.py > discrete_ref.csv
-#  
-#   For most cases, you don't have. You only need to run this when you
-#   implement a new distribution and want to add new test cases, then
-#   you should add the new test cases to discrete_ref.py and run this
-#   procedure to update the reference data.
-#
-
-immutable DiscreteRefEntry
-    distr::DiscreteUnivariateDistribution
-    mean::Float64
-    var::Float64
-    entropy::Float64
-    x25::Int
-    x50::Int
-    x75::Int
-    lp25::Float64
-    lp50::Float64
-    lp75::Float64
-end
-
-function DiscreteRefEntry(row::Vector)
-    @assert length(row) == 10
-    d = eval(parse(row[1]))
-    return DiscreteRefEntry(d, row[2:10]...)
-end
-
-csvpath = joinpath(dirname(@__FILE__), "discrete_ref.csv")
-table = readcsv(csvpath)
-
-R = [DiscreteRefEntry(vec(table[i,:])) for i = 2:size(table,1)]
-
-
-### check with references
-
-function verify(e::DiscreteRefEntry)
-    d = e.distr
-    @test_approx_eq_eps mean(d)    e.mean    1.0e-12
-    @test_approx_eq_eps var(d)     e.var     1.0e-12
-
-    if applicable(entropy, d)
-        @test_approx_eq_eps entropy(d) e.entropy 1.0e-6 * (abs(e.entropy) + 1.0)
-    end
-
-    @test int(quantile(d, 0.25)) == e.x25
-    @test int(quantile(d, 0.50)) == e.x50
-    @test int(quantile(d, 0.75)) == e.x75
-
-    @test_approx_eq_eps logpdf(d, e.x25) e.lp25 1.0e-12
-    @test_approx_eq_eps logpdf(d, e.x50) e.lp50 1.0e-12
-    @test_approx_eq_eps logpdf(d, e.x75) e.lp75 1.0e-12
-end
-
-n_tsamples = 10^6
-
-pdtitle(d::DiscreteUnivariateDistribution) = println("    testing $d")
-
-### Bernoulli
-
-for (d, p) in [ (Bernoulli(), 0.5), 
-                (Bernoulli(0.25), 0.25), 
-                (Bernoulli(0.75), 0.75),
-                (Bernoulli(0.00), 0.00),
-                (Bernoulli(1.00), 1.00) ]
-
-    pdtitle(d)
-
-    @test isa(d, Bernoulli)
-    @test succprob(d) == p
-    @test failprob(d) == 1.0 - p
-    @test minimum(d) == 0
-    @test maximum(d) == 1
-    @test mode(d) == ifelse(p <= 0.5, 0, 1)
-    @test mean(d) == p
-    @test var(d) == p * (1.0 - p)
-    @test median(d) == (p < 0.5 ? 0 : p > 0.5 ? 1 : 0.5)
-
-    if 0.0 < p < 1.0
-        @test_approx_eq entropy(d) -(p * log(p) + (1-p) * log(1-p))
-    else
-        @test entropy(d) == 0.0
-    end
-
-    test_distr(d, n_tsamples)
-end
-
-
-### Binomial
-
-for (d, n, p) in [ (Binomial(), 1, 0.5), 
-                   (Binomial(3), 3, 0.5),
-                   (Binomial(5, 0.4), 5, 0.4),
-                   (Binomial(6, 0.8), 6, 0.8),
-                   (Binomial(100, 0.1), 100, 0.1),
-                   (Binomial(100, 0.9), 100, 0.9),
-                   (Binomial(10, 0.0), 10, 0.0),
-                   (Binomial(10, 1.0), 10, 1.0) ]
-
-    pdtitle(d)
-
-    @test isa(d, Binomial)
-    @test succprob(d) == p
-    @test failprob(d) == 1.0 - p
-    @test minimum(d) == 0
-    @test maximum(d) == n
-    @test_approx_eq mean(d) n * p
-    @test_approx_eq var(d) n * p * (1.0 - p)
-
-    # TODO: current implementation has some problem when p = 1.0
-    if p < 1.0  
-        test_distr(d, n_tsamples)
-    end
-end
-
-
-### Categorical
-
-for distr in [
-    Categorical([0.1, 0.9]),
-    Categorical([0.5, 0.5]),
-    Categorical([0.9, 0.1]), 
-    Categorical([0.2, 0.5, 0.3])]
-
-    @test minimum(distr) == 1
-    @test maximum(distr) == ncategories(distr)
-    for i = 1:distr.K
-        @test pdf(distr, i) == distr.prob[i]
-    end
-
-    pdtitle(distr)
-    test_distr(distr, n_tsamples)
-end
-
-
-### Others
-
-for rentry in R
-    pdtitle(rentry.distr)
-    verify(rentry)
-    test_distr(rentry.distr, n_tsamples)
-end
-
-
+jsonfile = joinpath(dirname(@__FILE__), "discrete_test.json")
+verify_and_test_discretes(jsonfile, 10^6)
 
--- test/prepdref2.py
+++ test/prepdref2.py
@@ -2,7 +2,7 @@
 
 import re
 import numpy as np
-from numpy import sqrt, nan, inf
+from numpy import sqrt, nan, inf, ceil
 from scipy.stats import *
 import json
 
@@ -48,7 +48,16 @@ def dsamples(a, b):
 
 
 def get_dinfo(distr_name, args):
-	"""Make an python object that captures all relevant quantities"""
+	"""Make an python object that captures all relevant quantities
+
+		Returns a tuple in the form of (d, supp, xs, pdict), where:
+
+		- d: the scipy.stats distribution object
+		- supp: the support in the form of (minimum, maximum)
+		        Note that "inf" or "-inf" should be used for infinity values
+		- xs: a list of samples at which logpdf and cdf are to be tested
+		- pdict: a dictionary of distribution parameters to check
+	"""
 
 	if distr_name == "Bernoulli":
 		assert len(args) <= 1
@@ -69,12 +78,55 @@ def get_dinfo(distr_name, args):
 			"failprob" : 1.0 - p,
 			"ntrials" : n})
 
+	elif distr_name == "DiscreteUniform":
+		assert len(args) <= 2
+		if len(args) == 0:
+			a, b = 0, 1
+		elif len(args) == 1:
+			a, b = 0, args[0]
+		else:
+			a, b = args
+		d = randint(a, b+1)
+
+		return (d, (a, b), dsamples(a, b), {})
+
+	elif distr_name == "Geometric":
+		assert len(args) <= 1
+		p = args[0] if len(args) == 1 else 0.5
+		d = geom(p)
+
+		n = int(np.ceil(d.ppf(0.99)))
+		return (d, (0, "inf"), dsamples(1, n), {})
+
+	elif distr_name == "Hypergeometric":
+		assert len(args) == 3
+		ns, nf, n = args
+		d = hypergeom(ns + nf, ns, n)
+		a = max(n - nf, 0)
+		b = min(ns, n)
+		return (d, (a, b), dsamples(a, b), {})
+
+	elif distr_name == "NegativeBinomial":
+		assert len(args) <= 2
+		r = args[0] if len(args) >= 1 else 1
+		p = args[1] if len(args) >= 2 else 0.5
+		d = nbinom(r, p)
+		n = int(np.ceil(d.ppf(0.99)))
+		return (d, (0, "inf"), dsamples(0, n), {})
+
+	elif distr_name == "Poisson":
+		assert len(args) <= 1
+		lam = args[0] if len(args) == 1 else 1.0
+		d = poisson(lam)
+		n = int(np.ceil(d.ppf(0.99)))
+		return (d, (0, "inf"), dsamples(0, n), {})
+
 	else:
 		raise ValueError("Unrecognized distribution name: " + distr_name)
 
 
 
-def make_json(ex, c, distr_name, d, mm, xs, pdict):
+def make_json(ex, c, distr_name, args, d, mm, xs, pdict):
 	"""Make a json object by collecting all information"""
 
 	if c == "discrete":
@@ -99,14 +151,25 @@ def make_json(ex, c, distr_name, d, mm, xs, pdict):
 			"q90" : d.ppf(0.90)} 
 
 	if is_discrete:
-		lp = [{"x" : x, "logpdf" : d.logpmf(x), "cdf" : d.cdf(x)} for x in xs]
-
-		zip(xs, d.logpmf(xs))
+		pts = [{"x" : x, "logpdf" : d.logpmf(x), "cdf" : d.cdf(x)} for x in xs]
 	else:
-		lp = [{"x" : x, "logpdf" : d.logpdf(x), "cdf" : d.cdf(x)} for x in xs]
+		pts = [{"x" : x, "logpdf" : d.logpdf(x), "cdf" : d.cdf(x)} for x in xs]
 
-	jdict["points"] = lp
+	jdict["points"] = pts
 
+	# work around inconsistencies in scipy.stats
+	if distr_name == "Bernoulli":
+		if len(args) == 0 or args[0] == 0.5:
+			jdict["median"] = 0.5
+
+	elif distr_name == "Geometric":
+		for t in ["mean", "median", "q10", "q25", "q50", "q75", "q90"]:
+			jdict[t] -= 1
+
+		for pt in pts:
+			pt["x"] -= 1
+
+	# output
 	return [ex, jdict]
 
 
@@ -121,7 +184,9 @@ def do_main(c):
 	for (ex, dname, args) in entries:
 		print ex, "..."
 		d, mm, xs, pdict = get_dinfo(dname, args)
-		je = make_json(ex, c, dname, d, mm, xs, pdict)
+		je = make_json(ex, c, dname, args, d, mm, xs, pdict)
+
+		# add je to list
 		jall.append(je)
 
 	with open(dstfile, "wt") as fout:
