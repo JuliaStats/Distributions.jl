e5f473748a2fdecad16c4d437e0f76904bde5fd5
Dahua Lin <lindahua@gmail.com>
2014-11-22 19:57:24 +0800

    better implementation of generic cdc

--- src/univariates.jl
+++ src/univariates.jl
@@ -123,7 +123,15 @@ proper_kurtosis(d::Distribution) = kurtosis(d, false)
 ## pdf, cdf, and friends
 
 logpdf(d::UnivariateDistribution, x::Number) = log(pdf(d, x))
-cdf(d::DiscreteUnivariateDistribution, k::Real) = sum([pdf(d,i) for i in minimum(d):k])
+
+function cdf(d::DiscreteUnivariateDistribution, k::Real)
+    c = 0.0
+    for x = minimum(d):int(k)
+        c += pdf(d, x)
+    end 
+    return c
+end
+
 ccdf(d::UnivariateDistribution, q::Real) = 1.0 - cdf(d, q)
 cquantile(d::UnivariateDistribution, p::Real) = quantile(d, 1.0 - p)
 
