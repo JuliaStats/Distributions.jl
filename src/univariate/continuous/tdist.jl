a0229d3f94255dc308e85b1d29ae8a751c679ff6
Dahua Lin <lindahua@gmail.com>
2014-12-20 16:31:55 +0800

    Clean up TDist

--- src/univariate/continuous/tdist.jl
+++ src/univariate/continuous/tdist.jl
@@ -1,7 +1,8 @@
 immutable TDist <: ContinuousUnivariateDistribution
-    df::Float64 # non-integer degrees of freedom allowed
+    df::Float64
+
     function TDist(d::Real)
-    	d > zero(d) || error("df must be positive")
+    	d > zero(d) || error("TDist: df must be positive")
         new(float64(d))
     end
 end
@@ -10,29 +11,41 @@ end
 
 @distr_support TDist -Inf Inf
 
-function entropy(d::TDist)
-    hdf = 0.5*d.df
-    hdfph = hdf + 0.5
-    hdfph*(digamma(hdfph) - digamma(hdf)) +
-        0.5*log(d.df) + lbeta(hdf,0.5)
-end
+
+#### Parameters
+
+dof(d::TDist) = d.df
+params(d::TDist) = (d.df,)
+
+
+#### Statistics
 
 mean(d::TDist) = d.df > 1.0 ? 0.0 : NaN
-
 median(d::TDist) = 0.0
-
 mode(d::TDist) = 0.0
 
 function var(d::TDist)
-    d.df > 2.0 && return d.df / (d.df - 2.0)
-    d.df > 1.0 && return Inf
-    NaN
+    df = d.df
+    df > 2.0 ? df / (df - 2.0) :
+    df > 1.0 ? Inf : NaN
+end
+
+skewness(d::TDist) = d.df > 3.0 ? 0.0 : NaN
+
+function kurtosis(d::TDist)
+    df = d.df
+    df > 4.0 ? 6.0 / (df - 4.0) :
+    df > 2.0 ? Inf : NaN
+end
+
+function entropy(d::TDist)
+    hdf = 0.5 * d.df
+    hdfph = hdf + 0.5
+    hdfph * (digamma(hdfph) - digamma(hdf)) + 0.5 * log(d.df) + lbeta(hdf,0.5)
 end
 
 
-function pdf(d::TDist, x::Float64)
-    1.0 / (sqrt(d.df) * beta(0.5, 0.5 * d.df)) *
-        (1.0 + x^2 / d.df)^(-0.5 * (d.df + 1.0))
-end
+#### Evaluation
 
 gradlogpdf(d::TDist, x::Float64) = -((d.df + 1.0) * x) / (x^2 + d.df)
+
