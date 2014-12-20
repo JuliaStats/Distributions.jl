98220658147ffa8cbbdb6e3d9e172c3d3ddc72cc
Dahua Lin <lindahua@gmail.com>
2014-12-20 12:15:40 +0800

    Clean up Levy

--- src/univariate/continuous/levy.jl
+++ src/univariate/continuous/levy.jl
@@ -1,48 +1,74 @@
 immutable Levy <: ContinuousUnivariateDistribution
-    location::Float64
-    scale::Float64
-    function Levy(l::Real, s::Real)
-        s >= zero(s) || error("scale must be non-negative")
-        new(float64(l), float64(s))
-    end
-end
+    μ::Float64
+    c::Float64
 
-Levy(location::Real) = Levy(location, 1.0)
-Levy() = Levy(0.0, 1.0)
+    function Levy(μ::Real, c::Real)
+        c >= zero(c) || error("scale must be non-negative")
+        new(float64(μ), float64(c))
+    end
+
+    Levy(μ::Real) = new(μ, 1.0)
+    Levy() = new(0.0, 1.0)
+end
 
 @distr_support Levy d.location Inf
 
-function entropy(d::Levy)
-    c = d.scale
-    (1.0 - 3.0 * digamma(1.0) + log(16.0 * pi * c * c)) / 2.0
-end
+
+#### Parameters
+
+location(d::Levy) = d.μ
+params(d::Levy) = (d.μ, d.c)
+
+
+#### Statistics
 
 mean(d::Levy) = Inf
 var(d::Levy) = Inf
 skewness(d::Levy) = NaN
 kurtosis(d::Levy) = NaN
 
-mode(d::Levy) = d.scale / 3.0 + d.location
+mode(d::Levy) = d.c / 3.0 + d.μ
+
+function entropy(d::Levy)
+    c = scale(d)
+    (1.0 - 3.0 * digamma(1.0) + log(16.0 * pi * c * c)) / 2.0
+end
 
 function median(d::Levy)
-    m, c = d.location, d.scale
-    m + c / (2.0 * erfcinv(0.5)^2)
+    μ, c = params(d)
+    μ + c / (2.0 * erfcinv(0.5)^2)
 end
+
+
+#### Evaluation
 
 function pdf(d::Levy, x::Float64)
-    m, c = d.location, d.scale
-    (sqrt(c)/sqrt2π) * exp(-(c / (2.0 * (x - m)))) / (x - m)^1.5
+    μ, c = params(d)
+    z = x - μ
+    (sqrt(c) / sqrt2π) * exp((-c) / (2.0 * z)) / z^1.5
 end
 
-cdf(d::Levy, x::Float64) = erfc(sqrt(d.scale / (2.0 * (x - d.location))))
+function logpdf(d::Levy, x::Float64)
+    μ, c = params(d)
+    z = x - μ
+    0.5 * (log(c) - log2π - c / z - 3.0 * log(z))
+end
 
-quantile(d::Levy, p::Float64) = d.location + d.scale / (2.erfcinv(p)^2)
+cdf(d::Levy, x::Float64) = erfc(sqrt(d.c / (2.0 * (x - d.μ))))
+ccdf(d::Levy, x::Float64) = erf(sqrt(d.c / (2.0 * (x - d.μ))))
+
+quantile(d::Levy, p::Float64) = d.μ + d.c / (2.0 * erfcinv(p)^2)
+cquantile(d::Levy, p::Float64) = d.μ + d.c / (2.0 * erfinv(p)^2)
 
 mgf(d::Levy, t::Real) = t == zero(t) ? 1.0 : NaN
 
 function cf(d::Levy, t::Real)
-    m, c = d.location, d.scale
-    exp(im * m * t - sqrt(-2.0 * im * c * t))
+    μ, c = params(d)
+    exp(im * μ * t - sqrt(-2.0 * im * c * t))
 end
 
-rand(d::Levy) = d.location + 1 / rand(Normal(0.0, 1.0 / sqrt(d.scale)))^2
+
+#### Sampling
+
+rand(d::Levy) = d.μ + d.c / randn()^2
+
