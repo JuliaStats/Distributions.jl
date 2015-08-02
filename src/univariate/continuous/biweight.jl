d88ea438a0150d5a8d30007749f56c433741148f
Dahua Lin <lindahua@gmail.com>
2015-08-02 17:17:48 +0800

    Enforce consistent naming for Biweight & Cauchy

--- src/univariate/continuous/biweight.jl
+++ src/univariate/continuous/biweight.jl
@@ -1,53 +1,62 @@
 immutable Biweight <: ContinuousUnivariateDistribution
-    location::Float64
-    scale::Float64
-    function Biweight(l::Real, s::Real)
-        s > zero(s) || error("scale must be positive")
-        @compat new(Float64(l), Float64(s))
+    μ::Float64
+    σ::Float64
+
+    function Biweight(μ::Real, σ::Real)
+        σ > zero(σ) || throw(ArgumentError("scale must be positive"))
+        @compat new(Float64(μ), Float64(σ))
     end
+
+    Biweight(μ::Real) = new(μ, 1.0)
+    Biweight() = new(0.0, 1.0)
 end
 
-Biweight(location::Real) = Biweight(location, 1.0)
-Biweight() = Biweight(0.0, 1.0)
-
-@distr_support Biweight d.location-d.scale d.location+d.scale
+@distr_support Biweight d.μ - d.σ d.μ + d.σ
 
 ## Parameters
-params(d::Biweight) = (d.location, d.scale)
+params(d::Biweight) = (d.μ, d.σ)
 
 ## Properties
-mean(d::Biweight) = d.location
-median(d::Biweight) = d.location
-mode(d::Biweight) = d.location
+mean(d::Biweight) = d.μ
+median(d::Biweight) = d.μ
+mode(d::Biweight) = d.μ
 
-var(d::Biweight) = d.scale*d.scale/7.0
+var(d::Biweight) = d.σ^2 / 7.0
 skewness(d::Biweight) = 0.0
-kurtosis(d::Biweight) = 1/21-3
+kurtosis(d::Biweight) = -2.9523809523809526  # = 1/21-3
 
 ## Functions
-function pdf(d::Biweight, x::Real)
-    u = abs(x - d.location)/d.scale
-    u >= 1 ? 0.0 : 0.9375*(1-u*u)^2/d.scale
+function pdf(d::Biweight, x::Float64)
+    u = abs(x - d.μ) / d.σ
+    u >= 1.0 ? 0.0 : 0.9375 * (1 - u^2)^2 / d.σ
 end
 
-function cdf(d::Biweight, x::Real)
-    u = (x - d.location)/d.scale
-    u <= -1 ? 0.0 : u >= 1 ? 1.0 : 0.0625*(1+u)^3*@horner(u,8.0,-9.0,3.0)
+function cdf(d::Biweight, x::Float64)
+    u = (x - d.μ) / d.σ
+    u <= -1.0 ? 0.0 :
+    u >= 1.0 ? 1.0 :
+    0.0625 * (u + 1.0)^3 * @horner(u,8.0,-9.0,3.0)
 end
-function ccdf(d::Biweight, x::Real)
-    u = (d.location - x)/d.scale
-    u <= -1 ? 1.0 : u >= 1 ? 0.0 : 0.0625*(1+u)^3*@horner(u,8.0,-9.0,3.0)
+
+function ccdf(d::Biweight, x::Float64)
+    u = (d.μ - x) / d.σ
+    u <= -1.0 ? 1.0 :
+    u >= 1.0 ? 0.0 :
+    0.0625 * (u + 1.0)^3 * @horner(u,8.0,-9.0,3.0)
 end
 
 @quantile_newton Biweight
 
-function mgf(d::Biweight, t::Real)
-    a = d.scale*t
-    a2 = a*a
-    a == 0 ? one(a) : 15.0*exp(d.location*t)*(-3.0*cosh(a)+(a+3.0/a)*sinh(a))/(a2*a2)
+function mgf(d::Biweight, t::Float64)
+    a = d.σ*t
+    a2 = a^2
+    a == 0 ? 1.0 :
+    15.0 * exp(d.μ * t) * (-3.0 * cosh(a) + (a + 3.0/a) * sinh(a)) / (a2^2)
 end
-function cf(d::Biweight, t::Real)
-    a = d.scale*t
-    a2 = a*a
-    a == 0 ? complex(one(a)) : -15.0*cis(d.location*t)*(3.0*cos(a)+(a-3.0/a)*sin(a))/(a2*a2)
+
+function cf(d::Biweight, t::Float64)
+    a = d.σ * t
+    a2 = a^2
+    a == 0 ? 1.0+0.0im :
+    -15.0 * cis(d.μ * t) * (3.0 * cos(a) + (a - 3.0/a) * sin(a)) / (a2^2)
 end
