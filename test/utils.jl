064459794980e16468b47cadbeae88b29fbddec7
John Pearson <jmxpearson@gmail.com>
2016-06-01 13:09:27 -0400

    adding partype tests

--- test/utils.jl
+++ test/utils.jl
@@ -6,3 +6,6 @@ r = RealInterval(1.5, 4.0)
 @test minimum(r) == 1.5
 @test maximum(r) == 4.0
 
+@test partype(Gamma(1, 2)) == Int64
+@test partype(Gamma(1.1, 2)) == Float64
+@test partype(MvNormal(rand(Float16, 5), eye(Float32, 5))) == Float32
