c336bc92977a76bae283157a9a17e105fa32d434
Robert DJ <git@dahl-jacobsen.dk>
2016-08-15 14:22:48 +0200

    rand method for NIG

--- src/univariate/continuous/normalinversegaussian.jl
+++ src/univariate/continuous/normalinversegaussian.jl
@@ -56,3 +56,26 @@ function logpdf(d::NormalInverseGaussian, x::Real)
   μ, α, β, δ = params(d)
   log(α*δ) + log(besselk(1, α*sqrt(δ^2+(x-μ)^2))) - log(π*sqrt(δ^2+(x-μ)^2)) + δ*sqrt(α^2-β^2) + β*(x-μ)
 end
+
+
+#### Sampling
+
+# The Normal Inverse Gaussian distribution is a normal variance-mean
+# mixture with an inverse Gaussian as mixing distribution (albeit with a
+# different parametrization than the one in Distributions).
+#
+# Ole E. Barndorff-Nielsen (1997)
+# Normal Inverse Gaussian Distributions and Stochastic Volatility Modelling
+# Scandinavian Journal of Statistics, Vol. 24, pp. 1--13
+# DOI: http://dx.doi.org/10.1111/1467-9469.00045
+
+function Distributions.rand(d::NormalInverseGaussian)
+	μ, α, β, δ = params(d)
+	γ = sqrt(α^2 - β^2)
+
+	Z = InverseGaussian( δ/γ, δ^2 )
+	z = rand(Z)
+	X = Normal( μ + β*z, sqrt(z) )
+	rand(X)
+end
+
