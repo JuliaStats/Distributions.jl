fef46cdd18c1be98aeae49b9ac1c5d0b1cccfe82
Eric Ford <eford@psu.edu>
2016-09-09 13:44:52 -0400

    composite distribution

---
+++ test/composite.jl
@@ -0,0 +1,63 @@
+using Base.Test
+using Distributions
+
+include("../src/multivariate/composite.jl")
+using CompositeDistributions
+
+d1 = Rayleigh(0.3)
+d2 = MvNormal([log(0.1),log(0.01)],[0.1,log(0.01)])
+
+d = CompositeDist(ContinuousDistribution[d1, d2])
+
+# Test basic statistics
+m1 = mean(d1)
+m2 = mean(d2)
+m = mean(d)
+@test m1 == m[1]
+@test m2 == m[2:3]
+
+m1 = mode(d1)
+m2 = mode(d2)
+m = mode(d)
+@test m1 == m[1]
+@test m2 == m[2:3]
+
+v1 = var(d1)
+v2 = var(d2)
+v = var(d)
+@test v1 == v[1]
+@test v2 == v[2:3]
+
+cv2 = cov(d2)
+cv = cov(d)
+@test v1 == cv[1,1]
+@test all(zeros(2) .== cv[1,2:3])
+@test all(zeros(2) .== cv[2:3,1])
+@test all(cv2 .== cv[2:3,2:3])
+
+# Now sure how to test  these well
+r = rand(d)
+@test insupport(d,r)
+
+n = 10
+r10 = rand(d,n)
+@test all(insupport(d,r10))
+
+
+p1 = logpdf(d1,r[1])
+p2 = logpdf(d2,r[2:3])
+p = logpdf(d,r)
+@test_approx_eq(p1+p2,p) 
+
+p1 = pdf(d1,r[1])
+p2 = pdf(d2,r[2:3])
+p = pdf(d,r)
+@test_approx_eq(p1*p2,p) 
+
+out = zeros(n)
+logpdf!(out,d,r10)
+for i in 1:n
+  @test logpdf(d,r10[:,i]) == out[i]
+end
+
+
