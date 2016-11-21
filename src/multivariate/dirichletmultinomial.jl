095a850bffa77883fad7b4473f8eb873f5b62ee3
Josh Day <emailjoshday@gmail.com>
2016-11-21 14:44:24 -0500

    add sampling, start tests

--- src/multivariate/dirichletmultinomial.jl
+++ src/multivariate/dirichletmultinomial.jl
@@ -1,4 +1,4 @@
-immutable DirichletMultinomial{T <: Real} <: ContinuousMultivariateDistribution
+immutable DirichletMultinomial{T <: Real} <: DiscreteMultivariateDistribution
     n::Int
     α::Vector{T}
     α0::T
@@ -7,7 +7,7 @@ immutable DirichletMultinomial{T <: Real} <: ContinuousMultivariateDistribution
         α0 = sumabs(α)
         sum(α) == α0 || throw(ArgumentError("DirichletMultinomial: alpha must be a positive vector."))
         n > 0 || throw(ArgumentError("DirichletMultinomial: n must be a positive integer."))
-        new(round(Int, n), α, α0)
+        new(Int(n), α, α0)
     end
 end
 DirichletMultinomial{T <: Real}(n::Integer, α::Vector{T}) = DirichletMultinomial{T}(n, α)
@@ -15,6 +15,7 @@ DirichletMultinomial{T <: Integer}(n::Integer, α::Vector{T}) = DirichletMultino
 DirichletMultinomial(n::Integer, k::Integer) = DirichletMultinomial(n, ones(k))
 
 Base.show(io::IO, d::DirichletMultinomial) = show(io, d, (:n, :α,))
+
 
 # Parameters
 ncategories(d::DirichletMultinomial) = length(d.α)
@@ -34,6 +35,47 @@ function var{T <: Real}(d::DirichletMultinomial{T})
     end
     v
 end
-function cov(d::DirichletMultinomial)
-
+function cov{T <: Real}(d::DirichletMultinomial{T})
+    v = var(d)
+    c = d.α * d.α'
+    multiply!(c, -(d.n / d.α0 ^ 2) * ((d.n + d.α0) / (one(T) + d.α0)))
+    for i in 1:length(d)
+        @inbounds c[i, i] = v[i]
+    end
+    c
 end
+
+
+# Evaluation
+function insupport{T<:Real}(d::DirichletMultinomial, x::AbstractVector{T})
+    k = length(d)
+    length(x) == k || return false
+    for xi in x
+        (isinteger(xi) && xi >= 0) || return false
+    end
+    return sum(x) == ntrials(d)
+end
+function _logpdf{T<:Real}(d::DirichletMultinomial, x::AbstractVector{T})
+    c = log(factorial(d.n)) + lgamma(d.α0) - lgamma(d.n + d.α0)
+    for j in eachindex(x)
+        xj, αj = x[j], d.α[j]
+        c += lgamma(xj + αj) - log(factorial(xj)) - lgamma(αj)
+    end
+    c
+end
+
+
+# Sampling
+function _rand!{T<:Real}(d::DirichletMultinomial, x::AbstractVector{T})
+    multinom_rand!(ntrials(d), rand(Dirichlet(d.α)), x)
+end
+
+
+# Fit Model
+# TODO: use https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2945396/pdf/nihms205488.pdf
+# What gets returned if there are different number of trials in observations?
+#   - Not an option for Multinomial
+# immutable DirichletMultinomialStats <: SufficientStats
+#     r::Vector{Int}
+#     s::Matrix{Int}
+# end
--- test/dirichletmultinomial.jl
+++ test/dirichletmultinomial.jl
@@ -1,7 +1,46 @@
 # Tests for DirichletMultinomial
 
+
 using Distributions
 using Base.Test
 
-d1 = DirichletMultinomial(10, 5)
-d2 = DirichletMultinomial(10, rand(5))
+srand(123)
+
+α = rand(5)
+d = DirichletMultinomial(10, α)
+
+
+# test parameters
+@test length(d) == 5
+@test ntrials(d) == 10
+@test params(d) == (10, α)
+@test d.α == α
+@test d.α0 == sum(α)
+@test partype(d) == eltype(α)
+
+# test statistics
+@test_approx_eq(mean(d), α * (d.n / d.α0))
+p = d.α / d.α0
+@test_approx_eq(var(d), d.n * (d.n + d.α0) / (1 + d.α0) .* p .* (1.0 - p))
+
+# test Evaluation
+d = DirichletMultinomial(10, 5)
+@test !insupport(d, rand(5))
+@test insupport(d, [2, 2, 2, 2, 2])
+@test insupport(d, 2.0 * ones(5))
+@test !insupport(d, 3.0 * ones(5))
+
+for x in [2 * ones(5), [1, 2, 3, 4, 0], [3.0, 0.0, 3.0, 0.0, 4.0]]
+    @test_approx_eq(
+        pdf(d, x),
+        factorial(d.n) * gamma(d.α0) / gamma(d.n + d.α0) * prod(gamma(d.α + x) ./ factorial.(x) ./ gamma(d.α))
+    )
+    @test_approx_eq(
+        logpdf(d, x),
+        log(factorial(d.n)) + lgamma(d.α0) - lgamma(d.n + d.α0) + sum(lgamma(d.α + x) - log(factorial.(x)) - lgamma(d.α))
+    )
+end
+
+# test Sampling
+x = rand(d)
+@test sum(x) == d.n
