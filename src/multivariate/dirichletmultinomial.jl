ecc0cd2d578c8568f52b78e1d6c44d4da278028b
Josh Day <emailjoshday@gmail.com>
2016-11-22 09:51:39 -0500

    unweighted fit_mle up and running

--- src/multivariate/dirichletmultinomial.jl
+++ src/multivariate/dirichletmultinomial.jl
@@ -72,10 +72,48 @@ end
 
 
 # Fit Model
-# TODO: use https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2945396/pdf/nihms205488.pdf
-# What gets returned if there are different number of trials in observations?
-#   - Not an option for Multinomial
-# immutable DirichletMultinomialStats <: SufficientStats
-#     r::Vector{Int}
-#     s::Matrix{Int}
-# end
+# Using https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2945396/pdf/nihms205488.pdf
+immutable DirichletMultinomialStats <: SufficientStats
+    n::Int
+    s::Matrix{Float64}  # s_{jk} = ∑_i x_{ij} ≥ (k - 1),  k = 1,...,(n - 1)
+    tw::Float64
+    DirichletMultinomialStats(n::Int, s::Matrix{Float64}, tw::Real) = new(n, s, Float64(tw))
+end
+function suffstats{T<:Real}(::Type{DirichletMultinomial}, x::Matrix{T})
+    n = sum(x[:, 1])
+    all(sum(x, 1) .== n) || error("Each sample in X should sum to the same value.")
+    # s = [float(sum(x[i, :] .>= k)) for i in 1:size(x, 1), k in 1:n]
+    s = zeros(size(x, 1), n)
+    for k in 1:n
+        for i in 1:size(x, 2)
+            for j in 1:size(x, 1)
+                s[j, k] += Float64(x[j, i] >= k)
+            end
+        end
+    end
+    DirichletMultinomialStats(n, s, size(x, 2))
+end
+function fit_mle(::Type{DirichletMultinomial}, ss::DirichletMultinomialStats;
+        tol::Float64 = 1e-8, maxiter::Int = 1000)
+    k = size(ss.s, 2)
+    α = ones(size(ss.s, 1))
+    rng = 0.0:(k - 1)
+    tolerance = Inf
+    for iter in 1:maxiter
+        α_old = copy(α)
+        αsum = sum(α)
+        denom = ss.tw * sum(1 ./ (αsum + rng))
+        for j in eachindex(α)
+            αj = α[j]
+            num = αj * sum(ss.s[j, :] ./ (αj + rng))
+            α[j] = num / denom
+        end
+        tolerance = maxabs(α_old - α)
+        if tolerance < tol
+            println("done in $iter iterations")
+            break
+        end
+    end
+    println("tol: $tolerance")
+    DirichletMultinomial(ss.n, α)
+end
--- test/dirichletmultinomial.jl
+++ test/dirichletmultinomial.jl
@@ -25,22 +25,32 @@ p = d.α / d.α0
 
 # test Evaluation
 d = DirichletMultinomial(10, 5)
+@test typeof(d) == DirichletMultinomial{Float64}
 @test !insupport(d, rand(5))
 @test insupport(d, [2, 2, 2, 2, 2])
 @test insupport(d, 2.0 * ones(5))
 @test !insupport(d, 3.0 * ones(5))
 
-for x in (2 * ones(5), [1, 2, 3, 4, 0], [3.0, 0.0, 3.0, 0.0, 4.0])
+for x in (2 * ones(5), [1, 2, 3, 4, 0], [3.0, 0.0, 3.0, 0.0, 4.0], [0, 0, 0, 0, 10])
     @test_approx_eq(
         pdf(d, x),
         @compat factorial(d.n) * gamma(d.α0) / gamma(d.n + d.α0) * prod(gamma(d.α + x) ./ factorial.(x) ./ gamma(d.α))
     )
     @test_approx_eq(
         logpdf(d, x),
-        @compat log(factorial(d.n)) + lgamma(d.α0) - lgamma(d.n + d.α0) + sum(lgamma(d.α + x) - log(factorial.(x)) - lgamma(d.α))
+        @compat log(factorial(d.n)) + lgamma(d.α0) - lgamma(d.n + d.α0) + sum(lgamma(d.α + x) - log.(factorial.(x)) - lgamma(d.α))
     )
 end
 
 # test Sampling
 x = rand(d)
+@test isa(x, Vector{Int})
 @test sum(x) == d.n
+@test length(x) == length(d)
+@test insupport(d, x)
+
+x = rand(d, 50)
+@test size(x, 1) == length(d)
+@test size(x, 2) == 50
+@test all(sum(x, 1) .== ntrials(d))
+@test all(insupport(d, x))
