05a577bfa5c4f6fa1f9fb7d1af784f8a29dc7091
Simon Byrne <simonbyrne@gmail.com>
2017-08-17 09:42:02 -0700

    remove 0.5 deprecations

--- src/deprecates.jl
+++ src/deprecates.jl
@@ -1,21 +1,3 @@
-
-#### Deprecate on 0.5 (to be removed on 0.6)
-
-function dim(d::MultivariateDistribution)
-    Base.depwarn("dim(d::MultivariateDistribution) is deprecated. Please use length(d).", :dim)
-    return length(d)
-end
-
-function binaryentropy(d::UnivariateDistribution)
-    Base.depwarn("binaryentropy is deprecated. Please use entropy(d, 2).", :binaryentropy)
-    return entropy(d) / log(2)
-end
-
-@Base.deprecate logpmf logpdf
-@Base.deprecate logpmf! logpmf!
-@Base.deprecate pmf pdf
-
-
 #### Deprecate on 0.6 (to be removed on 0.7)
 
 @Base.deprecate expected_logdet meanlogdet
@@ -39,3 +21,5 @@ function BetaBinomial(n::Real, α::Real, β::Real)
     Base.depwarn("BetaBinomial(n::Real, α, β) is deprecated. Please use BetaBinomial(n::Integer, α, β) instead.", :BetaBinomial)
     BetaBinomial(Int(n), α, β)
 end
+
+
