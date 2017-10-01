2dd5de432150ed1871e4e8d267c6a31377b49d2b
Kenta Sato <bicycle1885@gmail.com>
2017-10-01 18:39:44 +0900

    more docs

--- src/univariate/continuous/semicircle.jl
+++ src/univariate/continuous/semicircle.jl
@@ -1,7 +1,22 @@
 """
     Semicircle(r)
 
-The Wigner semicircle distribution with radius `r`.
+The Wigner semicircle distribution with radius parameter `r` has probability
+density function
+
+```math
+f(x; r) = \\frac{2}{\\pi r^2} \\sqrt{r^2 - x^2}, \\quad x \\in [-r, r].
+```
+
+```julia
+Semicircle(r)   # Wigner semicircle distribution with radius r
+
+params(d)       # Get the radius parameter, i.e. (r,)
+```
+
+External links
+
+* [Wigner semicircle distribution on Wikipedia](https://en.wikipedia.org/wiki/Wigner_semicircle_distribution)
 """
 struct Semicircle{T<:Real} <: ContinuousUnivariateDistribution
     r::T
