2fc223f72a2df4934c47e5701b9afc8119e0448f
Ben Arthur <arthurb@hhmi.org>
2018-04-03 21:03:28 -0400

    Fix misuse of dot broadcasting and add QQ tests (#711)

--- src/qq.jl
+++ src/qq.jl
@@ -6,15 +6,15 @@ end
 function qqbuild(x::Vector, y::Vector)
 	n = min(length(x), length(y))
 	grid = [0.0:(1 / (n - 1)):1.0;]
-	qx = quantile.(x, grid)
-	qy = quantile.(y, grid)
+	qx = quantile(x, grid)
+	qy = quantile(y, grid)
 	return QQPair(qx, qy)
 end
 
 function qqbuild(x::Vector, d::UnivariateDistribution)
 	n = length(x)
 	grid = [(1 / (n - 1)):(1 / (n - 1)):(1.0 - (1 / (n - 1)));]
-	qx = quantile.(x, grid)
+	qx = quantile(x, grid)
 	qd = quantile.(d, grid)
 	return QQPair(qx, qd)
 end
@@ -23,6 +23,6 @@ function qqbuild(d::UnivariateDistribution, x::Vector)
 	n = length(x)
 	grid = [(1 / (n - 1)):(1 / (n - 1)):(1.0 - (1 / (n - 1)));]
 	qd = quantile.(d, grid)
-	qx = quantile.(x, grid)
+	qx = quantile(x, grid)
 	return QQPair(qd, qx)
 end
