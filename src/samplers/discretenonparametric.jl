0212fef6003de1bc56396e4cebb97c2777eee5b4
Gord Stephen <gord.stephen@nrel.gov>
2019-01-21 12:00:14 -0700

    Fix type instability in DiscreteNonParametricSampler

--- src/samplers/discretenonparametric.jl
+++ src/samplers/discretenonparametric.jl
@@ -4,15 +4,19 @@
 Data structure for efficiently sampling from an arbitrary probability mass
 function defined by support `xs` and probabilities `ps`.
 """
-struct DiscreteNonParametricSampler{T<:Real, S<:AbstractVector{T}} <: Sampleable{Univariate,Discrete}
+struct DiscreteNonParametricSampler{T<:Real, S<:AbstractVector{T}, A<:AliasTable} <: Sampleable{Univariate,Discrete}
     support::S
-    aliastable::AliasTable
+    aliastable::A
 
-    DiscreteNonParametricSampler{T,S}(support::S, probs::AbstractVector{<:Real}) where {T<:Real,S<:AbstractVector{T}} =
-        new(support, AliasTable(probs))
+    function DiscreteNonParametricSampler{T,S}(support::S, probs::AbstractVector{<:Real}
+    ) where {T<:Real,S<:AbstractVector{T}}
+        aliastable = AliasTable(probs)
+        new{T,S,typeof(aliastable)}(support, aliastable)
+    end
 end
 
-DiscreteNonParametricSampler(support::S, probs::AbstractVector{<:Real}) where {T<:Real,S<:AbstractVector{T}} =
+DiscreteNonParametricSampler(support::S, probs::AbstractVector{<:Real}
+    ) where {T<:Real,S<:AbstractVector{T}} =
     DiscreteNonParametricSampler{T,S}(support, probs)
 
 rand(rng::AbstractRNG, s::DiscreteNonParametricSampler) =
