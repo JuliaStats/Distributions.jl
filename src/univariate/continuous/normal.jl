0f756765d1760e76d0acb0ae12d2406e6e4fe59e
Simon Byrne <simonbyrne@gmail.com>
2019-02-14 13:50:24 -0800

    use | instead of /

--- src/univariate/continuous/normal.jl
+++ src/univariate/continuous/normal.jl
@@ -5,17 +5,18 @@ The *normal* or *Gaussian* probability distribution.
 
 # Constructors
 
-    Normal(μ/mu/mean=0, σ/sigma/std=1)
+    Normal(μ|mu|mean=0, σ|sigma|std=1)
 
 Construct a `Normal` object with mean `μ` and standard deviation `σ`,
 
-    Normal(μ/mu/mean=0, σ²/var=1)
+    Normal(μ|mu|mean=0, σ²|var=1)
 
 Construct a `Normal` object with mean `μ` and variance `σ²`.
 
 # Details
 
-The normal distribution mean `μ` and standard deviation `σ` has probability density function has probability density function
+The normal distribution mean `μ` and standard deviation `σ` has probability density
+function has probability density function
 
 ```math
 f(x; \\mu, \\sigma) = \\frac{1}{\\sqrt{2 \\pi \\sigma^2}}
