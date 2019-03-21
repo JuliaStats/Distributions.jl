cb836493439bae342acdc6dfe466b896b36569ec
Tamas K. Papp <tkpapp@gmail.com>
2019-03-21 17:28:31 +0100

    add mean for truncated exponential (#842)
    * add mean for truncated exponential
    
    * handle numerical and theoretical corner cases
    
    When the difference between left and right truncation points is small,
    the naive formula returns NaN or nonsensical results.
    
    * Also consider near-degenerate cases where l-r and θ have different scales.
    
    * simplify condition for midpoint branch

---
+++ src/truncated/exponential.jl
@@ -0,0 +1,20 @@
+#####
+##### Truncated exponential distribition
+#####
+
+function mean(d::Truncated{<:Exponential,Continuous})
+    θ = d.untruncated.θ
+    l, r = extrema(d)           # l is always finite
+    if isfinite(r)
+        if abs(l-r) ≤ √eps()*θ
+            # linear is a good approximation, just take the midpoint
+            middle(l, r)
+        else
+            Δ = r - l
+            R = -Δ/θ
+            θ + middle(l, r) + Δ/2*(exp(R)+1)/expm1(R)
+        end
+    else
+        θ + l
+    end
+end
