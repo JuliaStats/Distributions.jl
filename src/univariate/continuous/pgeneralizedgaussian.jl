816c51ef6555504ca9b5576ddc4cf38e4e6e6845
Jordan Smith <jrp248@wildcats.unh.edu>
2019-04-30 21:44:46 -0400

    Adding multi-type consideration. Line 55 requires a closer look.

--- src/univariate/continuous/pgeneralizedgaussian.jl
+++ src/univariate/continuous/pgeneralizedgaussian.jl
@@ -10,7 +10,7 @@ shape 'p' has the probability density function
 f(x, \\mu, \\alpha, p) = \\frac{p}{2\\alpha\\Gamma(1/p)} e^{-(\\frac{|x-\\mu|}{\\alpha})^p} \\quad x \\in (-\\infty, +\\infty) , \\alpha > 0, p > 0
 ```
 
-The p-gGneralized Gaussian (GGD) is a parametric distribution that incorporates the
+The p-Generalized Gaussian (GGD) is a parametric distribution that incorporates the
 Normal and Laplacian distributions as special cases where p = 1 and p = 2. As p → ∞,
 the distribution approaches the Uniform distribution on (μ-α, μ+α).
 
@@ -29,23 +29,23 @@ External Links
  * [Reference implementation paper](https://www.researchgate.net/publication/254282790_Simulation_of_the_p-generalized_Gaussian_distribution)
  """
 
-struct PGeneralizedGaussian{T<:Real} <: ContinuousUnivariateDistribution
-    α::T
-    μ::T
-    p::T
+struct PGeneralizedGaussian{T1<:Real, T2<:Real, T3<:Real} <: ContinuousUnivariateDistribution
+    α::T1
+    μ::T2
+    p::T3
 
-    PGeneralizedGaussian{T}(μ::T,α::T,p::T) where {T} = (@check_args(PGeneralizedGaussian, α > zero(α) && p > zero(p)); new{T}(μ,α,p))
+    PGeneralizedGaussian(μ::T1,α::T2,p::T3) where {T1,T2,T3} = (@check_args(PGeneralizedGaussian, α > zero(α) && p > zero(p)); new{T1,T2,T3}(μ,α,p))
 end
 
-PGeneralizedGaussian(μ::T,α::T,p::T) where {T<:Real} = PGeneralizedGaussian{T}(μ,α,p)
+PGeneralizedGaussian(μ::T1,α::T2,p::T3) where {T1<:Real,T2<:Real,T3<:Real} = PGeneralizedGaussian{T1,T2,T3}(μ,α,p)
 PGeneralizedGaussian(μ::Integer,α::Integer,p::Integer) = PGeneralizedGaussian(Float64(μ),Float64(α),Float64(p))
 PGeneralizedGaussian(p::Float64) = PGeneralizedGaussian(0.0, 1.0, p)
 PGeneralizedGaussian() = PGeneralizedGaussian(0.0, √2, 2.0) # approximate scale with unity std deviation and shape 2
 
 #### Conversions
 
-convert(::Type{PGeneralizedGaussian{T}}, μ::S, α::S, p::S) where {T <: Real, S <: Real} = PGeneralizedGaussian(T(μ),T(α),T(p))
-convert(::Type{PGeneralizedGaussian{T}}, d::PGeneralizedGaussian{S}) where {T <: Real, S <: Real} = PGeneralizedGaussian(T(d.μ), T(d.α), T(d.p))
+convert(::Type{PGeneralizedGaussian{T1,T2,T3}}, μ::S1, α::S2, p::S3) where {T1 <: Real, T2 <: Real, T3 <:Real, S1 <: Real, S2 <: Real, S3 <: Real} = PGeneralizedGaussian(T1(μ),T2(α),T3(p))
+convert(::Type{PGeneralizedGaussian{T1,T2,T3}}, d::PGeneralizedGaussian{S1,S2,S3}) where {T1 <: Real, T2 <: Real, T3 <: Real, S1 <: Real, S2 <: Real, S3 <: Real} = PGeneralizedGaussian(T1(d.μ), T2(d.α), T3(d.p))
 
 @distr_support PGeneralizedGaussian -Inf Inf
 
@@ -65,24 +65,24 @@ mean(d::PGeneralizedGaussian) = d.μ
 median(d::PGeneralizedGaussian) = d.μ
 mode(d::PGeneralizedGaussian) = d.μ
 
-var(d::PGeneralizedGaussian{T}) where {T<:Real} = (d.α^2) * ( gamma(3 / d.p) / gamma(1 / d.p) )
-std(d::PGeneralizedGaussian{T}) where {T<:Real} = (d.α) * sqrt( gamma(3 / d.p) / gamma(1 / d.p) )
+var(d::PGeneralizedGaussian{T1, T2, T3}) where {T1<:Real, T2<:Real, T3<:Real} = (d.α^2) * ( gamma(3 / d.p) / gamma(1 / d.p) )
+std(d::PGeneralizedGaussian{T1, T2, T3}) where {T1<:Real, T2<:Real, T3<:Real} = (d.α) * sqrt( gamma(3 / d.p) / gamma(1 / d.p) )
 
-skewness(d::PGeneralizedGaussian{T}) where {T<:Real} = zero(T)
-kurtosis(d::PGeneralizedGaussian{T}) where {T<:Real} = (gamma(5 / d.p) * gamma(1 / d.p)) / (gamma(3 / d.p)^2) - 3
+skewness(d::PGeneralizedGaussian{T1, T2, T3}) where {T1<:Real, T2<:Real, T3<:Real} = zero(T)
+kurtosis(d::PGeneralizedGaussian{T1, T2, T3}) where {T1<:Real, T2<:Real, T3<:Real} = (gamma(5 / d.p) * gamma(1 / d.p)) / (gamma(3 / d.p)^2) - 3
 
 # Skewness from Nadarajah, Saralees (September 2005).  "A generalized normal distribution".
 # Journal of Applied Statistics. 32 (7): 685–694
-entropy(d::PGeneralizedGaussian{T}) where {T<:Real} = (1 / d.p) - log( d.p / (2 * d.α * gamma(1 / d.p) ) )
+entropy(d::PGeneralizedGaussian{T1, T2, T3}) where {T1<:Real, T2<:Real, T3<:Real} = (1 / d.p) - log( d.p / (2 * d.α * gamma(1 / d.p) ) )
 
 #### Evaluation
 
-function pdf(d::PGeneralizedGaussian{T}, x::Real) where T<:Real
+function pdf(d::PGeneralizedGaussian{T1,T2,T3}, x::Real) where {T1<:Real, T2<:Real, T3<:Real}
     (μ, α, p) = params(d)
     return ( p / ( 2 * α * gamma(1 / p) ) ) * exp( -( abs(x - μ) / α )^p )
 end
 
-function cdf(d::PGeneralizedGaussian{T}, x::Real) where T<:Real
+function cdf(d::PGeneralizedGaussian{T1,T2,T3}, x::Real) where {T1<:Real, T2<:Real, T3<:Real}
 
     #    To calculate the CDF, the incomplete gamma function is required. The CDF
     #   of the Gamma distribution provides this, with the necessary 1/Γ(a) normalization.
