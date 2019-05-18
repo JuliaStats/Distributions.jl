d6503c4bb5007829ae76d4dd40b468d17bdf5598
Alexander <alexander@plav.in>
2019-05-18 13:22:48 +0300

    Float return values instead of Int

--- src/univariate/discrete/dirac.jl
+++ src/univariate/discrete/dirac.jl
@@ -4,8 +4,8 @@
 A *Dirac distribution* is parametrized by its only value, and takes its value with probability 1.
 
 ```julia
-d = Dirac(2.)  # Dirac distribution with value = 2.
-rand(d)       # Always returns the same value
+d = Dirac(2.0)  # Dirac distribution with value = 2.
+rand(d)         # Always returns the same value
 ```
 """
 struct Dirac{T} <: DiscreteUnivariateDistribution
@@ -14,17 +14,17 @@ end
 
 eltype(::Dirac{T}) where {T} = T
 rand(rng::AbstractRNG, d::Dirac) = d.value
-pdf(d::Dirac, x::Real) = x == d.value ? 1 : 0
-cdf(d::Dirac, x::Real) = x < d.value ? 0 : 1
+pdf(d::Dirac, x::Real) = x == d.value ? 1.0 : 0.0
+cdf(d::Dirac, x::Real) = x < d.value ? 0.0 : 1.0
 quantile(d::Dirac{T}, p) where {T} = 0 <= p <= 1 ? d.value : T(NaN)
 minimum(d::Dirac) = d.value
 maximum(d::Dirac) = d.value
 insupport(d::Dirac, x) = x == d.value
 mean(d::Dirac) = d.value
-var(d::Dirac) = 0
+var(d::Dirac) = 0.0
 mode(d::Dirac) = d.value
-skewness(d::Dirac) = 0
-kurtosis(d::Dirac) = 0
-entropy(d::Dirac) = 0
+skewness(d::Dirac) = 0.0
+kurtosis(d::Dirac) = 0.0
+entropy(d::Dirac) = 0.0
 mgf(d::Dirac, t) = exp(t * d.value)
 cf(d::Dirac, t) = exp(im * t * d.value)
