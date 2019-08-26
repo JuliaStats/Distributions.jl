852848870faf47ebe0303c8fab93aa638fdf7bf7
Richard Reeve <git@richardreeve.net>
2019-08-26 14:11:51 +0100

    Add in testing for countable distributions, and include Binomial as example.

--- test/eltypes.jl
+++ test/eltypes.jl
@@ -20,6 +20,28 @@ function test_types(D::Type{<:UnivariateDistribution{<:ContinuousSupport}},
         typeof(one(eltype(dist)))
 end
 
+function test_types(D::Type{<:UnivariateDistribution{<:CountableSupport}},
+                    targs)
+    dist = D(targs...)
+    @test eltype(dist) ≡ eltype(targs) ≡ typeof(rand(dist)) ≡
+        typeof(rand(GLOBAL_RNG, dist)) ≡ eltype(rand(dist, 3))
+    @test eltype(dist) ≡ typeof(mode(dist))
+    @test eltype(targs) ≡
+        typeof(quantile(dist, 0.5)) ≡ typeof(cquantile(dist, 0.5)) ≡
+        typeof(invlogcdf(dist, -2.0)) ≡ typeof(invlogccdf(dist, -2.0))
+    @test typeof(var(dist)) ≡ typeof(mean(dist) * mean(dist))
+    @test eltype(support(dist)) ≡ typeof(one(eltype(dist)))
+end
+
+@testset "Support for continuous distributions supporting just Float64" begin
+    @testset "Testing $Dist" for (Dist, args) in Dict(
+        # Chernoff broken Chernoff => (),
+        Cosine => (0.0, 1.0)
+    )
+        test_types(Dist, Float64.(args))
+    end
+end
+
 M = typeof(1.0u"m");
 @testset "Support for continuous distributions supporting any eltype" begin
     @testset "Testing $Dist" for (Dist, args) in Dict(
@@ -31,10 +53,12 @@ M = typeof(1.0u"m");
     end
 end
 
-
-
-@testset "Testing support and eltype" begin
-    for dist in [Binomial(), Binomial(Int16(2)), Binomial(UInt8(10))]
-        @test eltype(dist) == typeof(rand(dist)) == eltype(support(dist))
+@testset "Support for discrete distributions supporting only Int64s" begin
+    @testset "Testing $Dist" for (Dist, args) in Dict(
+        Binomial => (10,)
+    )
+        @testset "Testing $T for $Dist" for T in [Int64]
+            test_types(Dist, T.(args))
+        end
     end
 end
