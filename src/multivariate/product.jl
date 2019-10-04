a80efa2cc51c4794d13b82c03b2b3a4c9365c81c
Tom Pennington <tp373@bath.ac.uk>
2019-10-04 14:12:05 +0100

    Added insupport for Product-type distributions

--- src/multivariate/product.jl
+++ src/multivariate/product.jl
@@ -33,6 +33,7 @@ mean(d::Product) = mean.(d.v)
 var(d::Product) = var.(d.v)
 cov(d::Product) = Diagonal(var(d))
 entropy(d::Product) = sum(entropy, d.v)
+insupport(d::Product, x::AbstractArray) = all(insupport.(d.v, x))
 
 """
     product_distribution(dists::AbstractVector{<:UnivariateDistribution})
