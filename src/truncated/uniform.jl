d93d71e1cc6944761d327e22ea43589810837a71
mohamed82008 <mohamed82008@gmail.com>
2019-10-05 16:35:34 +1000

    make Truncated support AD

--- src/truncated/uniform.jl
+++ src/truncated/uniform.jl
@@ -2,4 +2,4 @@
 ##### Shortcut for truncating uniform distributions.
 #####
 
-Truncated(d::Uniform, l::Float64, u::Float64) = Uniform(max(l, d.a), min(u, d.b))
+Truncated(d::Uniform, l::Real, u::Real) = Uniform(promote(max(l, d.a), min(u, d.b))...)
