50d04be09b262d832c811ae013ea9fe7fdcee537
Mark Wells <maw461@psu.edu>
2019-10-14 10:11:15 -0400

    Update test/runtests.jl
    Co-Authored-By: Mathieu Besançon <mathieu.besancon@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -50,7 +50,7 @@ const tests = [
     "discretenonparametric",
     "functionals",
     "chernoff",
-    "univariate_bounds"
+    "univariate_bounds",
 ]
 
 printstyled("Running tests:\n", color=:blue)
