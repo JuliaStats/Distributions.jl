1ab63804d237f6da235fbf2409852bc80e93d77b
aminya <aminyahyaabadi74@gmail.com>
2020-01-01 01:18:22 +0330

    Type stable sampling

--- src/univariate/continuous/uniform.jl
+++ src/univariate/continuous/uniform.jl
@@ -118,7 +118,7 @@ end
 
 #### Sampling
 
-rand(rng::AbstractRNG, d::Uniform) = d.a + (d.b - d.a) * rand(rng)
+rand(rng::AbstractRNG, d::Uniform{T}) where {T} =  convert(T, d.a + (d.b - d.a) * rand(rng))
 
 
 #### Fitting
