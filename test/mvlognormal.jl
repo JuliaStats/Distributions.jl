43264564907fb9a747206494b0981f3f803489a9
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-01-14 10:35:15 +0100

    typo on zeros

--- test/mvlognormal.jl
+++ test/mvlognormal.jl
@@ -18,7 +18,7 @@ function test_mvlognormal(g::MvLogNormal, n_tsamples::Int=10^6,
     e = entropy(g)
     @test partype(g) == Float64
     @test isa(mn, Vector{Float64})
-    if g.normal.μ isa Zers{Float64,1}
+    if g.normal.μ isa Zeros{Float64,1}
         @test md isa FillArrays.Fill{Float64,1}
     else
         @test md isa Vector{Float64}
