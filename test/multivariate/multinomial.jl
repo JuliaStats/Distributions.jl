654a293374e8e0cfd7e11e5c1043690c77700255
Kyle Daruwalla <daruwalla@wisc.edu>
2020-01-29 09:47:42 -0600

    Added test to make sure rand works for real types other than Float64

--- test/multivariate/multinomial.jl
+++ test/multivariate/multinomial.jl
@@ -1,6 +1,6 @@
 # Tests for Multinomial
 
-using Distributions, Random, StaticArrays
+using Distributions, Random, StaticArrays, ForwardDiff
 using Test
 
 
@@ -210,3 +210,9 @@ p_v = [0.1, 0.4, 0.3, 0.8]
 @test_throws DomainError Multinomial(10, p_v)
 @test_throws DomainError Multinomial(10, p_v; check_args=true)
 Multinomial(10, p_v; check_args=false) # should not warn
+
+# check different prob vector types
+p = [0.2, 0.4, 0.3, 0.1]
+@test (rand(Multinomial(10, p)); true)
+@test (rand(Multinomial(10, convert.(Float32, p))); true)
+# @test (rand(Multinomial(10, convert.(ForwardDiff.Dual{Float64}, p))); true)
