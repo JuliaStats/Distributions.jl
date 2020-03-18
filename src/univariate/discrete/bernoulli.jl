a6174ae54574143317260fcb5a0d0af1a87720ce
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-03-18 17:01:46 +0100

    Revert "Revert "Make Bernoulli produce Bools (#1079)""
    This reverts commit 0574a0ffbecc37ca8e1c809b38dfe0accb130c5c.

--- src/univariate/discrete/bernoulli.jl
+++ src/univariate/discrete/bernoulli.jl
@@ -38,7 +38,9 @@ end
 Bernoulli(p::Integer) = Bernoulli(float(p))
 Bernoulli() = Bernoulli(0.5, check_args=false)
 
-@distr_support Bernoulli 0 1
+@distr_support Bernoulli false true
+
+Base.eltype(::Type{<:Bernoulli}) = Bool
 
 #### Conversions
 convert(::Type{Bernoulli{T}}, p::Real) where {T<:Real} = Bernoulli(T(p))
@@ -83,11 +85,11 @@ pdf(d::Bernoulli, x::Bool) = x ? succprob(d) : failprob(d)
 pdf(d::Bernoulli, x::Int) = x == 0 ? failprob(d) :
                             x == 1 ? succprob(d) : zero(d.p)
 
-cdf(d::Bernoulli, x::Bool) = x ? failprob(d) : one(d.p)
+cdf(d::Bernoulli, x::Bool) = x ? one(d.p) : failprob(d)
 cdf(d::Bernoulli, x::Int) = x < 0 ? zero(d.p) :
                             x < 1 ? failprob(d) : one(d.p)
 
-ccdf(d::Bernoulli, x::Bool) = x ? succprob(d) : one(d.p)
+ccdf(d::Bernoulli, x::Bool) = x ? zero(d.p) : succprob(d)
 ccdf(d::Bernoulli, x::Int) = x < 0 ? one(d.p) :
                              x < 1 ? succprob(d) : zero(d.p)
 
@@ -104,7 +106,7 @@ cf(d::Bernoulli, t::Real) = failprob(d) + succprob(d) * cis(t)
 
 #### Sampling
 
-rand(rng::AbstractRNG, d::Bernoulli) = rand(rng) <= succprob(d) ? 1 : 0
+rand(rng::AbstractRNG, d::Bernoulli) = rand(rng) <= succprob(d)
 
 #### MLE fitting
 
