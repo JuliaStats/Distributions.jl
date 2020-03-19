56a5cc9e390100a95fb0296b100aea9f143d572d
san-soucie <44901782+san-soucie@users.noreply.github.com>
2020-03-19 11:30:06 -0400

    Added matrixgaussian.jl to includes

--- src/matrixvariates.jl
+++ src/matrixvariates.jl
@@ -213,7 +213,7 @@ _logpdf(d::MatrixDistribution, x::AbstractArray)
 ##### Specific distributions #####
 
 for fname in ["wishart.jl", "inversewishart.jl", "matrixnormal.jl",
-              "matrixtdist.jl", "matrixbeta.jl", "matrixfdist.jl",
-              "lkj.jl"]
+              "matrigaussian.jl", "matrixtdist.jl", "matrixbeta.jl", 
+              "matrixfdist.jl", "lkj.jl"]
     include(joinpath("matrix", fname))
 end
