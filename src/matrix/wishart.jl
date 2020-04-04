90bf428010470fca9d99aab64bb35215c2555f69
John Zito <johnczito@gmail.com>
2020-04-03 23:54:52 -0500

    update wishart test util

--- src/matrix/wishart.jl
+++ src/matrix/wishart.jl
@@ -239,7 +239,7 @@ end
 
 function _rand_params(::Type{Wishart}, elty, n::Int, p::Int)
     n == p || throw(ArgumentError("dims must be equal for Wishart"))
-    ν = elty( n + 1 + abs(10randn()) )
+    ν = elty( n - 1 + abs(10randn()) )
     S = (X = 2rand(elty, n, n) .- 1; X * X')
     return ν, S
 end
