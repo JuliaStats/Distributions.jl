34704f178aaa2946ea870b677baa3aafba6f6074
Albin Severinson <albin@severinson.org>
2020-06-14 13:05:36 +0200

    run all Soliton tests in a testset, added more tests

--- test/soliton.jl
+++ test/soliton.jl
@@ -1,17 +1,28 @@
 using Distributions
 
-K, M, δ, atol = 100, 60, 0.2, 0
-Ω = Soliton(K, M, δ, atol)
-@test pdf(Ω, M) > pdf(Ω, M-1)
-@test pdf(Ω, M) > pdf(Ω, M+1)
-@test cumsum(pdf.(Ω, 1:K)) ≈ cdf.(Ω, 1:K)
-@test cdf(Ω, 0) ≈ 0
-@test cdf(Ω, K) ≈ 1
-@test mean(Ω) ≈ 7.448826535558562
+@testset "Soliton" begin
+    K, M, δ, atol = 100, 60, 0.2, 0
+    Ω = Soliton(K, M, δ, atol)
+    @test pdf(Ω, M) > pdf(Ω, M-1)
+    @test pdf(Ω, M) > pdf(Ω, M+1)
+    @test cumsum(pdf.(Ω, 1:K)) ≈ cdf.(Ω, 1:K)
+    @test cdf(Ω, 0) ≈ 0
+    @test cdf(Ω, K) ≈ 1
+    @test mean(Ω) ≈ 7.448826535558562
+    @test var(Ω) ≈ 178.91717915136957
+    @test minimum(Ω) == 1
+    @test maximum(Ω) == K
+    @test quantile(Ω, 0) == 1
+    @test quantile(Ω, 1) == K
 
-K, M, δ, atol = 100, 60, 0.2, 1e-3
-Ω = Soliton(K, M, δ, atol)
-ds = [d for d in 1:K if pdf(Ω, d) > 0]
-@test all(pdf.(Ω, ds) .> atol)
-@test cdf(Ω, 0) ≈ 0
-@test cdf(Ω, K) ≈ 1
+    K, M, δ, atol = 100, 60, 0.2, 1e-3
+    Ω = Soliton(K, M, δ, atol)
+    ds = [d for d in 1:K if pdf(Ω, d) > 0]
+    @test all(pdf.(Ω, ds) .> atol)
+    @test cdf(Ω, 0) ≈ 0
+    @test cdf(Ω, K) ≈ 1
+    @test minimum(Ω) == 1
+    @test maximum(Ω) == K
+    @test quantile(Ω, 0) == 1
+    @test quantile(Ω, 1) == M
+end
