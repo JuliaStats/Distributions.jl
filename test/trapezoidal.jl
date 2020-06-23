994280f2af7ca3a84bd0df8d51cf322e41418c40
Andreas Scheidegger <andreas.scheidegger@eawag.ch>
2020-06-23 08:17:58 +0200

    fix test for Julia 1.0

--- test/trapezoidal.jl
+++ test/trapezoidal.jl
@@ -32,11 +32,11 @@ using Distributions
     @test var(d) ≈ 0.41666666666666696
 
     # cdf and quantile
-    for x in range(1, 4, length=20)
+    for x in range(1, stop=4, length=20)
         @test quantile(d, cdf(d, x)) ≈ x
     end
 
-    for p in range(0, 1, length=20)
+    for p in range(0, stop=1, length=20)
         @test cdf(d, quantile(d, p)) ≈ p
     end
 end
