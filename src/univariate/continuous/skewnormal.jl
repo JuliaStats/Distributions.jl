3678a8371e2ad26dd4c9d048018dce997dc8bd37
azev77 <azev77@users.noreply.github.com>
2020-06-28 23:36:36 -0400

    Update skewnormal.jl

--- src/univariate/continuous/skewnormal.jl
+++ src/univariate/continuous/skewnormal.jl
@@ -3,6 +3,8 @@ SkewNormal(ξ, ω, α)
     The *skew normal distribution* is a continuous probability distribution
     that generalises the normal distribution to allow for non-zero skewness.
 #
+This code is based on the three external links below.
+#
 External links
 * [Skew normal distribution on Wikipedia](https://en.wikipedia.org/wiki/Skew_normal_distribution)
 * [Discourse](https://discourse.julialang.org/t/skew-normal-distribution/21549/7)
@@ -28,12 +30,12 @@ SkewNormal() = SkewNormal(0.0, 1.0, 0.0)
 @distr_support SkewNormal -Inf Inf
 
 #### Conversions
-#convert(::Type{SkewNormal{T}}, ξ::S, ω::S, α::S) where {T <: Real, S <: Real} = SkewNormal(T(μ), T(σ))
-#convert(::Type{SkewNormal{T}}, d::SkewNormal{S}) where {T <: Real, S <: Real} = SkewNormal(T(d.μ), T(d.σ), check_args=false)
+convert(::Type{SkewNormal{T}}, ξ::S, ω::S, α::S) where {T <: Real, S <: Real} = SkewNormal(T(ξ), T(ω), T(α))
+convert(::Type{SkewNormal{T}}, d::SkewNormal{S}) where {T <: Real, S <: Real} = SkewNormal(T(d.ξ), T(d.ω), T(d.α), check_args=false)
 
 #### Parameters
 params(d::SkewNormal) = (d.ξ, d.ω, d.α)
-#partype(::SkewNormal{T}) where {T} = T
+@inline partype(d::SkewNormal{T}) where {T<:Real} = T
 
 #### Statistics
 delta(d::SkewNormal) = d.α/√(1+d.α^2)
@@ -45,9 +47,15 @@ var(d::SkewNormal) = abs2(d.ω)*(1-mean_z(d)^2)
 std(d::SkewNormal) = √var(d)
 skewness(d::SkewNormal) = (4-π)/2 * mean_z(d)^3 / (1-mean_z(d)^2)^(3/2)
 
+# no analytic expression for max m_0(d) but accurate numerical approximation 
+m_0(d::SkewNormal) = mean_z(d) - (skewness(d)*std_z(d))/2 - (sign(d.α)/2)*exp(-2π/abs(d.α))
+mode(d::SkewNormal) = d.ξ + d.ω * m_0(d)  
+
+
 #### Evalution
 pdf(d::SkewNormal, x::Real) = 2/d.ω*normpdf((x-d.ξ)/d.ω)*normcdf(d.α*(x-d.ξ)/d.ω)
 logpdf(d::SkewNormal, x::Real) = log(2)-log(d.ω)+normlogpdf((x-d.ξ)/d.ω)+normlogcdf(d.α*(x-d.ξ)/d.ω)
+#cdf requires Owen's T function.
 #cdf/quantile etc mgf/cf
 
 #### Sampling
@@ -59,22 +67,6 @@ function rand(rng::AbstractRNG, d::SkewNormal)
     return d.ξ + d.ω * sign(u0) * u1
 end
 
-## Fitting
-# function fit_mle(::Type{<:LogNormal}, x::AbstractArray{T}) where T<:Real
-#     lx = log.(x)
-#     μ, σ = mean_and_std(lx)
-#     LogNormal(μ, σ)
-# end
+## Fitting  # to be added see: https://github.com/STOR-i/SkewDist.jl/issues/3
 
 
-### AZ test
-# d= SkewNormal()
-# mean(d)
-# var(d)
-#
-# using Random
-# rng = MersenneTwister(123)
-# rand(rng, d)
-#
-# import Distributions: normpdf, normcdf
-# pdf.(d, -10:10 )
