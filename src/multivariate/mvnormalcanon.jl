4907f23d6d5753c7d953582777f18dccde31dccc
Andreas Noack <andreas@noack.dk>
2020-10-16 23:31:31 +0200

    Make new unwhiten_winv! conditional on PDSparseMat
    The `PDSparseMat` struct is defined behind a `HAVE_CHOLMOD` flag which is false when people build Julia without GPL libraries.

--- src/multivariate/mvnormalcanon.jl
+++ src/multivariate/mvnormalcanon.jl
@@ -176,8 +176,10 @@ sqmahal!(r::AbstractVector, d::MvNormalCanon, x::AbstractMatrix) = quad!(r, d.J,
 unwhiten_winv!(J::AbstractPDMat, x::AbstractVecOrMat) = unwhiten!(inv(J), x)
 unwhiten_winv!(J::PDiagMat, x::AbstractVecOrMat) = whiten!(J, x)
 unwhiten_winv!(J::ScalMat, x::AbstractVecOrMat) = whiten!(J, x)
-unwhiten_winv!(J::PDSparseMat, x::AbstractVecOrMat) = x[:] = J.chol.U \ x
-
+if isdefined(PDMats, :PDSparseMat)
+    unwhiten_winv!(J::PDSparseMat, x::AbstractVecOrMat) = x[:] = J.chol.U \ x
+end
+    
 _rand!(rng::AbstractRNG, d::MvNormalCanon, x::AbstractVector) =
     add!(unwhiten_winv!(d.J, randn!(rng,x)), d.μ)
 _rand!(rng::AbstractRNG, d::MvNormalCanon, x::AbstractMatrix) =
