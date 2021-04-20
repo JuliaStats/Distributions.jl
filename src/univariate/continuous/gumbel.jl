b69bdfdf8e0ad77d22f1dc40d2f82e8f7d6a6812
David Widmann <dev+github@devmotion.de>
2021-04-20 23:21:23 +0200

    Faster sampling from `Gumbel`

--- src/univariate/continuous/gumbel.jl
+++ src/univariate/continuous/gumbel.jl
@@ -42,6 +42,8 @@ Gumbel() = Gumbel(0.0, 1.0, check_args=false)
 
 const DoubleExponential = Gumbel
 
+Base.eltype(::Type{Gumbel{T}}) where {T} = T
+
 #### Conversions
 
 convert(::Type{Gumbel{T}}, μ::S, θ::S) where {T <: Real, S <: Real} = Gumbel(T(μ), T(θ))
@@ -54,6 +56,14 @@ scale(d::Gumbel) = d.θ
 params(d::Gumbel) = (d.μ, d.θ)
 partype(::Gumbel{T}) where {T} = T
 
+function Base.rand(rng::Random.AbstractRNG, d::Gumbel)
+    return d.μ - d.θ * log(randexp(rng, float(eltype(d))))
+end
+function Random.rand!(rng::Random.AbstractRNG, d::Gumbel, x::AbstractArray)
+    randexp!(rng, x)
+    x .= d.μ .- d.θ .* log.(x)
+    return x
+end
 
 #### Statistics
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -61,6 +61,7 @@ const tests = [
     "soliton",
     "skewnormal",
     "chi",
+    "gumbel",
 ]
 
 printstyled("Running tests:\n", color=:blue)
