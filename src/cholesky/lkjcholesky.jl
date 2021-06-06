c51c314e3f4e0c6db01913ffb43feaa8e8a6bb09
Seth Axen <seth.axen@gmail.com>
2021-06-07 01:42:47 +0200

    Add constructor

--- src/cholesky/lkjcholesky.jl
+++ src/cholesky/lkjcholesky.jl
@@ -4,3 +4,22 @@ struct LKJCholesky{T <: Real, D <: Integer} <: Distribution{CholeskyVariate,Cont
     uplo::Char
     logc0::T
 end
+
+#  -----------------------------------------------------------------------------
+#  Constructors
+#  -----------------------------------------------------------------------------
+
+function LKJCholesky(d::Integer, η::Real, _uplo::Union{Char,Symbol} = 'L'; check_args = true)
+    if check_args
+        d > 0 || throw(ArgumentError("Matrix dimension must be positive."))
+        η > 0 || throw(ArgumentError("Shape parameter must be positive."))
+    end
+    logc0 = lkj_logc0(d, η)
+    uplo = _as_char(_uplo)
+    uplo ∈ ('U', 'L') || throw(ArgumentError("uplo must be 'U' or 'L'."))
+    T = Base.promote_eltype(η, logc0)
+    LKJCholesky{T, typeof(d)}(d, T(η), uplo, T(logc0))
+end
+
+_as_char(c::Char) = c
+_as_char(x) = only(string(x))
