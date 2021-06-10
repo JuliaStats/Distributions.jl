3fe2abe8c6eb8a26d1f22f81e57fe2a4e7e7cd23
Seth Axen <seth.axen@gmail.com>
2021-06-10 21:01:21 +0200

    Remove unused vine method

--- src/cholesky/lkjcholesky.jl
+++ src/cholesky/lkjcholesky.jl
@@ -191,59 +191,6 @@ function rand!(rng::AbstractRNG, d::LKJCholesky, Rs::AbstractArray{<:Cholesky{<:
 end
 
 #
-# vine method
-#
-
-function _lkj_cholesky_vine_sampler!(rng::AbstractRNG, d::LKJCholesky, R::Cholesky)
-    p, η = params(d)
-    factors = R.factors
-    if R.uplo === 'U'
-        z = _lkj_vine_rand_cpcs!(rng, transpose(factors), p, η)
-        _cpcs_to_cholesky!(factors, z, p)
-    else  # uplo === 'L'
-        z = _lkj_vine_rand_cpcs!(rng, factors, p, η)
-        _cpcs_to_cholesky!(transpose(factors), z, p)
-    end
-    return R
-end
-
-# sample partial canonical correlations z using the vine method from Section 2.4 in LKJ (2009 JMA)
-function _lkj_vine_rand_cpcs!(rng::AbstractRNG, z::AbstractMatrix, d::Integer, η::Real)
-    β = η + (d - 1) // 2
-    for i in 1:(d - 1)
-        β -= 1 // 2
-        spl = sampler(Beta(β, β))
-        # use function barrier since sampler(Beta(β, β)) is not type-stable
-        # see https://github.com/JuliaStats/Distributions.jl/pull/1281#issue-573169822
-        _lkj_vine_rand_cpcs_loop!(rng, spl, z, i, (i + 1):d)
-    end
-    return z
-end
-
-function _lkj_vine_rand_cpcs_loop!(rng, sampler, z, i, js)
-    @inbounds for j in js
-        z[i, j] = 2 * rand(rng, sampler) - 1
-    end
-    return z
-end
-
-# map partial canonical correlations z stored in the upper triangle of a matrix
-# to the corresponding upper triangular cholesky factor w
-# adapted from https://github.com/TuringLang/Bijectors.jl/blob/v0.9.4/src/bijectors/corr.jl
-function _cpcs_to_cholesky!(w, z, d::Integer)
-    @inbounds for j in 1:d
-        w[1, j] = 1
-        for i in 1:(j-1)
-            wij = w[i, j]
-            zij = z[i, j]
-            w[i, j] = zij * wij
-            w[i+1, j] = wij * sqrt(1 - zij^2)
-        end
-    end
-    return w
-end
-
-#
 # onion method
 #
 
