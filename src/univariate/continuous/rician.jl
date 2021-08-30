9ddff52f1c7c2567d5c23e5aeae0380733103574
Mandar Chitre <mandar@nus.edu.sg>
2021-08-30 19:47:28 +0800

    Removed Rician Integer constructor based on review
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/univariate/continuous/rician.jl
+++ src/univariate/continuous/rician.jl
@@ -34,7 +34,6 @@ function Rician(ν::T, σ::T; check_args=true) where {T<:Real}
 end
 
 Rician(ν::Real, σ::Real) = Rician(promote(ν, σ)...)
-Rician(ν::Integer, σ::Integer) = Rician(float(ν), float(σ))
 
 function Rician(; K=0.0, Ω=2.0)
     σ = sqrt(Ω / (K + 1) / 2)
