3ce8f36af05abe581a3d25e95ad16ac2b5cb7ff8
st-- <st--@users.noreply.github.com>
2021-10-04 16:35:51 +0300

    Apply suggestions from code review

--- src/univariate/continuous/normalcanon.jl
+++ src/univariate/continuous/normalcanon.jl
@@ -1,9 +1,10 @@
 """
     NormalCanon(η, λ)
 
-The canonical form of the normal distribution, which is an [exponential family distribution](http://en.wikipedia.org/wiki/Exponential_family).
-The two *canonical parameters* are ``\\eta = \\sigma^{-1} \\mu`` and ``\\lambda = \\sigma^{-1}``,
-where ``\\mu`` is the mean and ``\\sigma`` is the standard deviation.
+Canonical parametrisation of the Normal distribution with canonical parameters `η` and `λ`.
+
+The two *canonical parameters* of a normal distribution ``\\mathcal{N}(\\mu, \\sigma^2)`` with mean ``\\mu`` and
+standard deviation ``\\sigma`` are ``\\eta = \\sigma^{-2} \\mu`` and ``\\lambda = \\sigma^{-2}``.
 """
 struct NormalCanon{T<:Real} <: ContinuousUnivariateDistribution
     η::T       # σ^(-2) * μ
