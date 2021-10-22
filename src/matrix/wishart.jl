13294458f94d4304ce93dcee236b40f373b07f60
David Widmann <dev+github@devmotion.de>
2021-10-22 17:45:00 +0200

    Remove warning in constructor of `Wishart`

--- src/matrix/wishart.jl
+++ src/matrix/wishart.jl
@@ -45,13 +45,13 @@ end
 function Wishart(df::T, S::AbstractPDMat{T}, warn::Bool = true) where T<:Real
     df > 0 || throw(ArgumentError("df must be positive. got $(df)."))
     p = dim(S)
-    rnk = p
     singular = df <= p - 1
     if singular
-        isinteger(df) || throw(ArgumentError("singular df must be an integer. got $(df)."))
-        rnk = convert(Integer, df)
-        warn && @warn("got df <= dim - 1; returning a singular Wishart")
+        isinteger(df) || throw(
+            ArgumentError("df of a singular Wishart distribution must be an integer (got $df)")
+        )
     end
+    rnk::Integer = ifelse(singular, df, p)
     logc0 = wishart_logc0(df, S, rnk)
     R = Base.promote_eltype(T, logc0)
     prom_S = convert(AbstractArray{T}, S)
