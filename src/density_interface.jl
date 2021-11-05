29b01fccd7f8641f88cfe8f0e02a44b0c83d1224
Oliver Schulz <oschulz@mpp.mpg.de>
2021-11-05 13:30:09 +0100

    DensityInterface.logdensityof should call loglikelihood
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/density_interface.jl
+++ src/density_interface.jl
@@ -1,5 +1,5 @@
 @inline DensityInterface.hasdensity(::Distribution) = true
 
-DensityInterface.logdensityof(d::Distribution, x) = logpdf(d, x)
+DensityInterface.logdensityof(d::Distribution, x) = loglikelihood(d, x)
 
 DensityInterface.densityof(d::Distribution, x) = pdf(d, x)
