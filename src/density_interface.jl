1fc67cafd351aff3e3a1e2f74b552db2a67770f5
Oliver Schulz <oschulz@mpp.mpg.de>
2021-11-05 19:43:25 +0100

    Don't specialize DensityInterface.densityof for now.

--- src/density_interface.jl
+++ src/density_interface.jl
@@ -2,4 +2,6 @@
 
 DensityInterface.logdensityof(d::Distribution, x) = loglikelihood(d, x)
 
-DensityInterface.densityof(d::Distribution, x) = pdf(d, x)
+# Don't specialize `DensityInterface.densityof(d::Distribution, x)`
+# until something like `likelihood(d, x)` is available, `pdf(d, x)` can have
+# different behavior.
