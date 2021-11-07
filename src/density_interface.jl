10ed991f39ef54fed6ff6b19b107cd96f9516b35
Oliver Schulz <oschulz@mpp.mpg.de>
2021-11-07 17:40:38 +0100

    logdensityof doesn't support multiple samples

--- src/density_interface.jl
+++ src/density_interface.jl
@@ -2,6 +2,18 @@
 
 DensityInterface.logdensityof(d::Distribution, x) = loglikelihood(d, x)
 
+
+DensityInterface.logdensityof(d::Distribution{ArrayLikeVariate{N}}, x::AbstractArray{<:Real,N}) where N = loglikelihood(d, x)
+
+function DensityInterface.logdensityof(d::Distribution{ArrayLikeVariate{N}}, x::AbstractArray{<:Real}) where N
+    throw(ArgumentError("logdensityof doesn't support multiple samples in a single function call"))
+end
+
+function DensityInterface.logdensityof(d::Distribution{ArrayLikeVariate{N}}, x::AbstractVector{<:AbstractArray{<:Real}}) where N
+    throw(ArgumentError("logdensityof doesn't support multiple samples in a single function call"))
+end
+
+
 # Don't specialize `DensityInterface.densityof(d::Distribution, x)`
 # until something like `likelihood(d, x)` is available, `pdf(d, x)` can have
 # different behavior.
--- test/density_interface.jl
+++ test/density_interface.jl
@@ -4,8 +4,9 @@
     d_uv_continous = Normal(-1.5, 2.3)
     d_uv_discrete = Poisson(4.7)
     d_mv = MvNormal([2.3 0.4; 0.4 1.2])
+    d_av = Distributions.MatrixReshaped(MvNormal(rand(10)), 2, 5)
 
-    for d in (d_uv_continous, d_uv_discrete, d_mv)
+    for d in (d_uv_continous, d_uv_discrete, d_mv, d_av)
         x = rand(d)
         ref_logd_at_x = logpdf(d, x)
         DensityInterface.test_density_interface(d, x, ref_logd_at_x)
@@ -13,4 +14,8 @@
         # Stricter than required by test_density_interface:
         @test logfuncdensity(logdensityof(d)) === d
     end
+
+    @test_throws ArgumentError logdensityof(d_uv_continous, [rand(d_uv_continous) for i in 1:3])
+    @test_throws ArgumentError logdensityof(d_mv, hcat([rand(d_mv) for i in 1:3]...))
+    @test_throws ArgumentError logdensityof(d_av, [rand(d_av) for i in 1:3])
 end
