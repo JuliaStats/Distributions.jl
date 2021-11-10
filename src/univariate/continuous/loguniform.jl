cd481442005170a5d649a20832a5acbe12071d9c
Jan Weidner <jw3126@gmail.com>
2021-11-10 13:36:02 +0100

    improve LogUniform docs

--- src/univariate/continuous/loguniform.jl
+++ src/univariate/continuous/loguniform.jl
@@ -1,12 +1,11 @@
 """
     LogUniform(a,b)
 
-The *log uniform* distribution also known as *reciprocal distribution* is the exponential
-of the [`Uniform`](@ref) distribution `Uniform(log(a), log(b))`.
+A positive random variable `X` is log-uniformly with parameters `a` and `b` if the logarithm of `X` is `Uniform(log(a), log(b))`.
+The *log uniform* distribution is also known as *reciprocal distribution*.
 ```julia
 LogUniform(1,10)
 ```
-
 External links
 
 * [Log uniform distribution on Wikipedia](https://en.wikipedia.org/wiki/Reciprocal_distribution)
