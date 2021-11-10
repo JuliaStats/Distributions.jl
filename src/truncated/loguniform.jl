3911ba8d5735478714dade9631a2077f1d6c4f5b
Jan Weidner <jw3126@gmail.com>
2021-11-10 17:29:33 +0100

    Update src/truncated/loguniform.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/truncated/loguniform.jl
+++ src/truncated/loguniform.jl
@@ -1,7 +1,7 @@
-truncated(d::LogUniform, lo::T, hi::T) where {T<:Real} = truncated_LogUniform(d,lo,hi)
-truncated(d::LogUniform, lo::Integer, hi::Integer) = truncated_LogUniform(d,lo,hi)
-
-function truncated_LogUniform(d::LogUniform, lo, hi)
-    a,b = params(d)
-    LogUniform(max(a, lo), min(b, hi))
+function truncated(d::LogUniform, lo::T, hi::T) where {T<:Real}
+    a, b = params(d)
+    return LogUniform(max(a, lo), min(b, hi))
 end
+
+# fix method ambiguity
+truncated(d::LogUniform, lo::Integer, hi::Integer) = truncated(d, float(lo), float(hi))
