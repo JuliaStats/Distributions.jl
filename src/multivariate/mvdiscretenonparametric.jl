cbb6ae75965c9ff79f91f53fc81f3ac047b2d86b
Davi Barreira <davisbarreira@gmail.com>
2021-11-16 21:39:39 -0300

    Removing ArraysOfArarys dependency.

--- src/multivariate/mvdiscretenonparametric.jl
+++ src/multivariate/mvdiscretenonparametric.jl
@@ -1,11 +1,89 @@
-const MvDiscreteNonParametric{T<:AbstractVector{<:Real},P<:Real,Ts<:AbstractVector{T},Ps<:AbstractVector{P}} = GeneralDiscreteNonParametric{Multivariate,T,P,Ts,Ps}
+# struct MvDiscreteNonParametric{T <: Real,P <: Real,Ts <:  ArrayOfSimilarArrays{T},Ps <: AbstractVector{P}} <: DiscreteMultivariateDistribution
+#     support::Ts
+#     p::Ps
+#     function MvDiscreteNonParametric{T,P,Ts,Ps}(support::Ts,
+#         p::Ps) where {T <: Real,P <: Real,Ts <: AbstractVector{<:AbstractVector{T}},Ps <: AbstractVector{P}}
+#         length(support) == length(p) || error("length of `support` and `p` must be equal")
+#         isprobvec(p) || error("`p` must be a probability vector")
+#         allunique(support) || error("`support` must contain only unique value")
+#         new{T,P,Ts,Ps}(support, p)
+#     end
+# end
+# """
+#     MvDiscreteNonParametric(
+#         support::AbstractVector,
+#         p::AbstractVector{<:Real}=fill(inv(length(support)), length(support)),
+#     )
+# Construct a multivariate discrete nonparametric probability distribution with `support` and corresponding
+# probabilities `p`. If the probability vector argument is not passed, then
+# equal probability is assigned to each entry in the support.
+# # Examples
+# ```julia
+# using ArraysOfArrays
+# # rows correspond to samples
+# μ = MvDiscreteNonParametric(nestedview(rand(7,3)'))
+# # columns correspond to samples
+# ν = MvDiscreteNonParametric(nestedview(rand(7,3)))
+# ```
+# """
+# function MvDiscreteNonParametric(
+#     support::AbstractVector{<:AbstractVector{<:Real}},
+#     p::AbstractVector{<:Real}=fill(inv(length(support)), length(support)),
+# )
+#     return MvDiscreteNonParametric{eltype(eltype(support)),eltype(p),typeof(ArrayOfSimilarArrays(support)),typeof(p)}(
+#         ArrayOfSimilarArrays(support), p)
+# end
+# """
+#     MvDiscreteNonParametric(
+#         support::Matrix{<:Real},
+#         p::AbstractVector{<:Real}=fill(inv(length(support)), length(support)
+#     )
+# Construct a multivariate discrete nonparametric probability distribution
+# from a matrix as `support` where each row is a sample, and corresponding
+# probabilities `p`. If the probability vector argument is not passed, then
+# equal probability is assigned to each entry in the support.
+# # Examples
+# ```julia
+# # the rows correspond to the samples
+# using LinearAlgebra
+# μ = MvDiscreteNonParametric(rand(10,3), normalize!(rand(10),1))
+# ```
+# """
+# function MvDiscreteNonParametric(
+#     support::Matrix{<:Real},
+#     p::AbstractVector{<:Real}=fill(inv(size(support)[1]), size(support)[1])
+# )
+#     return MvDiscreteNonParametric(nestedview(support'), p)
+# end
+# Base.eltype(::Type{<:MvDiscreteNonParametric{T}}) where T = T
 
+
+
+# struct GeneralDiscreteNonParametric{VF,T,P<:Real,Ts<:AbstractVector{T},Ps<:AbstractVector{P}} <: Distribution{VF,Discrete}
+
+#     support::Ts
+#     p::Ps
+
+#     function GeneralDiscreteNonParametric{VF,T,P,Ts,Ps}(support::Ts,
+#         p::Ps) where {T,P <: Real,Ts <: AbstractVector{<:AbstractVector{T}},Ps <: AbstractVector{P}}
+
+#         length(support) == length(p) || error("length of `support` and `p` must be equal")
+#         isprobvec(p) || error("`p` must be a probability vector")
+#         allunique(support) || error("`support` must contain only unique value")
+#         new{VF,T,P,Ts,Ps}(support, p)
+#     end
+# end
+
+# const MvDiscreteNonParametric{T<:AbstractVector{<:Real},
+#     P<:Real,Ts<:AbstractVector{T},
+#     Ps<:AbstractVector{P}} = GeneralDiscreteNonParametric{Multivariate,T,P,Ts,Ps}
+
+struct MvDiscreteNonParametric{T <: Real,P <: Real,
+    Ts <: AbstractVector{<: AbstractVector{T}},Ps <: AbstractVector{P}} <: DiscreteMultivariateDistribution
     support::Ts
     p::Ps
-
     function MvDiscreteNonParametric{T,P,Ts,Ps}(support::Ts,
         p::Ps) where {T <: Real,P <: Real,Ts <: AbstractVector{<:AbstractVector{T}},Ps <: AbstractVector{P}}
-
         length(support) == length(p) || error("length of `support` and `p` must be equal")
         isprobvec(p) || error("`p` must be a probability vector")
         allunique(support) || error("`support` must contain only unique value")
@@ -13,6 +91,21 @@ const MvDiscreteNonParametric{T<:AbstractVector{<:Real},P<:Real,Ts<:AbstractVect
     end
 end
 
+
+# struct MvDiscreteNonParametric{VF,T,P<:Real,Ts<:AbstractVector{T},Ps<:AbstractVector{P}} <: Distribution{VF,Discrete}
+
+#     support::Ts
+#     p::Ps
+
+#     function MvDiscreteNonParametric{T,P,Ts,Ps}(support::Ts,
+#         p::Ps) where {T <: Real,P <: Real,Ts <: AbstractVector{<:AbstractVector{T}},Ps <: AbstractVector{P}}
+
+#         length(support) == length(p) || error("length of `support` and `p` must be equal")
+#         isprobvec(p) || error("`p` must be a probability vector")
+#         allunique(support) || error("`support` must contain only unique value")
+#         new{T,P,Ts,Ps}(support, p)
+#     end
+# end
 """
     MvDiscreteNonParametric(
         support::AbstractVector,
@@ -34,11 +127,10 @@ using ArraysOfArrays
 ```
 """
 function MvDiscreteNonParametric(
-    support::AbstractVector{<:AbstractVector{<:Real}},
+    support::AbstractArray{<:AbstractVector{<:Real}},
     p::AbstractVector{<:Real}=fill(inv(length(support)), length(support)),
 )
-    return MvDiscreteNonParametric{eltype(eltype(support)),eltype(p),typeof(ArrayOfSimilarArrays(support)),typeof(p)}(
-        ArrayOfSimilarArrays(support), p)
+    return MvDiscreteNonParametric{eltype(eltype(support)),eltype(p),typeof(support),typeof(p)}(support, p)
 end
 
 """
@@ -82,7 +174,7 @@ probs(d::MvDiscreteNonParametric) = d.p
 
 
 # It would be more intuitive if length was the
-# 
+#
 Base.length(d::MvDiscreteNonParametric) = length(first(d.support))
 Base.size(d::MvDiscreteNonParametric) = (length(d), length(d.support))
 
@@ -101,7 +193,6 @@ function _rand!(rng::AbstractRNG, d::MvDiscreteNonParametric, x::AbstractVector{
     end
     copyto!(x, s[i])
     return x
-end
 end
 
 function _logpdf(d::MvDiscreteNonParametric, x::AbstractVector{T}) where T <: Real
