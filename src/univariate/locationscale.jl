46d49d1c76d50f1597479945938e792bf0002640
Carlos Parada <paradac@carleton.edu>
2021-12-11 13:30:59 -0800

    Update src/univariate/locationscale.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/univariate/locationscale.jl
+++ src/univariate/locationscale.jl
@@ -63,7 +63,7 @@ Base.eltype(::Type{<:AffineDistribution{T}}) where T = T
 minimum(d::AffineDistribution) = d.μ + d.σ * minimum(d.ρ)
 maximum(d::AffineDistribution) = d.μ + d.σ * maximum(d.ρ)
 support(d::AffineDistribution) = AffineDistribution_support(d.μ, d.σ, support(d.ρ))
-function AffineDistribution_support(μ::Real, σ::Real, support::RealInterval)
+function affinedistribution_support(μ::Real, σ::Real, support::RealInterval)
     return RealInterval(μ + σ * support.lb, μ + σ * support.ub)
 end
 AffineDistribution_support(μ::Real, σ::Real, support) = μ .+ σ .* support
