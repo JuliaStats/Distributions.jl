a1e8fe32738e9e41028d90cbc7c5358792d4bf12
Seth Axen <seth.axen@gmail.com>
2022-01-31 12:54:34 +0100

    Add and test partially truncated overloads for discrete uniform

--- src/truncated/discrete_uniform.jl
+++ src/truncated/discrete_uniform.jl
@@ -7,3 +7,9 @@ function truncated(d::DiscreteUniform, l::T, u::T) where {T <: Real}
     b = floor(Int, min(u, d.b))
     return DiscreteUniform(a, b)
 end
+function truncated(d::DiscreteUniform, l::Real, ::Nothing)
+    return DiscreteUniform(ceil(Int, max(l, d.a)), d.b)
+end
+function truncated(d::DiscreteUniform, ::Nothing, u::Real)
+    return DiscreteUniform(d.a, floor(Int, min(u, d.b)))
+end
