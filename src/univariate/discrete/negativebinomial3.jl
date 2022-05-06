46b7b13b6ec5c95b041145aa3874e5e589f17663
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-05-05 18:49:28 -0700

    Update NegativeBinomial3

--- src/univariate/discrete/negativebinomial3.jl
+++ src/univariate/discrete/negativebinomial3.jl
@@ -14,12 +14,12 @@ z \\sim \\text{Gamma}(\\alpha, \\beta)
 ```
 
 ```julia
-NegativeBinomial3()        # distribution with α = 1.0 and β = 1.0
-NegativeBinomial3(α, β)    # distribution with shape α and scale β
+NegativeBinomialPoissonGamma()        # distribution with α = 1.0 and β = 1.0
+NegativeBinomialPoissonGamma(α, β)    # distribution with shape α and scale β
 
-convert(NegativeBinomial{T}, d)         # Parametric conversion to NegativeBinomial
-convert(NegativeBinomial2Log{T}, d)     # Parametric conversion to NegativeBinomial2Log
-convert(NegativeBinomial3{T}, d)        # Parametric conversion to NegativeBinomial3
+convert(NegativeBinomial{T}, d)                # Parametric conversion to NegativeBinomial
+convert(NegativeBinomialLocation{T}, d)        # Parametric conversion to NegativeBinomialLocation
+convert(NegativeBinomialLogLocation{T}, d)     # Parametric conversion to NegativeBinomialLogLocation
 ```
 
 External links:
@@ -27,58 +27,65 @@ External links:
 Note: the definition above uses the scale parameterization of the Gamma distribution
 (matching Distributions.jl), whereas BDA uses the inverse scale parameterization.
 
-See also: [`NegativeBinomial2`](@ref), [`NegativeBinomial2Log`](@ref)
+See also: [`NegativeBinomialLocation`](@ref), [`NegativeBinomialLogLocation`](@ref)
 """
-struct NegativeBinomial3{T<:Real} <: DiscreteUnivariateDistribution
+struct NegativeBinomialPoissonGamma{T<:Real} <: DiscreteUnivariateDistribution
     α::T
     β::T
-    NegativeBinomial3{T}(α::T, β::T) where {T<:Real} = new{T}(α, β)
+    NegativeBinomialPoissonGamma{T}(α::T, β::T) where {T<:Real} = new{T}(α, β)
 end
 
-function NegativeBinomial3(α::T, β::T; check_args::Bool=true) where {T<:Real}
-    @check_args NegativeBinomial3 (α, α > zero(α)) (β, β > zero(β))
-    return NegativeBinomial3{T}(α, β)
+const PoissonGamma = NegativeBinomialPoissonGamma
+
+function NegativeBinomialPoissonGamma(α::T, β::T; check_args::Bool=true) where {T<:Real}
+    @check_args NegativeBinomialPoissonGamma (α, α > zero(α)) (β, β > zero(β))
+    return NegativeBinomialPoissonGamma{T}(α, β)
 end
 
-NegativeBinomial3(α::Real, β::Real; check_args::Bool=true) = NegativeBinomial3(promote(α, β)...; check_args=check_args)
-NegativeBinomial3(α::Real; check_args::Bool=true) = NegativeBinomial3(α, one(α); check_args=check_args)
-NegativeBinomial3() = NegativeBinomial3{Float64}(1.0, 1.0)
+NegativeBinomialPoissonGamma(α::Real, β::Real; check_args::Bool=true) = NegativeBinomialPoissonGamma(promote(α, β)...; check_args=check_args)
+NegativeBinomialPoissonGamma(α::Integer, β::Integer; check_args::Bool=true) = NegativeBinomialPoissonGamma(float(α), float(β); check_args=check_args)
+NegativeBinomialPoissonGamma(α::Real; check_args::Bool=true) = NegativeBinomialPoissonGamma(α, one(α); check_args=check_args)
+NegativeBinomialPoissonGamma() = NegativeBinomialPoissonGamma{Float64}(1.0, 1.0)
 
-@distr_support NegativeBinomial3 0 Inf
+@distr_support NegativeBinomialPoissonGamma 0 Inf
 
-insupport(d::NegativeBinomial3, x::Real) = isinteger(x) && x ≥ 0
+insupport(d::NegativeBinomialPoissonGamma, x::Real) = isinteger(x) && x ≥ 0
 #### Conversions
 
-function convert(::Type{NegativeBinomial3{T}}, d::NegativeBinomial3) where {T<:Real}
-    NegativeBinomial3{T}(T(d.α), T(d.β))
+function convert(::Type{NegativeBinomialPoissonGamma{T}}, d::NegativeBinomialPoissonGamma) where {T<:Real}
+    NegativeBinomialPoissonGamma{T}(T(d.α), T(d.β))
 end
-convert(::Type{NegativeBinomial3{T}}, d::NegativeBinomial3{T}) where {T<:Real} = d
+convert(::Type{NegativeBinomialPoissonGamma{T}}, d::NegativeBinomialPoissonGamma{T}) where {T<:Real} = d
 
 #### Parameters
 
-params(d::NegativeBinomial3) = (d.α, d.β)
-partype(::NegativeBinomial3{T}) where {T} = T
+params(d::NegativeBinomialPoissonGamma) = (d.α, d.β)
+partype(::NegativeBinomialPoissonGamma{T}) where {T} = T
 
-succprob(d::NegativeBinomial3{T}) where {T} = inv(d.β + one(T))
-failprob(d::NegativeBinomial3{T}) where {T} = (β = d.β; β / (β + one(T)))
+shape(d::NegativeBinomialPoissonGamma) = d.α
+scale(d::NegativeBinomialPoissonGamma) = d.β
+
+succprob(d::NegativeBinomialPoissonGamma{T}) where {T} = inv(d.β + one(T))
+failprob(d::NegativeBinomialPoissonGamma{T}) where {T} = d.β / (d.β + one(T))
 
 #### Statistics
 
-mean(d::NegativeBinomial3) = d.α * d.β
+mean(d::NegativeBinomialPoissonGamma) = d.α * d.β
 
-var(d::NegativeBinomial3{T}) where {T} = d.α * d.β * (one(T) + d.β)
+var(d::NegativeBinomialPoissonGamma{T}) where {T} = d.α * d.β * (one(T) + d.β)
 
-std(d::NegativeBinomial3{T}) where {T} = √(d.α * d.β * (one(T) + d.β))
+std(d::NegativeBinomialPoissonGamma{T}) where {T} = √(d.α * d.β * (one(T) + d.β))
 
-skewness(d::NegativeBinomial3{T}) where {T} = (p = succprob(d); (T(2) - p) / sqrt((one(T) - p) * d.α))
+skewness(d::NegativeBinomialPoissonGamma{T}) where {T} = (p = succprob(d); (T(2) - p) / sqrt((one(T) - p) * d.α))
 
-kurtosis(d::NegativeBinomial3{T}) where {T} = (p = succprob(d); T(6) / d.α + (p * p) / ((one(T) - p) * d.α))
+kurtosis(d::NegativeBinomialPoissonGamma{T}) where {T} = (p = succprob(d); T(6) / d.α + (p * p) / ((one(T) - p) * d.α))
 
-mode(d::NegativeBinomial3{T}) where {T} = d.α > one(T) ? floor(Int, d.β * (d.α - one(T))) : 0
+mode(d::NegativeBinomialPoissonGamma{T}) where {T} = floor(Int, d.β * (d.α - one(T)))
+mode(d::NegativeBinomialPoissonGamma{BigFloat}) = floor(BigInt, d.β * (d.α - one(T)))
 
 #### Evaluation & Sampling
 
-function logpdf(d::NegativeBinomial3, n::Real)
+function logpdf(d::NegativeBinomialPoissonGamma, n::Real)
     α, β = params(d)
     r = n * log(β) - (n + α) * log(β + 1)
     if isone(succprob(d)) && iszero(n)
@@ -90,29 +97,29 @@ function logpdf(d::NegativeBinomial3, n::Real)
     end
 end
 
-rand(rng::AbstractRNG, d::NegativeBinomial3) = rand(rng, Poisson(rand(rng, Gamma(d.α, d.β))))
+rand(rng::AbstractRNG, d::NegativeBinomialPoissonGamma) = rand(rng, Poisson(rand(rng, Gamma(d.α, d.β))))
 
 # cdf and quantile is roundabout, but this is the most reliable approach
-cdf(d::NegativeBinomial3{T}, x::Real) where {T} = cdf(convert(NegativeBinomial{T}, d), x)
-ccdf(d::NegativeBinomial3{T}, x::Real) where {T} = ccdf(convert(NegativeBinomial{T}, d), x)
-logcdf(d::NegativeBinomial3{T}, x::Real) where {T} = logcdf(convert(NegativeBinomial{T}, d), x)
-logccdf(d::NegativeBinomial3{T}, x::Real) where {T} = logccdf(convert(NegativeBinomial{T}, d), x)
-quantile(d::NegativeBinomial3{T}, q::Real) where {T} = quantile(convert(NegativeBinomial{T}, d), q)
-cquantile(d::NegativeBinomial3{T}, q::Real) where {T} = quantile(convert(NegativeBinomial{T}, d), q)
-invlogcdf(d::NegativeBinomial3{T}, lq::Real) where {T} = invlogcdf(convert(NegativeBinomial{T}, d), lq)
-invlogccdf(d::NegativeBinomial3{T}, lq::Real) where {T} = invlogccdf(convert(NegativeBinomial{T}, d), lq)
+cdf(d::NegativeBinomialPoissonGamma{T}, x::Real) where {T} = cdf(convert(NegativeBinomial{T}, d), x)
+ccdf(d::NegativeBinomialPoissonGamma{T}, x::Real) where {T} = ccdf(convert(NegativeBinomial{T}, d), x)
+logcdf(d::NegativeBinomialPoissonGamma{T}, x::Real) where {T} = logcdf(convert(NegativeBinomial{T}, d), x)
+logccdf(d::NegativeBinomialPoissonGamma{T}, x::Real) where {T} = logccdf(convert(NegativeBinomial{T}, d), x)
+quantile(d::NegativeBinomialPoissonGamma{T}, q::Real) where {T} = quantile(convert(NegativeBinomial{T}, d), q)
+cquantile(d::NegativeBinomialPoissonGamma{T}, q::Real) where {T} = quantile(convert(NegativeBinomial{T}, d), q)
+invlogcdf(d::NegativeBinomialPoissonGamma{T}, lq::Real) where {T} = invlogcdf(convert(NegativeBinomial{T}, d), lq)
+invlogccdf(d::NegativeBinomialPoissonGamma{T}, lq::Real) where {T} = invlogccdf(convert(NegativeBinomial{T}, d), lq)
 
 
-function mgf(d::NegativeBinomial3, t::Real)
+function mgf(d::NegativeBinomialPoissonGamma{T}, t::Real) where {T}
     α, β = params(d)
-    p = inv(β + 1)
+    p = inv(β + one(T))
     # ((1 - p) * exp(t))^α / (1 - p * exp(t))^α
-    ((1 - p) / (inv(exp(t)) - p))^α
+    ((one(T) - p) / (inv(exp(t)) - p))^α
 end
 
-function cf(d::NegativeBinomial3, t::Real)
+function cf(d::NegativeBinomialPoissonGamma{T}, t::Real) where {T}
     α, β = params(d)
-    p = inv(β + 1)
+    p = inv(β + one(T))
     # (((1 - p) * cis(t)) / (1 - p * cis(t)))^α
-    ((1 - p) / (inv(cis(t)) - p))^α
+    ((one(T) - p) / (inv(cis(t)) - p))^α
 end
