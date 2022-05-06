a542b30ad1756c3acacaadb06ecbe4e60a4e923f
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-05-05 19:02:56 -0700

    Update extant tests

--- test/negativebinomial2.jl
+++ test/negativebinomial2.jl
@@ -2,32 +2,32 @@ using Distributions
 using Test
 
 @testset "Check the corner case p==1" begin
-    d = convert(NegativeBinomial2{Float64}, NegativeBinomial(0.5, 1.0))
+    d = convert(NegativeBinomialLocation{Float64}, NegativeBinomial(0.5, 1.0))
     @test logpdf(d, 0) === 0.0
     @test logpdf(d, 1) === -Inf
 end
 
 @testset "Interconversion checks" begin
-    d = NegativeBinomial2()
+    d = NegativeBinomialLocation()
     nb = NegativeBinomial()
-    nb2 = NegativeBinomial2()
-    nb2l = NegativeBinomial2Log()
-    nb3 = NegativeBinomial3()
-    for (d₂, T) ∈ zip((nb, nb2, nb2l, nb3), (NegativeBinomial, NegativeBinomial2, NegativeBinomial2Log, NegativeBinomial3))
-        # Convert to other distribution from NegativeBinomial2
+    nbl = NegativeBinomialLocation()
+    nbll = NegativeBinomialLogLocation()
+    nbpg = NegativeBinomialPoissonGamma()
+    for (d₂, T) ∈ zip((nb, nbl, nbll, nbpg), (NegativeBinomial, NegativeBinomialLocation, NegativeBinomialLogLocation, NegativeBinomialPoissonGamma))
+        # Convert to other distribution from NegativeBinomialLocation
         @test d₂ == convert(T{Float64}, d)
-        # Convert from other distribution to NegativeBinomial2
-        @test d == convert(NegativeBinomial2{Float64}, d₂)
+        # Convert from other distribution to NegativeBinomialLocation
+        @test d == convert(NegativeBinomialLocation{Float64}, d₂)
     end
 end
 
 @testset "logpdf checks" begin
-    d = NegativeBinomial2()
+    d = NegativeBinomialLocation()
     nb = NegativeBinomial()
-    nb2 = NegativeBinomial2()
-    nb2l = NegativeBinomial2Log()
-    nb3 = NegativeBinomial3()
-    for d₂ ∈ (nb, nb2, nb2l, nb3), n ∈ (2, 10, 10^2, 10^5, 10^8)
+    nbl = NegativeBinomialLocation()
+    nbll = NegativeBinomialLogLocation()
+    nbpg = NegativeBinomialPoissonGamma()
+    for d₂ ∈ (nb, nbl, nbll, nbpg), n ∈ (2, 10, 10^2, 10^5, 10^8)
         @test logpdf(d, n) ≈ logpdf(d₂, n)
     end
 end
--- test/negativebinomial3.jl
+++ test/negativebinomial3.jl
@@ -8,26 +8,26 @@ using Test
 end
 
 @testset "Interconversion checks" begin
-    d = NegativeBinomial3()
+    d = NegativeBinomialPoissonGamma()
     nb = NegativeBinomial()
-    nb2 = NegativeBinomial2()
-    nb2l = NegativeBinomial2Log()
-    nb3 = NegativeBinomial3()
-    for (d₂, T) ∈ zip((nb, nb2, nb2l, nb3), (NegativeBinomial, NegativeBinomial2, NegativeBinomial2Log, NegativeBinomial3))
-        # Convert to other distribution from NegativeBinomial2
+    nbl = NegativeBinomialLocation()
+    nbll = NegativeBinomialLogLocation()
+    nbpg = NegativeBinomialPoissonGamma()
+    for (d₂, T) ∈ zip((nb, nbl, nbll, nbpg), (NegativeBinomial, NegativeBinomialLocation, NegativeBinomialLogLocation, NegativeBinomialPoissonGamma))
+        # Convert to other distribution from NegativeBinomialPoissonGamma
         @test d₂ == convert(T{Float64}, d)
-        # Convert from other distribution to NegativeBinomial2
-        @test d == convert(NegativeBinomial3{Float64}, d₂)
+        # Convert from other distribution to NegativeBinomialPoissonGamma
+        @test d == convert(NegativeBinomialPoissonGamma{Float64}, d₂)
     end
 end
 
 @testset "logpdf checks" begin
-    d = NegativeBinomial3()
+    d = NegativeBinomialPoissonGamma()
     nb = NegativeBinomial()
-    nb2 = NegativeBinomial2()
-    nb2l = NegativeBinomial2Log()
-    nb3 = NegativeBinomial3()
-    for d₂ ∈ (nb, nb2, nb2l, nb3), n ∈ (2, 10, 10^2, 10^5, 10^8)
+    nbl = NegativeBinomialLocation()
+    nbll = NegativeBinomialLogLocation()
+    nbpg = NegativeBinomialPoissonGamma()
+    for d₂ ∈ (nb, nbl, nbll, nbpg), n ∈ (2, 10, 10^2, 10^5, 10^8)
         @test logpdf(d, n) ≈ logpdf(d₂, n)
     end
 end
