28c797c4e3daa8182aee983d94f300d9f1f57d2a
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-05-05 18:48:24 -0700

    Update NegativeBinomial2Log

--- src/univariate/discrete/negativebinomial2log.jl
+++ src/univariate/discrete/negativebinomial2log.jl
@@ -1,7 +1,7 @@
 """
-Related to the location-scale parameterization of `NegativeBinomial2`, the following
-parameterization is defined in terms of the log location parameter, `η`,
-such that `NegativeBinomial2Log(η, ϕ)` = `NegativeBinomial2(exp(η), ϕ)`.
+Alternative parameterization of the negative binomial distribution in terms of
+the log location parameter `η` and overdispersion `ϕ`,
+such that `NegativeBinomialLogLocation(η, ϕ)` = `NegativeBinomialLocation(exp(η), ϕ)`.
 
 This paramerization is important in that it connects the the negative binomial distribution
 to the classic form of a Poisson GLM with offset (with logarithmic link),
@@ -9,113 +9,112 @@ the rate parameter of which is expressed as λ = μT, with T an exposure and μ 
 per unit exposure. Moreover, paramerization on the log scale enables evaluation of
 the `logpdf` as:
 
-`log(binomial(n + ϕ - 1, n)) + n * η - n * (log(exp(η) / ϕ + 1) + log(ϕ)) - ϕ * log(exp(η) / ϕ + 1)`
+`log(binomial(n + ϕ⁻¹ - 1, n)) + n * η - n * log(exp(η) * ϕ + 1)  + n * log(ϕ) - log(exp(η) * ϕ + 1) / ϕ`
 
 (note: equation for clarity, not for implementation).
 This results in fewer special function calls for each `logpdf` evaluation compared
-to the NegativeBinomial(μ, ϕ).
+to the NegativeBinomialLocation(μ, ϕ).
 
 ```julia
-NegativeBinomial2Log()        # distribution with η = 0.0 and ϕ = 1.0
-NegativeBinomial2Log(η, ϕ)    # distribution with log location η and overdispersion ϕ
+NegativeBinomialLogLocation()        # distribution with η = 0.0 and ϕ = 1.0
+NegativeBinomialLogLocation(η, ϕ)    # distribution with log location η and overdispersion ϕ
 
-convert(NegativeBinomial{T}, d)      # Parametric conversion to NegativeBinomial
-convert(NegativeBinomial2{T}, d)     # Parametric conversion to NegativeBinomial2
-convert(NegativeBinomial3{T}, d)     # Parametric conversion to NegativeBinomial3
+convert(NegativeBinomial{T}, d)                 # Parametric conversion to NegativeBinomial
+convert(NegativeBinomialLocation{T}, d)         # Parametric conversion to NegativeBinomialLocation
+convert(NegativeBinomialPoissonGamma{T}, d)     # Parametric conversion to NegativeBinomialPoissonGamma
 ```
 
 External links:
 * [Negative binomial distribution (log alternative parameterization), Stan](https://mc-stan.org/docs/2_29/functions-reference/neg-binom-2-log.html)
 
-See also: [`NegativeBinomial2`](@ref), [`NegativeBinomial3`](@ref)
+See also: [`NegativeBinomialLocation`](@ref), [`NegativeBinomialPoissonGamma`](@ref)
 """
-struct NegativeBinomial2Log{T<:Real} <: DiscreteUnivariateDistribution
+struct NegativeBinomialLogLocation{T<:Real} <: DiscreteUnivariateDistribution
     η::T
     ϕ::T
-    NegativeBinomial2Log{T}(η::T, ϕ::T) where {T<:Real} = new{T}(η, ϕ)
+    NegativeBinomialLogLocation{T}(η::T, ϕ::T) where {T<:Real} = new{T}(η, ϕ)
 end
 
-function NegativeBinomial2Log(η::T, ϕ::T; check_args::Bool=true) where {T<:Real}
-    @check_args NegativeBinomial2Log (η, η > -Inf) (ϕ, ϕ > zero(ϕ))
-    return NegativeBinomial2Log{T}(η, ϕ)
+function NegativeBinomialLogLocation(η::T, ϕ::T; check_args::Bool=true) where {T<:Real}
+    @check_args NegativeBinomialLogLocation (η, η > zero(η)) (ϕ, ϕ ≥ zero(ϕ))
+    return NegativeBinomialLogLocation{T}(η, ϕ)
 end
 
-NegativeBinomial2Log(η::Real, ϕ::Real; check_args::Bool=true) = NegativeBinomial2Log(promote(η, ϕ)...; check_args=check_args)
-NegativeBinomial2Log(η::Real; check_args::Bool=true) = NegativeBinomial2Log(η, one(η); check_args=check_args)
-NegativeBinomial2Log() = NegativeBinomial2Log{Float64}(0.0, 1.0)
+NegativeBinomialLogLocation(η::Real, ϕ::Real; check_args::Bool=true) = NegativeBinomialLogLocation(promote(η, ϕ)...; check_args=check_args)
+NegativeBinomialLogLocation(η::Integer, ϕ::Integer; check_args::Bool=true) = NegativeBinomialLogLocation(float(η), float(ϕ); check_args=check_args)
+NegativeBinomialLogLocation(η::Real; check_args::Bool=true) = NegativeBinomialLogLocation(η, one(η); check_args=check_args)
+NegativeBinomialLogLocation() = NegativeBinomialLogLocation{Float64}(1.0, 1.0)
 
-@distr_support NegativeBinomial2Log 0 Inf
+@distr_support NegativeBinomialLogLocation 0 Inf
 
-insupport(d::NegativeBinomial2Log, x::Real) = isinteger(x) && x ≥ 0
+insupport(d::NegativeBinomialLogLocation, x::Real) = isinteger(x) && x ≥ 0
 #### Conversions
 
-function convert(::Type{NegativeBinomial2Log{T}}, d::NegativeBinomial2Log) where {T<:Real}
-    NegativeBinomial2Log{T}(T(d.η), T(d.ϕ))
+function convert(::Type{NegativeBinomialLogLocation{T}}, d::NegativeBinomialLogLocation) where {T<:Real}
+    NegativeBinomialLogLocation{T}(T(d.η), T(d.ϕ))
 end
-convert(::Type{NegativeBinomial2Log{T}}, d::NegativeBinomial2Log{T}) where {T<:Real} = d
-
+convert(::Type{NegativeBinomialLogLocation{T}}, d::NegativeBinomialLogLocation{T}) where {T<:Real} = d
 
 #### Parameters
 
-params(d::NegativeBinomial2Log) = (d.η, d.ϕ)
-partype(::NegativeBinomial2Log{T}) where {T} = T
+params(d::NegativeBinomialLogLocation) = (d.η, d.ϕ)
+partype(::NegativeBinomialLogLocation{T}) where {T} = T
 
-succprob(d::NegativeBinomial2Log) = d.ϕ / (exp(d.η) + d.ϕ)
-failprob(d::NegativeBinomial2Log) = (μ = exp(d.η); μ / (μ + d.ϕ))
+succprob(d::NegativeBinomialLogLocation{T}) where {T} = inv(exp(d.η) * d.ϕ + one(T))
+failprob(d::NegativeBinomialLogLocation{T}) where {T} = inv(one(T) + inv(exp(d.η) * d.ϕ))
 
 #### Statistics
 
-mean(d::NegativeBinomial2Log) = d.η
+mean(d::NegativeBinomialLogLocation) = exp(d.η)
 
-var(d::NegativeBinomial2Log{T}) where {T} = (μ = exp(d.η); μ * (one(T) + μ / d.ϕ))
+var(d::NegativeBinomialLogLocation{T}) where {T} = (μ = exp(d.η); μ * (one(T) + μ * d.ϕ))
 
-std(d::NegativeBinomial2Log{T}) where {T} = (μ = exp(d.η); √(μ * (one(T) + μ / d.ϕ)))
+std(d::NegativeBinomialLogLocation{T}) where {T} = (μ = exp(d.η); √(μ * (one(T) + μ * d.ϕ)))
 
-skewness(d::NegativeBinomial2Log{T}) where {T} = (p = succprob(d); (T(2) - p) / sqrt((one(T) - p) * d.ϕ))
+skewness(d::NegativeBinomialLogLocation{T}) where {T} = (p = succprob(d); (T(2) - p) / sqrt((one(T) - p) * inv(d.ϕ)))
 
-kurtosis(d::NegativeBinomial2Log{T}) where {T} = (p = succprob(d); T(6) / d.ϕ + (p * p) / ((one(T) - p) * d.ϕ))
+kurtosis(d::NegativeBinomialLogLocation{T}) where {T} = (p = succprob(d); T(6) * d.ϕ + p * p * d.ϕ / (one(T) - p))
 
-mode(d::NegativeBinomial2Log{T}) where {T} = d.ϕ > one(T) ? floor(Int, exp(d.η) * (d.ϕ - one(T)) / d.ϕ) : 0
+mode(d::NegativeBinomialLogLocation) = (μ = exp(d.η); floor(Int, μ - μ * d.ϕ))
+mode(d::NegativeBinomialLogLocation{BigFloat}) = (μ = exp(d.η); floor(BigInt, μ - μ * d.ϕ))
 
 #### Evaluation & Sampling
 
-function logpdf(d::NegativeBinomial2Log, n::Real)
+function logpdf(d::NegativeBinomialLogLocation, n::Real)
     η, ϕ = params(d)
-    r = log1p(exp(η) / ϕ)
+    r = log1p(exp(η) * ϕ)
     if isone(succprob(d)) && iszero(n)
         return zero(r)
     elseif !insupport(d, n)
         return oftype(r, -Inf)
     else
-        c = loggamma(n + ϕ) - loggamma(n + 1) - loggamma(ϕ)
-        return c + n * η - n * (r + log(ϕ)) - ϕ * r
+        ϕ⁻¹ = inv(ϕ)
+        c = loggamma(n + ϕ⁻¹) - loggamma(n + 1) - loggamma(ϕ⁻¹)
+        return c + n * η - n * r + n * log(ϕ) - r / ϕ
     end
 end
 
-rand(rng::AbstractRNG, d::NegativeBinomial2Log) = rand(rng, Poisson(rand(rng, Gamma(d.ϕ, exp(d.η) / d.ϕ))))
+rand(rng::AbstractRNG, d::NegativeBinomialLogLocation) = rand(rng, Poisson(exp(d.η) * rand(rng, Gamma(inv(d.ϕ), d.ϕ))))
 
 # cdf and quantile is roundabout, but this is the most reliable approach
-cdf(d::NegativeBinomial2Log{T}, x::Real) where {T} = cdf(convert(NegativeBinomial{T}, d), x)
-ccdf(d::NegativeBinomial2Log{T}, x::Real) where {T} = ccdf(convert(NegativeBinomial{T}, d), x)
-logcdf(d::NegativeBinomial2Log{T}, x::Real) where {T} = logcdf(convert(NegativeBinomial{T}, d), x)
-logccdf(d::NegativeBinomial2Log{T}, x::Real) where {T} = logccdf(convert(NegativeBinomial{T}, d), x)
-quantile(d::NegativeBinomial2Log{T}, q::Real) where {T} = quantile(convert(NegativeBinomial{T}, d), q)
-cquantile(d::NegativeBinomial2Log{T}, q::Real) where {T} = quantile(convert(NegativeBinomial{T}, d), q)
-invlogcdf(d::NegativeBinomial2Log{T}, lq::Real) where {T} = invlogcdf(convert(NegativeBinomial{T}, d), lq)
-invlogccdf(d::NegativeBinomial2Log{T}, lq::Real) where {T} = invlogccdf(convert(NegativeBinomial{T}, d), lq)
+cdf(d::NegativeBinomialLogLocation{T}, x::Real) where {T} = cdf(convert(NegativeBinomial{T}, d), x)
+ccdf(d::NegativeBinomialLogLocation{T}, x::Real) where {T} = ccdf(convert(NegativeBinomial{T}, d), x)
+logcdf(d::NegativeBinomialLogLocation{T}, x::Real) where {T} = logcdf(convert(NegativeBinomial{T}, d), x)
+logccdf(d::NegativeBinomialLogLocation{T}, x::Real) where {T} = logccdf(convert(NegativeBinomial{T}, d), x)
+quantile(d::NegativeBinomialLogLocation{T}, q::Real) where {T} = quantile(convert(NegativeBinomial{T}, d), q)
+cquantile(d::NegativeBinomialLogLocation{T}, q::Real) where {T} = quantile(convert(NegativeBinomial{T}, d), q)
+invlogcdf(d::NegativeBinomialLogLocation{T}, lq::Real) where {T} = invlogcdf(convert(NegativeBinomial{T}, d), lq)
+invlogccdf(d::NegativeBinomialLogLocation{T}, lq::Real) where {T} = invlogccdf(convert(NegativeBinomial{T}, d), lq)
 
 
-
-function mgf(d::NegativeBinomial2Log, t::Real)
+function mgf(d::NegativeBinomialLogLocation{T}, t::Real) where {T}
     η, ϕ = params(d)
-    p = ϕ / (exp(η) + ϕ)
-    # ((1 - p) * exp(t))^ϕ / (1 - p * exp(t))^ϕ
-    ((1 - p) / (inv(exp(t)) - p))^ϕ
+    p = inv(exp(η) * ϕ + one(T))
+    ((one(T) - p) / (inv(exp(t)) - p))^inv(ϕ)
 end
 
-function cf(d::NegativeBinomial2Log, t::Real)
+function cf(d::NegativeBinomialLogLocation{T}, t::Real) where {T}
     η, ϕ = params(d)
-    p = ϕ / (exp(η) + ϕ)
-    # (((1 - p) * cis(t)) / (1 - p * cis(t)))^ϕ
-    ((1 - p) / (inv(cis(t)) - p))^ϕ
+    p = inv(exp(η) * ϕ + one(T))
+    ((one(T) - p) / (inv(cis(t)) - p))^inv(ϕ)
 end
