496dfa2af76a6a35a35aa1adbeb81a8be744b4b1
Simone Surace <simone.surace@artificialy.com>
2022-06-30 14:11:00 +0200

    Use FD for all tests, use random parameters

--- test/negativebinomial.jl
+++ test/negativebinomial.jl
@@ -6,20 +6,29 @@ using Test, ForwardDiff
 # Currently, most of the tests for NegativeBinomial are in the "ref" folder.
 # Eventually, we might want to consolidate the tests here
 
-mydiffp(r, p, k) = r/p - k/(1 - p)
-
-@testset "NegativeBinomial r=$r" for r in exp10.(range(-10, stop=2, length=25))
-    # avoid p==1 since it's not differentiable
-    @testset "p = $p" for p in exp10.(-10:0) .- eps()
-        @testset "k = $k" for k in (0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024)
-            @test ForwardDiff.derivative(_p -> logpdf(NegativeBinomial(r, _p), k), p) ≈ mydiffp(r, p, k) rtol=1e-12 atol=1e-12
-
-            dist = NegativeBinomial(r, p)
-            if p == 1. - eps()
-                # Did not get to work:
-                # test_rrule(logpdf, dist, k; fdm = backward_fdm(5, 1, max_range = r/2), rtol = 1e-5, atol = 1e-1)
-            else
-                test_rrule(logpdf, dist, k; fdm = central_fdm(5, 1, max_range = min(r, p)/2), rtol = 1e-10, atol = 1e-1)
+@testset "NegativeBinomial" begin
+    @testset "ForwardDiff and rrule" begin
+        rs = -log.(rand(25))
+        ps = rand(25)
+        ks = (0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024)
+        @testset "r=$r" for r in rs
+            @testset "p=1, k=0" begin
+                dist = NegativeBinomial(r, 1.0)
+                fdm = backward_fdm(5, 1, max_range = r/10)
+                f1(_p) = logpdf(NegativeBinomial(r, _p), 0)
+                f2(_r) = logpdf(NegativeBinomial(_r, 1.0), 0)
+                @test ForwardDiff.derivative(f1, 1.0) ≈ fdm(f1, 1.0)
+                @test ForwardDiff.derivative(f2, r) ≈ fdm(f2, r)
+                test_rrule(logpdf, dist, 0; fdm = fdm, check_inferred=true)
+            end
+            @testset "p=$p, k=$k" for p in ps, k in ks
+                dist = NegativeBinomial(r, p)
+                fdm = central_fdm(5, 1, max_range = min(r, p)/2)
+                f1(_p) = logpdf(NegativeBinomial(r, _p), k)
+                f2(_r) = logpdf(NegativeBinomial(_r, p), k)
+                @test ForwardDiff.derivative(f1, p) ≈ fdm(f1, p)
+                @test ForwardDiff.derivative(f2, r) ≈ fdm(f2, r)
+                test_rrule(logpdf, dist, k; fdm = fdm, check_inferred=true)
             end
         end
     end
@@ -28,8 +37,4 @@ end
 @testset "Check the corner case p==1" begin
     @test logpdf(NegativeBinomial(0.5, 1.0), 0) === 0.0
     @test logpdf(NegativeBinomial(0.5, 1.0), 1) === -Inf
-
-    @testset "r=$r" for r in exp10.(range(-10, stop=2, length=25))
-        test_rrule(logpdf, NegativeBinomial(r, 1.0), 0; fdm = backward_fdm(5, 1, max_range = r/2), rtol = 1e-8, atol = 1e-4)
-    end
 end
