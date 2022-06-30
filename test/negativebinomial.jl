d0b1257929202bb96e08ebf187880537d4a8e2b8
Simone Carlo Surace <51025924+simsurace@users.noreply.github.com>
2022-07-01 01:11:24 +0200

    Draw multiple samples
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- test/negativebinomial.jl
+++ test/negativebinomial.jl
@@ -17,5 +17,5 @@ end
 @testset "Check the corner case p==1" begin
     @test logpdf(NegativeBinomial(0.5, 1.0), 0) === 0.0
     @test logpdf(NegativeBinomial(0.5, 1.0), 1) === -Inf
-    @test iszero(rand(NegativeBinomial(0.5, 1.0)))
+    @test all(iszero, rand(NegativeBinomial(rand(), 1.0), 10))
 end
