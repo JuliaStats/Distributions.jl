254f27f1b32a72d6529e2f78a3745b68bd38b31e
David Widmann <devmotion@users.noreply.github.com>
2022-07-02 21:48:33 +0200

    Include and update tests for `BetaBinomial` (#1578)

--- test/betabinomial.jl
+++ test/betabinomial.jl
@@ -2,12 +2,12 @@ using Distributions
 using Test
 
 @testset "Log of Beta-binomial distribution" begin
-    d = BetaBinomial(50, 0.2,0.6)
+    d = BetaBinomial(50, 0.2, 0.6)
 
-    for k in Base.OneTo(50)
+    for k in 1:50
         p  = pdf(d, k)
         lp = logpdf(d, k)
-        @test_approx_eq lp log(p)
+        @test lp ≈ log(p)
         @test insupport(d, k)
     end
     @test !insupport(d, 51)
