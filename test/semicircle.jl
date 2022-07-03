53c395138f5596e9497bc102597320b29b2bb291
Jan Weidner <jw3126@gmail.com>
2022-07-03 20:22:15 +0200

    add semicircle rand tests

--- test/semicircle.jl
+++ test/semicircle.jl
@@ -1,4 +1,5 @@
 using Distributions
+using Random: MersenneTwister
 using  Test
 
 d = Semicircle(2.0)
@@ -37,3 +38,36 @@ d = Semicircle(2.0)
 @test quantile(d,  .0) == -2.0
 @test quantile(d,  .5) ==   .0
 @test quantile(d, 1.0) == +2.0
+
+function test_sample_uniform(rng,sample)
+    N = length(sample)
+    sample = sort(sample)
+    ks = rand(rng,eachindex(sample), 20)
+    ifirst = firstindex(sample)
+    ilast = lastindex(sample)
+    imid = round(Int,(ifirst + ilast) / 2)
+    push!(ks, ifirst)
+    push!(ks, ilast)
+    push!(ks, imid)
+    for k in ks
+        # test k-th order statistic of sample is sane
+        dk = Beta(k, N+1-k)
+        lo = quantile(dk, 0.001)
+        hi = quantile(dk, 0.999)
+        x = sample[k]
+        @test lo < x < hi
+    end
+    # test sample mean compatible with central limit theorem
+    dmean = Normal(0.5, std(Uniform(0,1))/√(N))
+    @test quantile(dmean, 0.01) < mean(sample) < quantile(dmean, 0.99)
+end
+
+N = 10^3
+rng = MersenneTwister(0)
+for r in rand(rng, Uniform(0,10), 5)
+    semi = Semicircle(r)
+    sample = rand(rng, semi, N) 
+    mi, ma = extrema(sample)
+    @test -r <= mi < ma <= r
+    test_sample_uniform(rng,cdf.(semi, sample))
+end
