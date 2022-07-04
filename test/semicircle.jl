fcec8436538a0c131d2228cd3fd5e6906282a413
Jan Weidner <jw3126@gmail.com>
2022-07-04 07:56:27 +0200

    improve semicirle rand tests

--- test/semicircle.jl
+++ test/semicircle.jl
@@ -1,6 +1,6 @@
 using Distributions
 using Random: MersenneTwister
-using  Test
+using Test
 
 d = Semicircle(2.0)
 
@@ -39,35 +39,30 @@ d = Semicircle(2.0)
 @test quantile(d,  .5) ==   .0
 @test quantile(d, 1.0) == +2.0
 
-function test_sample_uniform(rng,sample)
-    N = length(sample)
-    sample = sort(sample)
-    ks = rand(rng,eachindex(sample), 20)
-    ifirst = firstindex(sample)
-    ilast = lastindex(sample)
-    imid = round(Int,(ifirst + ilast) / 2)
-    push!(ks, ifirst)
-    push!(ks, ilast)
-    push!(ks, imid)
-    for k in ks
-        # test k-th order statistic of sample is sane
-        dk = Beta(k, N+1-k)
-        lo = quantile(dk, 0.001)
-        hi = quantile(dk, 0.999)
-        x = sample[k]
-        @test lo < x < hi
-    end
-    # test sample mean compatible with central limit theorem
-    dmean = Normal(0.5, std(Uniform(0,1))/√(N))
-    @test quantile(dmean, 0.01) < mean(sample) < quantile(dmean, 0.99)
-end
-
-N = 10^3
 rng = MersenneTwister(0)
 for r in rand(rng, Uniform(0,10), 5)
+    N = 10^4
     semi = Semicircle(r)
-    sample = rand(rng, semi, N) 
+    sample = rand(rng, semi, N)
     mi, ma = extrema(sample)
     @test -r <= mi < ma <= r
-    test_sample_uniform(rng,cdf.(semi, sample))
+
+    # test order statistic of sample min is sane
+    d_min = Beta(1, N)
+    lo = quantile(d_min, 0.01)
+    hi = quantile(d_min, 0.99)
+    @test lo < cdf(semi, mi) < hi
+
+    # test order statistic of sample max is sane
+    d_max = Beta(N, 1)
+    lo = quantile(d_max, 0.01)
+    hi = quantile(d_max, 0.99)
+    @test lo < cdf(semi, ma) < hi
+
+    # central limit theorem
+    dmean = Normal(mean(semi), std(semi)/√(N))
+    @test quantile(dmean, 0.01) < mean(sample) < quantile(dmean, 0.99)
+
+    pvalue = pvalue_kolmogorovsmirnoff(sample, semi)
+    @test pvalue > 1e-2
 end
