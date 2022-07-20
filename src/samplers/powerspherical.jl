ba9ff6be0498f90d7064bc4ccc9953aebc95cd63
Marc Pabst <2624210+marcpabst@users.noreply.github.com>
2022-07-20 18:46:06 +0200

    Update src/samplers/powerspherical.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/samplers/powerspherical.jl
+++ src/samplers/powerspherical.jl
@@ -25,20 +25,4 @@ function _rand!(rng::AbstractRNG, spl::PowerSphericalSampler, x::AbstractVector)
     x .= (-1) * (I(spl.d) .- 2*x*x') * y
 end
 
-function rand(rng::AbstractRNG, spl::PowerSphericalSampler)
-    z = rand(rng, spl.dist_b)
-    v = rand(rng, spl.dist_u)
-
-    t = 2 * z - 1
-    m = sqrt(1 - t ^ 2) * v'
-
-    y = [t; m]
-    e_1 = [1.; zeros(eltype(spl.μ), spl.d -1)]
-
-    û = e_1 - spl.μ
-    u = normalize(û)
-
-    return (-1) * (I(spl.d) .- 2*u*u') * y
-end
-
 Base.length(s::PowerSphericalSampler) = length(s.μ)
