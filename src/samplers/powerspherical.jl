0808adb963809668337477bd5d6743b80962649c
Marc Pabst <github@marcpabst.com>
2022-07-20 18:46:10 +0200

    removed rand()

--- src/samplers/powerspherical.jl
+++ src/samplers/powerspherical.jl
@@ -3,7 +3,6 @@
 struct PowerSphericalSampler{T <: Real} <: Sampleable{Multivariate,Continuous}
     μ::Vector{T}
     κ::T
-    d::Int
     dist_b::Beta
     dist_u::HyperSphericalUniform
 end
@@ -22,23 +21,7 @@ function _rand!(rng::AbstractRNG, spl::PowerSphericalSampler, x::AbstractVector)
 
     normalize!(x)
 
-    x .= (-1) * (I(spl.d) .- 2*x*x') * y
-end
-
-function rand(rng::AbstractRNG, spl::PowerSphericalSampler)
-    z = rand(rng, spl.dist_b)
-    v = rand(rng, spl.dist_u)
-
-    t = 2 * z - 1
-    m = sqrt(1 - t ^ 2) * v'
-
-    y = [t; m]
-    e_1 = [1.; zeros(eltype(spl.μ), spl.d -1)]
-
-    û = e_1 - spl.μ
-    u = normalize(û)
-
-    return (-1) * (I(spl.d) .- 2*u*u') * y
+    x .= (-1) * (I(length(spl)) .- 2*x*x') * y
 end
 
 Base.length(s::PowerSphericalSampler) = length(s.μ)
