2c75061a1e8f9d8f60e6393801b4f72e32ab0d74
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-07-31 21:01:05 +0200

    fix op

--- src/multivariate/mvnormal.jl
+++ src/multivariate/mvnormal.jl
@@ -529,7 +529,7 @@ function ChainRulesCore.rrule(::typeof(mvnormal_c0), d::MvNormal)
     y = mvnormal_c0(d)
     function mvnormal_c0_pullback(dy)
         dy = ChainRulesCore.unthunk(dy)
-        ∂Σ = (dy / (-2)) / _cov(d)
+        ∂Σ = _cov(d) \ (dy / (-2))
         ∂d = ChainRulesCore.Tangent{typeof(d)}(Σ = ∂Σ)
         return ChainRulesCore.NoTangent(), ∂d
     end
