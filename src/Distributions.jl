aca39200d7e2288b571895a09d8e0ec34b8dcf83
J S <49557684+svilupp@users.noreply.github.com>
2022-08-14 01:36:52 +0100

    Fix mini typos in docs (#1600)
    * Fixes as per Grammarly and Google check
    
    * Fixes as per Google + Grammarly
    
    * Fixes as per Google + Grammarly
    
    * Fixed as per Google + Grammarly
    
    * `subtype` as its more popular
    
    * Fixes as per Grammarly and Google
    
    * Fixes as per Grammarly
    
    * Fixed as per Wikipedia term
    
    * Alignment to in-place form as per Wikipedia
    
    * Alignment to `logscale` form
    
    * Alignment to in-place form as per Wikipedia
    
    * Alignment to `log scale` as per Wikipedia
    
    * Update README.md
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>
    
    * Update docs/src/multivariate.md
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>
    
    * Update src/univariate/continuous/skewedexponentialpower.jl
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/Distributions.jl
+++ src/Distributions.jl
@@ -220,7 +220,7 @@ export
 
     invscale,           # Inverse scale parameter
     sqmahal,            # squared Mahalanobis distance to Gaussian center
-    sqmahal!,           # inplace evaluation of sqmahal
+    sqmahal!,           # in-place evaluation of sqmahal
     location,           # get the location parameter
     location!,          # provide storage for the location parameter (used in multivariate distribution mvlognormal)
     mean,               # mean of distribution
