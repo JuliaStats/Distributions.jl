a121d32aea922f984865acaa9da87b44901d0d5d
Jan Weidner <jw3126@gmail.com>
2022-08-15 23:47:06 +0200

    Update test/univariate/discrete/geometric.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- test/univariate/discrete/geometric.jl
+++ test/univariate/discrete/geometric.jl
@@ -1,11 +1,13 @@
 using Distributions
 using Test
-using ForwardDiff
+using FiniteDifferences
 
 @testset "Geometric mgf and k vs k-1 parametrization #1604" begin
     d = Geometric(0.2)
-    @test mgf(d, 0) ≈ 1 atol = 10eps(Float64) rtol=0
-    @test cf(d, 0)  ≈ 1 atol = 10eps(Float64) rtol=0
-    @test ForwardDiff.derivative(Base.Fix1(mgf, d), 0) ≈ mean(d)
-    @test ForwardDiff.derivative(Base.Fix1(cf, d), 0) ≈ mean(d) * im
+    @test mgf(d, 0) == 1
+    @test cf(d, 0) == 1
+
+    fdm = central_fdm(5, 1)
+    @test fdm(Base.Fix1(mgf, d), 0) ≈ mean(d)
+    @test fdm(Base.Fix1(cf, d), 0) ≈ mean(d) * im
 end
