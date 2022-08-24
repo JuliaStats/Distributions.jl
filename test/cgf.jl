dab418bd72af4b22415043b7d0f7fb5b8b70ff22
Jan Weidner <jw3126@gmail.com>
2022-08-24 20:32:12 +0200

    simplify cgf tests

--- test/cgf.jl
+++ test/cgf.jl
@@ -5,38 +5,35 @@ import ForwardDiff
 
 @testset "CumulantGeneratingFunctions.jl" begin
     d(f) = Base.Fix1(ForwardDiff.derivative, f)
-    mk(dist, ts; compare_logmf=true) = (;dist=dist, ts=ts, compare_logmf)
-
-    @testset "$(item.dist)" for item in [
-        mk(Dirac(13),               Any[1, 1f-4, 1e10, 10, -4]),
-        mk(Dirac(-1f2),             Any[1, 1f-4, 1e10, 10,-4]),
-        mk(Bernoulli(0.5),           Any[1f0, -1f0,1e6, -1e6]),
-        mk(Bernoulli(0.1),           Any[1f0, -1f0,1e6, -1e6]),
-        mk(Binomial(10,0.1),         Any[1f0, -1f0,1e6, -1e6]),
-        mk(Binomial(100,1e-3),       Any[1f0, -1f0,1e6, -1e6]),
-        mk(Geometric(0.1),           Any[1f-1, -1e6]           ),
-        mk(Geometric(0.5),           Any[1f-1, -1e6]           ),
-        mk(Geometric(0.5),           Any[1f-1, -1e6]           ),
-        mk(NegativeBinomial(10,0.5), Any[-1f0, -200.0,-1e6]  , compare_logmf=false),
-        mk(NegativeBinomial(3,0.1), Any[-1f0, -200.0,-1e6]  , compare_logmf=false),
-        mk(Poisson(1),               Any[1f0,2f0,10.0,50.0]),
-        mk(Poisson(10),               Any[1f0,2f0,10.0,50.0]),
-        mk(Poisson(1e-3),               Any[1f0,2f0,10.0,50.0]),
-        mk(Uniform(0,1),               Any[1, -1, 100f0, 1e6, -1e6]),
-        mk(Normal(0,1),               Any[1, -1, 100f0, 1e6, -1e6]),
-        mk(Normal(1,0.4),               Any[1, -1, 100f0, 1e6, -1e6]),
-        mk(Exponential(1),               Any[0.9, -1, -100f0, -1e6]),
-        mk(Exponential(0.91),               Any[0.9, -1, -100f0, -1e6]),
-        mk(Exponential(10),           Any[0.08, -1, -100f0, -1e6]),
-        mk(Gamma(1,1),               Any[0.9, -1, -100f0, -1e6]),
-        mk(Gamma(10,1),               Any[0.9, -1, -100f0, -1e6]),
-        mk(Gamma(0.2, 10),           Any[0.08, -1, -100f0, -1e6]),
-        mk(Laplace(1, 1),           Any[0.99, -0.99, 1f-2, -1f-5]),
-        mk(Chisq(1),           Any[0.49, -1, -100, -1f6]),
-        mk(Chisq(3),           Any[0.49, -1, -100, -1f6]),
-        mk(NoncentralChisq(3,2),           Any[0.49, -1, -100, -1f6]),
+    @testset "$(dist)" for (dist, ts) in [
+        (Dirac(13),                Any[1, 1f-4, 1e10, 10, -4]),
+        (Dirac(-1f2),              Any[1, 1f-4, 1e10, 10,-4]),
+        (Bernoulli(0.5),           Any[1f0, -1f0,1e6, -1e6]),
+        (Bernoulli(0.1),           Any[1f0, -1f0,1e6, -1e6]),
+        (Binomial(10,0.1),         Any[1f0, -1f0,1e6, -1e6]),
+        (Binomial(100,1e-3),       Any[1f0, -1f0,1e6, -1e6]),
+        (Geometric(0.1),           Any[1f-1, -1e6]           ),
+        (Geometric(0.5),           Any[1f-1, -1e6]           ),
+        (Geometric(0.5),           Any[1f-1, -1e6]           ),
+        (NegativeBinomial(10,0.5), Any[-1f0, -200.0,-1e6]),
+        (NegativeBinomial(3,0.1),  Any[-1f0, -200.0,-1e6] ),
+        (Poisson(1),               Any[1f0,2f0,10.0,50.0]),
+        (Poisson(10),              Any[1f0,2f0,10.0,50.0]),
+        (Poisson(1e-3),            Any[1f0,2f0,10.0,50.0]),
+        (Uniform(0,1),             Any[1, -1, 100f0, 1e6, -1e6]),
+        (Normal(0,1),              Any[1, -1, 100f0, 1e6, -1e6]),
+        (Normal(1,0.4),            Any[1, -1, 100f0, 1e6, -1e6]),
+        (Exponential(1),           Any[0.9, -1, -100f0, -1e6]),
+        (Exponential(0.91),        Any[0.9, -1, -100f0, -1e6]),
+        (Exponential(10),          Any[0.08, -1, -100f0, -1e6]),
+        (Gamma(1,1),               Any[0.9, -1, -100f0, -1e6]),
+        (Gamma(10,1),              Any[0.9, -1, -100f0, -1e6]),
+        (Gamma(0.2, 10),           Any[0.08, -1, -100f0, -1e6]),
+        (Laplace(1, 1),            Any[0.99, -0.99, 1f-2, -1f-5]),
+        (Chisq(1),                 Any[0.49, -1, -100, -1f6]),
+        (Chisq(3),                 Any[0.49, -1, -100, -1f6]),
+        (NoncentralChisq(3,2),     Any[0.49, -1, -100, -1f6]),
                    ]
-        dist = item.dist
         κ₀ = cgf(dist, 0)
         @test κ₀ ≈ 0 atol=2*eps(one(float(κ₀)))
         κ₁ = d(Base.Fix1(cgf, dist))(0)
@@ -44,10 +41,10 @@ import ForwardDiff
         κ₂ = d(d(Base.Fix1(cgf, dist)))(0)
         @test κ₂ ≈ var(dist)
         
-        for t in item.ts
+        for t in ts
             val = @inferred cgf(dist, t)
             @test isfinite(val)
-            if item.compare_logmf && isfinite(mgf(dist, t))
+            if isfinite(mgf(dist, t))
                 rtol = eps(float(one(t)))^(1/2)
                 @test (exp∘cgf)(dist, t) ≈ mgf(dist, t) rtol=rtol
             end
