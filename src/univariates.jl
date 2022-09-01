c8674cbdc76e5e19ef6369269f6b99b9aa5c315d
David Widmann <devmotion@users.noreply.github.com>
2022-09-02 00:39:01 +0200

    Improve docstrings

--- src/univariates.jl
+++ src/univariates.jl
@@ -270,16 +270,20 @@ proper_kurtosis(d::Distribution) = kurtosis(d, false)
 """
     mgf(d::UnivariateDistribution, t)
 
-Evaluate the moment generating function of distribution `d`.
+Evaluate the [moment-generating function](https://en.wikipedia.org/wiki/Moment-generating_function) of distribution `d` at `t`.
+
+See also [`cgf`](@ref)
 """
 mgf(d::UnivariateDistribution, t)
 
 """
     cgf(d::UnivariateDistribution, t)
 
-Evaluate the [cumulant-generating-function](https://en.wikipedia.org/wiki/Cumulant) of `distribution` at `t`.
-Mathematically the cumulant-generating-function is the logarithm of the [moment-generating-function](https://en.wikipedia.org/wiki/Moment-generating_function):
-`cgf = log ∘ mgf`. In practice, however, the right hand side may have overflow issues.
+Evaluate the [cumulant-generating function](https://en.wikipedia.org/wiki/Cumulant) of distribution `d` at `t`.
+
+The cumulant-generating-function is the logarithm of the [moment-generating function](https://en.wikipedia.org/wiki/Moment-generating_function):
+`cgf = log ∘ mgf`.
+In practice, however, the right hand side may have overflow issues.
 
 See also [`mgf`](@ref)
 """
