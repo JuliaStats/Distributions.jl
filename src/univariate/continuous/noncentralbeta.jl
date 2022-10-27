529003860da12b40c17aeda85483ae4a6e2e44da
Rik Huijzer <t.h.huijzer@rug.nl>
2022-10-27 09:30:11 +0200

    Document parameters for noncentral distributions (#1632)
    * Document parameters for noncentral distributions
    
    * Apply suggestions from code review
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/univariate/continuous/noncentralbeta.jl
+++ src/univariate/continuous/noncentralbeta.jl
@@ -1,5 +1,7 @@
 """
     NoncentralBeta(α, β, λ)
+
+*Noncentral Beta distribution* with shape parameters `α > 0` and `β > 0` and noncentrality parameter `λ >= 0`.
 """
 struct NoncentralBeta{T<:Real} <: ContinuousUnivariateDistribution
     α::T
