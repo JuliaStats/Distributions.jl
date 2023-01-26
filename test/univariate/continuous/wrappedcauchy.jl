f61de9d98fab7c78784d0ab3cccf378dc99b78ad
Jeremy Worsfold <jeremyworsfold2@gmail.com>
2023-01-26 15:14:02 +0000

    wrapped cauchy tests

--- test/univariate/continuous/wrappedcauchy.jl
+++ test/univariate/continuous/wrappedcauchy.jl
@@ -2,35 +2,23 @@ using Distributions
 using Random: MersenneTwister
 using Test
 
-@testset "wrappedcauchy.jl" begin
-    d = WrappedCauchy(0.6)
+d = WrappedCauchy(0.6)
 
-    @test params(d) == (0.6,)
+@test mean(d)     ==  .0
+@test var(d)      == 0.4
+@test skewness(d) ==  .0
+@test median(d)   ==  .0
+@test mode(d)     ==  .0
+@test entropy(d)  == 1.391589963780926
 
-    @test minimum(d) == -oftype(d.r,π)
-    @test maximum(d) == oftype(d.r,π)
-    @test extrema(d) == (-oftype(d.r,π), oftype(d.r,π))
+@test pdf(d, -5) == .0
+@test pdf(d, 2π) == .0
+@test pdf(d,  0) == 0.6366197723675816
 
-    @test mean(d)     ==  .0
-    @test var(d)      == 0.4
-    @test skewness(d) ==  .0
-    @test median(d)   ==  .0
-    @test mode(d)     ==  .0
-    @test entropy(d)  == 1.33787706640934544458
+@test logpdf(d, -5) == -Inf
+@test logpdf(d,  0) ≈  log(0.6366197723675816)
+@test logpdf(d, +5) == -Inf
 
-    @test pdf(d, -10) == .0
-    @test pdf(d, 2π) == .0
-    #@test pdf(d,  0) == .31830988618379067154
-
-    @test logpdf(d, -10) == -Inf
-    @test logpdf(d, 2π) == -Inf
-    #@test logpdf(d,  0) ≈  log(.31830988618379067154)
-
-    @test cdf(d, -10) ==  .0
-    @test cdf(d, 2π) ==  1.0
-    @test cdf(d,  0) ==  .5
-
-    @test quantile(d,  .0) == -oftype(d.r,π)
-    @test quantile(d,  .5) ==   .0
-    @test quantile(d, 1.0) == +oftype(d.r,π)
-end
+@test cdf(d, -5) ==  .0
+@test cdf(d,  0) ==  .5
+@test cdf(d, +5) == 1.0
