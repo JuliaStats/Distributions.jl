5255826f54608cd4dfe6ea6aa9173104c1191669
Jeremy Worsfold <jeremyworsfold2@gmail.com>
2023-01-28 12:27:48 +0000

    mle for wrapped cauchy

--- src/univariate/continuous/wrappedcauchy.jl
+++ src/univariate/continuous/wrappedcauchy.jl
@@ -64,7 +64,7 @@ entropy(d::WrappedCauchy) = log2π + log1p(-d.r^2)
 
 cf(d::WrappedCauchy, t::Real) = cis(t * d.μ - abs(t) * log(d.r) * im)
 
-
+#### Evaluation
 
 function pdf(d::WrappedCauchy, x::Real)
     μ, r = params(d)
@@ -82,7 +82,7 @@ function cdf(d::WrappedCauchy, x::Real)
     μ, r = params(d)
     min_d, max_d = extrema(d)
     c = (one(r) + r) / (one(r) - r)
-    res = (atan(c*tan((x-μ)/2)) - atan(c*cot(μ/2))) / π
+    res = (atan(c * tan((x - μ) / 2)) - atan(c * cot(μ / 2))) / π
     return if x ≤ min_d
         zero(res)
     elseif x ≥ max_d
@@ -94,7 +94,52 @@ function cdf(d::WrappedCauchy, x::Real)
     end
 end
 
+#### Sampling
+
 function rand(rng::AbstractRNG, d::WrappedCauchy)
     return mod2pi(d.μ + log(d.r) * tan(π * (rand(rng) - 0.5))) - π
 
+end
+
+#### Fitting
+
+function _WC_mle_update(x::AbstractArray{T}, μ1, μ2) where {T <: Real}
+    n = length(x)
+    sc = zero(T)
+    sw = zero(T)
+    ss = zero(T)
+    for i = 1:n
+        @inbounds xi = x[i]
+        wi = 1/(1 - μ1*cos(xi) - μ2*sin(xi))
+        ss += wi*sin(xi)
+        sc += wi*cos(xi)
+        sw += wi
+    end
+    return sc / sw, ss / sw
+end
+
+
+function fit_mle(::Type{<:WrappedCauchy},
+    x::AbstractArray{T};
+    mu0::Float64=NaN, r0::Float64=NaN, maxiter::Int=1000, tol::Float64=1e-16
+) where {T <: Real}
+
+    μ::Float64 = isnan(mu0) ? angle(mean(exp.(im*x))) : mu0
+    r::Float64 = isnan(mu0) ? 0.5 : r0
+    converged = false
+
+    μ1 = 2*r*cos(μ)/(1+r^2)
+    μ2 = 2*r*sin(μ)/(1+r^2)
+
+    t = 0
+    while !converged && t < maxiter
+        t += 1
+        μ1_old, μ2_old = μ1, μ2
+        μ1, μ2 = _WC_mle_update(x, μ1, μ2)
+        converged = (abs(μ1 - μ1_old) <= tol && abs(μ2 - μ2_old) <= tol)
+    end
+    μ = mod2pi(atan(μ2,μ1)) - π
+    r = (1 - sqrt(1-μ1^2-μ2^2))/sqrt(μ1^2+μ2^2)
+
+    WrappedCauchy{T}(μ, r)
 end
