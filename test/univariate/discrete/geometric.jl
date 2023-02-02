c431d20d3a0f00e0cf22f9fd959b9bb11d978517
Kristoffer Carlsson <kcarlsson89@gmail.com>
2023-02-02 18:23:04 +0100

    change some floating point equality checks to approximate (#1671)

--- test/univariate/discrete/geometric.jl
+++ test/univariate/discrete/geometric.jl
@@ -4,8 +4,8 @@ using FiniteDifferences
 
 @testset "Geometric mgf and k vs k-1 parametrization #1604" begin
     d = Geometric(0.2)
-    @test mgf(d, 0) == 1
-    @test cf(d, 0) == 1
+    @test mgf(d, 0) ≈ 1
+    @test cf(d, 0) ≈ 1
 
     fdm1 = central_fdm(5, 1)
     @test fdm1(Base.Fix1(mgf, d), 0) ≈ mean(d)
