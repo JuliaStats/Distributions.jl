460a312e8c3e009fde3315a8ffc5d33f47caf73d
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2023-03-06 20:49:22 +0800

    Apply suggestions from code review
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/univariate/continuous/johnsonsu.jl
+++ src/univariate/continuous/johnsonsu.jl
@@ -3,17 +3,11 @@
 
 The Johnson's ``S_U``-distribution with parameters ξ, λ, γ and δ is a transformation of the normal distribution:
 
+If
 ```math
-z = \\gamma + \\delta \\sinh^{-1}\\Big(\\frac{x-\\xi}{\\lambda}\\Big),
+X = \\lambda\\sinh\\Bigg( \\frac{Z - \\gamma}{\\delta} \\Bigg) + \\xi
 ```
-
-where ``z \\sim \\mathcal{N}(0,1)`` i.e. if a random variable ``X`` is defined to be
-
-```math
-X = \\lambda\\sinh\\Bigg( \\frac{Z - \\gamma}{\\delta} \\Bigg) + \\xi,
-```
-
-where ``Z \\sim \\mathcal{N}(0,1)``,  then ``X \\sim {\\rm Johnson}(\\xi, \\lambda, \\gamma, \\delta)``.
+where ``Z \\sim \\mathcal{N}(0,1)``, then ``X \\sim \\operatorname{Johnson}(\\xi, \\lambda, \\gamma, \\delta)``.
 
 ```julia
 JohnsonSU()           # Equivalent to JohnsonSU(0, 1, 0, 1)
@@ -78,7 +72,8 @@ end
 function skewness(d::JohnsonSU)
     a = exp(d.δ^-2)
     r = d.γ/d.δ
-    - (d.λ^3 * sqrt(a) * (a-1)^2 * (a*(a+2)*sinh(3r)+3sinh(2r))) / 4sqrt(var(d)^3)
+    var_d = var(d)
+    - (d.λ^3 * sqrt(a) * (a-1)^2 * (a*(a+2)*sinh(3r)+3sinh(2r))) / (4 * var_d * sqrt(var_d)))
 end
 function kurtosis(d::JohnsonSU)
     a = exp(d.δ^-2)
@@ -95,8 +90,8 @@ yval(d::JohnsonSU, x::Real) = (x - d.ξ) / d.λ
 zval(d::JohnsonSU, x::Real) = d.γ + d.δ * asinh(yval(d, x))
 xval(d::JohnsonSU, x::Real) = d.λ * sinh((x - d.γ) / d.δ) + d.ξ
 
-pdf(d::JohnsonSU, x::Real) = d.δ / d.λ / sqrt(1 + yval(d, x)^2) * normpdf(zval(d, x))
-logpdf(d::JohnsonSU, x::Real) = log(d.δ) - log(d.λ) - 1/2log(1 + yval(d, x)^2) + normlogpdf(zval(d, x))
+pdf(d::JohnsonSU, x::Real) = d.δ / (d.λ * hypot(1, yval(d, x))) * normpdf(zval(d, x))
+logpdf(d::JohnsonSU, x::Real) = log(d.δ) - log(d.λ) - log1psq(yval(d, x)) / 2 + normlogpdf(zval(d, x))
 cdf(d::JohnsonSU, x::Real) = normcdf(zval(d, x))
 logcdf(d::JohnsonSU, x::Real) = normlogcdf(zval(d, x))
 ccdf(d::JohnsonSU, x::Real) = normccdf(zval(d, x))
