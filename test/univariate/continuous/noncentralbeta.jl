e84ae2996df96919f3059adc04ab11ffdc62547f
Arun <74652697+ArunSanganal@users.noreply.github.com>
2023-03-24 02:28:22 +0530

    updated test cases

--- test/univariate/continuous/noncentralbeta.jl
+++ test/univariate/continuous/noncentralbeta.jl
@@ -7,4 +7,9 @@ using Test
     d = NoncentralBeta(α, β, λ)
     @test isapprox(mean(d), 0.4466, atol=1e-4)
     @test isapprox(var(d), 0.0416, atol=1e-4)
+    λ = 0.0
+    d = NoncentralBeta(α, β, λ)
+    d1 = Beta(α, β)
+    @test isapprox(mean(d), mean(d1))
+    @test isapprox(var(d), var(d1))
 end
