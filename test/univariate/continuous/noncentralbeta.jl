0cc89bd61d368a43acf22638ade4a7a5a0f084d9
Arun <74652697+ArunSanganal@users.noreply.github.com>
2023-03-24 00:29:39 +0530

    commit suggestion.
    This reverts commit f2735babf1330bae1fa3e82d189346aca7fd813f.

--- test/univariate/continuous/noncentralbeta.jl
+++ test/univariate/continuous/noncentralbeta.jl
@@ -1,7 +1,8 @@
 using Distributions
 using Test
 
-@testset "NoncentralBeta" begin
+
+let 
     α = 2.0; β = 3.0; λ = 1.0
     d = NoncentralBeta(α, β, λ)
     @test isapprox(mean(d), 0.4466, atol=1e-4)
