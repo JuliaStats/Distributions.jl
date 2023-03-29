3d616250c8de89df9db634ac46d2fec61a64e949
Arun <74652697+ArunSanganal@users.noreply.github.com>
2023-03-29 15:36:48 +0530

    updates test and added compat

--- test/univariate/continuous/noncentralbeta.jl
+++ test/univariate/continuous/noncentralbeta.jl
@@ -3,10 +3,10 @@ using Test
 
 
 @testset "NoncentralBeta" begin 
-    α = 2.0; β = 3.0; λ = 1.0
+    α = 11.0 ; β = 8.0; λ = 5.0
     d = NoncentralBeta(α, β, λ)
-    @test isapprox(mean(d), 0.4466, atol=1e-4)
-    @test isapprox(var(d), 0.0416, atol=1e-4)
+    @test isapprox(mean(d), 0.6259, atol=1e-4)
+    @test isapprox(var(d), 0.0111, atol=1e-4)
     λ = 0.0
     d = NoncentralBeta(α, β, λ)
     d1 = Beta(α, β)
