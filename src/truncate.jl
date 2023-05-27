c9caac955131b56b1e82b9ff035673b96544dbc1
Alex Arslan <ararslan@comcast.net>
2023-05-26 17:26:11 -0700

    Discrete probability is my nemesis, apparently
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/truncate.jl
+++ src/truncate.jl
@@ -72,7 +72,11 @@ function truncated(d::UnivariateDistribution, l::T, u::T) where {T <: Real}
     ucdf = exp(logucdf)
 
     # (log)tp = (log) P(l ≤ X ≤ u) where X ∼ d
-    logtp = logdiffcdf(d, u, l)
+    logtp = if d isa DiscreteUnivariateDistribution
+        logaddexp(logdiffcdf(d, u, l), logpdf(d, l))
+    else
+        logdiffcdf(d, u, l)
+    end    
     tp = exp(logtp)
 
     Truncated(d, promote(l, u, loglcdf, lcdf, ucdf, tp, logtp)...)
