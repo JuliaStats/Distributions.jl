3a06e2e9d6f4efcda6798d595c15850536d169e4
spaette <111918424+spaette@users.noreply.github.com>
2023-07-31 17:24:47 -0500

    typos (#1754)

--- src/common.jl
+++ src/common.jl
@@ -150,7 +150,7 @@ end
 
 `Distribution` is a `Sampleable` generating random values from a probability
 distribution. Distributions define a Probability Distribution Function (PDF)
-to implement with `pdf` and a Cumulated Distribution Function (CDF) to implement
+to implement with `pdf` and a Cumulative Distribution Function (CDF) to implement
 with `cdf`.
 """
 abstract type Distribution{F<:VariateForm,S<:ValueSupport} <: Sampleable{F,S} end
