025c8a8a1825a97f60f0abb1fa145bf234c8fd2e
Jack Li <yli566@ucsc.edu>
2023-10-18 21:16:11 -0700

    remove tests with a single argument

--- test/univariate/continuous/loglogistic.jl
+++ test/univariate/continuous/loglogistic.jl
@@ -5,15 +5,12 @@ using Test
 
     @test round(pdf(LogLogistic(), -1), digits=6) == 0.0
     @test round(pdf(LogLogistic(), 1), digits=6) == 0.25
-    @test round(pdf(LogLogistic(2), 2), digits=6) == 0.125
     @test round(pdf(LogLogistic(2,2), 1), digits=6) == 0.32
 
     @test round(cdf(LogLogistic(), -1), digits=6) == 0.0
     @test round(cdf(LogLogistic(), 1), digits=6) == 0.5
-    @test round(cdf(LogLogistic(2), 3), digits=6) == 0.6
     @test round(cdf(LogLogistic(2,2), 4), digits=6) == 0.8
 
-    @test round(ccdf(LogLogistic(2), 3), digits=6) == 0.4
     @test round(ccdf(LogLogistic(2,2), 4), digits=6) == 0.2
 
     @test round(logpdf(LogLogistic(), -1), digits=6) == -Inf
