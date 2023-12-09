038445642f4d496b8dab60b2af2114cdefdb022c
Alexander Plavin <alexander@plav.in>
2023-12-09 18:48:31 -0500

    support intervals

---
+++ ext/DistributionIntervalSetsExt.jl
@@ -0,0 +1,12 @@
+module DistributionIntervalSetsExt
+
+using Distribution: Uniform, truncated, censored, RealInterval
+using IntervalSets
+
+Uniform(i::Interval) = Uniform(leftendpoint(i), rightendpoint(i))
+truncated(d0, i::Interval) = truncated(d, leftendpoint(i), rightendpoint(i))
+censored(d0, i::Interval) = censored(d, leftendpoint(i), rightendpoint(i))
+
+Base.convert(::Type{T}, ri::RealInterval) where {T<:Interval} = T(minimum(ri), maximum(ri))
+
+end
