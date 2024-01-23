56860a1956e04dafc0de1bb21b0713f19d535449
Ricardo M. S. Rosa <rmsrosa@gmail.com>
2024-01-23 12:54:01 +0000

    make gradlogpdf type stable... hopefully

--- src/mixtures/mixturemodel.jl
+++ src/mixtures/mixturemodel.jl
@@ -368,7 +368,8 @@ function _mixlogpdf!(r::AbstractArray, d::AbstractMixtureModel, x)
 end
 
 function _mixgradlogpdf1(d::AbstractMixtureModel, x)
-    glp = insupport(d, x) ? sum(pi * pdf(d.components[i], x) .* gradlogpdf(d.components[i], x) for (i, pi) in enumerate(probs(d)) if (!iszero(pi) && !iszero(pdf(d.components[i], x)))) / pdf(d, x) : zero(x)
+    pdfdx = pdf(d, x) 
+    glp = !iszero(pdfdx) ? sum(pi * pdf(d.components[i], x) .* gradlogpdf(d.components[i], x) for (i, pi) in enumerate(probs(d)) if (!iszero(pi) && !iszero(pdf(d.components[i], x)))) / pdfdx : pdfdx
     return glp
 end
 
