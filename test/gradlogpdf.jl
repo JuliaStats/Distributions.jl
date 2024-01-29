4d4af7e0069c7f81ed70d51049fb4b3e0c26f958
Ricardo M. S. Rosa <rmsrosa@gmail.com>
2024-01-29 13:52:48 -0300

    add tests for single distribution "mixtures"

--- test/gradlogpdf.jl
+++ test/gradlogpdf.jl
@@ -26,22 +26,36 @@ using Test
 @test isapprox(gradlogpdf(MvTDist(5., [1., 2.], [1. 0.1; 0.1 1.]), [0.7, 0.9]),
     [0.2150711513583442, 1.2111901681759383] ,atol=1.0e-8)
 
-# Test for gradlogpdf on univariate mixture distributions against centered finite-difference on logpdf
+# Test for gradlogpdf on univariate mixture distributions
 
 x = [-0.2, 0.3, 0.8, 1.0, 1.3, 10.5]
 delta = 0.0001
 
+for di in (
+    Normal(-4.5, 2.0),
+    Exponential(2.0),
+    Uniform(0.0, 1.0),
+    Beta(2.0, 3.0),
+    Beta(0.5, 0.5)
+)
+    d = MixtureModel([di], [1.0])
+    glp1 = gradlogpdf.(d, x)
+    glp2 = gradlogpdf.(di, x)
+    @info "Testing `gradlogpdf` on $d"
+    @test isapprox(glp1, glp2, atol = 0.01)
+end
+
 for d in (
-    MixtureModel([Normal(-4.5, 2.0)], [1.0]),
-    MixtureModel([Exponential(2.0)], [1.0]),
-    MixtureModel([Uniform(-1.0, 1.0)], [1.0]),
     MixtureModel([Normal(1//1, 2//1), Beta(2//1, 3//1), Exponential(3//2)], [3//10, 4//10, 3//10]),
     MixtureModel([Normal(-2.0, 3.5), Normal(-4.5, 2.0)], [0.0, 1.0]),
     MixtureModel([Beta(1.5, 3.0), Chi(5.0), Chisq(7.0)], [0.4, 0.3, 0.3]),
     MixtureModel([Exponential(2.0), Gamma(9.0, 0.5), Gumbel(3.5, 1.0), Laplace(7.0)], [0.3, 0.2, 0.4, 0.1]),
     MixtureModel([Logistic(-6.0), LogNormal(5.5), TDist(8.0), Weibull(2.0)], [0.3, 0.2, 0.4, 0.1])
 )
+
+    # finite differences don't handle when not in the interior of the support
     xs = filter(s -> all(insupport.(d, [s - delta, s, s + delta])), x)
+
     glp1 = gradlogpdf.(d, xs)
     glp2 = ( logpdf.(d, xs .+ delta) - logpdf.(d, xs .- delta) ) ./ 2delta
     @info "Testing `gradlogpdf` on $d"
