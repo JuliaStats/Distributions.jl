d72109476a6e8781a684dfcbbb6986accaaea137
Andy Dienes <andydienes@gmail.com>
2024-01-31 15:30:25 -0500

    more improvements to batch randomness

--- src/samplers/multinomial.jl
+++ src/samplers/multinomial.jl
@@ -39,31 +39,55 @@ function multinom_rand!(rng::AbstractRNG, n::Int, p::AbstractVector{<:Real},
     return x
 end
 
-struct MultinomialSampler{T<:Real} <: Sampleable{Multivariate,Discrete}
+abstract type AbstractMultinomialSampler{T<:Real} <: Sampleable{Multivariate,Discrete} end
+
+struct MultinomialSamplerBinomial{T<:Real} <: AbstractMultinomialSampler{T}
     n::Int
+    k::Int
     prob::Vector{T}
+end
+
+struct MultinomialSamplerSequential
+    n::Int
+    k::Int
     alias::AliasTable
+    scratch_idx::Vector{Int}
+    scratch_acc::Vector{Float64}
 end
 
 function MultinomialSampler(n::Int, prob::Vector{<:Real})
-    return MultinomialSampler(n, prob, AliasTable(prob))
-end
+    k = length(prob)
 
-function _rand!(rng::AbstractRNG, s::MultinomialSampler,
-                x::AbstractVector{<:Real})
-    n = s.n
-    k = length(s)
-    if n^2 > k
-        multinom_rand!(rng, n, s.prob, x)
+    # the constant λ term should be proportional to the perfomance ratio of
+    # constructing and sampling from a Binomial vs sampling from an AliasTable
+    λ = 10
+    if n > λ * k
+        MultinomialSamplerBinomial(n, k, prob)
     else
-        # Use an alias table
-        fill!(x, zero(eltype(x)))
-        a = s.alias
-        for i = 1:n
-            x[rand(rng, a)] += 1
-        end
+        MultinomialSamplerSequential(
+            n,
+            k,
+            AliasTable(prob),
+            Vector{Int}(undef, n),
+            Vector{Float64}(undef, n)
+        )
     end
-    return x
 end
 
-length(s::MultinomialSampler) = length(s.prob)
+function _rand!(rng::AbstractRNG, s::MultinomialSamplerBinomial, x::AbstractVector{<:Real})
+    multinom_rand!(rng, s.n, s.prob, x)
+end
+
+function _rand!(rng::AbstractRNG, s::MultinomialSamplerSequential, x::AbstractVector{<:Real})
+    fill!(x, zero(eltype(x)))
+    at = s.alias
+    rand!(rng, s.scratch_idx, 1:length(at.alias))
+    rand!(rng, s.scratch_acc)
+
+    @inbounds for i = 1:s.n
+        i2 = s.scratch_idx[i] % Int
+        x[ifelse(s.scratch_acc[i] < at.accept[i2], i2, at.alias[i2])] += 1
+    end
+end
+
+length(s::AbstractMultinomialSampler) = s.k
