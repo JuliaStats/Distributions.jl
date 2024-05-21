a0b1b9a73a88ccfbc1dec4b933cd4331d1bb1073
ajshephard <andrew.james.shephard@gmail.com>
2024-05-21 09:53:18 -0400

    Update src/univariate/continuous/gumbel.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/univariate/continuous/gumbel.jl
+++ src/univariate/continuous/gumbel.jl
@@ -110,5 +110,5 @@ invlogccdf(d::Gumbel, lp::Real) = xval(d, -log(-log1mexp(lp)))
 gradlogpdf(d::Gumbel, x::Real) = expm1(-zval(d, x)) / d.θ
 
 mgf(d::Gumbel, t::Real) = gamma(1 - d.θ * t) * exp(d.μ * t)
-cgf(d::Gumbel, t::Real) = loggamma(1.0 - d.θ * t) + d.μ * t
+cgf(d::Gumbel, t::Real) = loggamma(1 - d.θ * t) + d.μ * t
 cf(d::Gumbel, t::Real) = gamma(1.0 - im * d.θ * t) * exp(im * d.μ * t)
