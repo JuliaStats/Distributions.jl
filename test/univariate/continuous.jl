7cf954c3dd7a5507480a02f69b9f648eda1fd9c4
quildtide <42811940+quildtide@users.noreply.github.com>
2024-08-09 03:46:41 -0400

    Restore location of old NormalInverseGaussian tests

--- test/univariate/continuous.jl
+++ test/univariate/continuous.jl
@@ -79,6 +79,26 @@ end
     @test cdf(d, 1.0) ≈ 0.000787319 atol=1e-9
 end
 
+@testset "NormalInverseGaussian random repeatable and basic metrics" begin
+    rng = Random.MersenneTwister(42)
+    rng2 = copy(rng)
+    µ = 0.0
+    α = 1.0
+    β = 0.5
+    δ = 3.0
+    g = sqrt(α^2 - β^2)
+    d = NormalInverseGaussian(μ, α, β, δ)
+    v1 = rand(rng, d)
+    v2 = rand(rng, d)
+    v3 = rand(rng2, d)
+    @test v1 ≈ v3
+    @test v1 ≉ v2
+
+    @test mean(d) ≈ µ + β * δ / g
+    @test var(d) ≈ δ * α^2 / g^3
+    @test skewness(d) ≈ 3β/(α*sqrt(δ*g))
+end
+
 @testset "edge cases" begin
     # issue #1371: cdf should not return -0.0
     @test cdf(Rayleigh(1), 0) === 0.0
