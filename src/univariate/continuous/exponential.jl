22486bf5fbb077998f5326207bf5ba2a64dd9902
Kyurae Kim <msca8h@naver.com>
2024-08-18 01:12:12 +0100

    fix remove type specification in `rand(Exponential)`
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/univariate/continuous/exponential.jl
+++ src/univariate/continuous/exponential.jl
@@ -105,7 +105,7 @@ cf(d::Exponential, t::Real) = 1/(1 - t * im * scale(d))
 
 
 #### Sampling
-rand(rng::AbstractRNG, d::Exponential{T}) where {T} = xval(d, randexp(rng, T))
+rand(rng::AbstractRNG, d::Exponential) = xval(d, randexp(rng))
 
 
 #### Fit model
