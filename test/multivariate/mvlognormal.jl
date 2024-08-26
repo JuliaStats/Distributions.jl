2992883a2665350860c32e58e653507c9a65f002
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-08-26 17:32:46 +0530

    Relax Fill return type in mvlognormal tests

--- test/multivariate/mvlognormal.jl
+++ test/multivariate/mvlognormal.jl
@@ -19,7 +19,7 @@ function test_mvlognormal(g::MvLogNormal, n_tsamples::Int=10^6,
     @test partype(g) == Float64
     @test isa(mn, Vector{Float64})
     if g.normal.μ isa Zeros{Float64,1}
-        @test md isa Fill{Float64,1}
+        @test md isa Union{Fill{Float64,1}, Ones{Float64,1}}
     else
         @test md isa Vector{Float64}
     end
