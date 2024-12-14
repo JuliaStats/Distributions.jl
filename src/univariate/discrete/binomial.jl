9bb0947609fad570952eadfc750c0601192060f1
David Widmann <devmotion@users.noreply.github.com>
2024-12-14 10:43:36 +0100

    Update R reference

--- src/univariate/discrete/binomial.jl
+++ src/univariate/discrete/binomial.jl
@@ -73,8 +73,6 @@ function mode(d::Binomial{T}) where T<:Real
 end
 modes(d::Binomial) = Int[mode(d)]
 
-# `median` falls back to quantile-based definition
-
 function skewness(d::Binomial)
     n, p1 = params(d)
     p0 = 1 - p1
