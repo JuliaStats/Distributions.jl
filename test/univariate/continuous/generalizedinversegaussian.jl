9e8c53ac1433744a38a66178adcdc0f1702eb039
ForceBru <ForceBru@users.noreply.github.com>
2025-06-06 23:51:29 +0300

    Canonicalize tests for generalized inverse Gaussian

--- test/univariate/continuous/generalizedinversegaussian.jl
+++ test/univariate/continuous/generalizedinversegaussian.jl
@@ -1,24 +1,48 @@
 import SpecialFunctions: besselk
 
 @testset "Generalized inverse Gaussian" begin
+	GIG(μ, λ, θ) = GeneralizedInverseGaussian(Val(:Wolfram), μ, λ, θ)
 	# Derivative d/dp log(besselk(p, x))
 	dlog_besselk_dp(p::Real, x::Real, h::Real=1e-5) = (log(besselk(p + h, x)) - log(besselk(p - h, x))) / (2h)
 
 	distributions = [
-		GeneralizedInverseGaussian(3, 8, -1/2), # basic inverse Gaussian
-		GeneralizedInverseGaussian(3, 8, 1.0),
-		GeneralizedInverseGaussian(1, 1, 1.0),
-		GeneralizedInverseGaussian(1, 1, -1.0),
+		# basic inverse Gaussian
+		(
+			d=GIG(3, 8, -1/2),
+			tmean=3,
+			tvar=3.375,
+			tskew=1.83711730708738357364796305603,
+			tkurt=5.625
+		), (
+			d=GIG(3, 8, 1.0),
+			tmean=4.80482214427053451026053858645,
+			tvar=7.53538381114490814886717269870,
+			tskew=1.46944720741022099816138306944,
+			tkurt=3.41236660328772156010103826417
+		), (
+			d=GIG(1, 1, 1.0),
+			tmean=2.69948393559377234389267739977,
+			tvar=4.51072222384624734344698799307,
+			tskew=1.80431858973895795576206086081,
+			tkurt=4.99738802301388098591337070387
+		), (
+			d=GIG(1, 1, -1.0),
+			tmean=0.699483935593772343892677399771,
+			tvar=0.510722223846247343446987993073,
+			tskew=3.52208925791368986307687496050,
+			tkurt=21.7757675389062712238907921144
+		)
 	]
-	skewnesses = [1.3554030054147672479, 1.2001825583839212282, 1.8043185897389579558, 3.5220892579136898631]
-	exc_kurtoses = [3.0618621784789726227, 2.3183896418050106456, 4.9973880230138809859, 21.775767538906271224]
-	N = 10^6
-	for (d, skew_true, kurt_true) in zip(distributions, skewnesses, exc_kurtoses)
+	NSAMPLES = 10^6
+	for (d, mean_true, var_true, skew_true, kurt_true) in distributions
 		println("\ttesting $d")
+
+		@test mean(d)     ≈ mean_true
+		@test var(d)      ≈ var_true
 		@test skewness(d) ≈ skew_true
 		@test kurtosis(d) ≈ kurt_true
 
-		samples = rand(d, N)
+		samples = rand(d, NSAMPLES)
 
 		@test abs(mean(d) - mean(samples)) < 0.01
 		@test abs(std(d) - std(samples)) < 0.01
@@ -34,6 +58,6 @@ import SpecialFunctions: besselk
 		expected_loglik_sample = mean(x->logpdf(d, x), samples)
 		@test abs(expected_loglik_true - expected_loglik_sample) < 0.01
 
-		test_samples(d, N)
+		test_samples(d, NSAMPLES)
 	end
 end
