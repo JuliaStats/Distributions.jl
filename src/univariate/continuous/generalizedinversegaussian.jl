5f8c9121605b85145cac77cbfb5701b459650e03
ForceBru <ForceBru@users.noreply.github.com>
2025-06-07 21:03:37 +0300

    Convert indentation to spaces

--- src/univariate/continuous/generalizedinversegaussian.jl
+++ src/univariate/continuous/generalizedinversegaussian.jl
@@ -16,21 +16,21 @@ External links:
 * [Generalized Inverse Gaussian distribution on Wikipedia](https://en.wikipedia.org/wiki/Generalized_inverse_Gaussian_distribution).
 """
 struct GeneralizedInverseGaussian{T<:Real} <: ContinuousUnivariateDistribution
-	a::T
-	b::T
-	p::T
-	function GeneralizedInverseGaussian{T}(a::T, b::T, p::T) where T<:Real
-		new{T}(a, b, p)
-	end
+    a::T
+    b::T
+    p::T
+    function GeneralizedInverseGaussian{T}(a::T, b::T, p::T) where T<:Real
+        new{T}(a, b, p)
+    end
 end
 
 function GeneralizedInverseGaussian(a::T, b::T, p::T; check_args::Bool=true) where T<:Real
-	check_args && @check_args GeneralizedInverseGaussian (a, a > zero(a)) (b, b > zero(b))
-	GeneralizedInverseGaussian{T}(a, b, p)
+    check_args && @check_args GeneralizedInverseGaussian (a, a > zero(a)) (b, b > zero(b))
+    GeneralizedInverseGaussian{T}(a, b, p)
 end
 
 GeneralizedInverseGaussian(a::Real, b::Real, p::Real; check_args::Bool=true) =
-	GeneralizedInverseGaussian(promote(a, b, p)...; check_args)
+    GeneralizedInverseGaussian(promote(a, b, p)...; check_args)
 
 """
     GeneralizedInverseGaussian(::Val{:Wolfram}, μ::Real, λ::Real, θ::Real=-1/2)
@@ -39,11 +39,11 @@ Wolfram Language parameterization, equivalent to `InverseGamma(μ, λ)`. `μ, λ
 Obtain parameters in Wolfram parameterization like `params(the_GIG, Val(:Wolfram))`
 """
 GeneralizedInverseGaussian(::Val{:Wolfram}, μ::Real, λ::Real, θ::Real=-1/2; check_args::Bool=true) =
-	GeneralizedInverseGaussian(λ / μ^2, λ, θ; check_args)
+    GeneralizedInverseGaussian(λ / μ^2, λ, θ; check_args)
 
 params(d::GeneralizedInverseGaussian) = (d.a, d.b, d.p)
 params(d::GeneralizedInverseGaussian, ::Val{:Wolfram}) = (
-	μ=sqrt(d.b/d.a), λ=d.b, θ=d.p
+    μ=sqrt(d.b/d.a), λ=d.b, θ=d.p
 )
 partype(::GeneralizedInverseGaussian{T}) where T = T
 
@@ -52,73 +52,73 @@ maximum(::GeneralizedInverseGaussian) = Inf
 insupport(::GeneralizedInverseGaussian, x::Real) = x >= 0
 
 mode(d::GeneralizedInverseGaussian) = (
-	(d.p - 1) + sqrt((d.p - 1)^2 + d.a * d.b)
+    (d.p - 1) + sqrt((d.p - 1)^2 + d.a * d.b)
 ) / d.a
 
 mean(d::GeneralizedInverseGaussian) =
-	sqrt(d.b/d.a) * besselk(d.p+1, sqrt(d.a*d.b)) / besselk(d.p, sqrt(d.a*d.b))
+    sqrt(d.b/d.a) * besselk(d.p+1, sqrt(d.a*d.b)) / besselk(d.p, sqrt(d.a*d.b))
 
 var(d::GeneralizedInverseGaussian) = begin
-	tmp1 = sqrt(d.a * d.b)
-	tmp2 = besselk(d.p, tmp1)
-	d.b/d.a * (
-		besselk(d.p+2, tmp1) / tmp2 - (besselk(d.p+1, tmp1) / tmp2)^2
-	)
+    tmp1 = sqrt(d.a * d.b)
+    tmp2 = besselk(d.p, tmp1)
+    d.b/d.a * (
+        besselk(d.p+2, tmp1) / tmp2 - (besselk(d.p+1, tmp1) / tmp2)^2
+    )
 end
 
 # Source: Wolfram
 skewness(d::GeneralizedInverseGaussian) = begin
-	μ, λ, θ = params(d, Val(:Wolfram))
-	t0 = besselk(0 + θ, λ/μ)
-	t1 = besselk(1 + θ, λ/μ)
-	t2 = besselk(2 + θ, λ/μ)
-	t3 = besselk(3 + θ, λ/μ)
-	(
-		2 * t1^3 - 3t0 * t1 * t2 + t0^2 * t3
-	) / sqrt(
-		-t1^2 + t0 * t2
-	)^3
+    μ, λ, θ = params(d, Val(:Wolfram))
+    t0 = besselk(0 + θ, λ/μ)
+    t1 = besselk(1 + θ, λ/μ)
+    t2 = besselk(2 + θ, λ/μ)
+    t3 = besselk(3 + θ, λ/μ)
+    (
+        2 * t1^3 - 3t0 * t1 * t2 + t0^2 * t3
+    ) / sqrt(
+        -t1^2 + t0 * t2
+    )^3
 end
 
 # Source: Wolfram
 kurtosis(d::GeneralizedInverseGaussian) = begin
-	μ, λ, θ = params(d, Val(:Wolfram))
-	t0 = besselk(0 + θ, λ/μ)
-	t1 = besselk(1 + θ, λ/μ)
-	t2 = besselk(2 + θ, λ/μ)
-	t3 = besselk(3 + θ, λ/μ)
-	t4 = besselk(4 + θ, λ/μ)
-	(
-		-3 * t1^4 + 6t0 * t1^2 * t2 - 4 * t0^2 * t1 * t3 + t0^3 * t4
-	) / (
-		t1^2 - t0 * t2
-	)^2 - 3 # EXCESS kurtosis!
+    μ, λ, θ = params(d, Val(:Wolfram))
+    t0 = besselk(0 + θ, λ/μ)
+    t1 = besselk(1 + θ, λ/μ)
+    t2 = besselk(2 + θ, λ/μ)
+    t3 = besselk(3 + θ, λ/μ)
+    t4 = besselk(4 + θ, λ/μ)
+    (
+        -3 * t1^4 + 6t0 * t1^2 * t2 - 4 * t0^2 * t1 * t3 + t0^3 * t4
+    ) / (
+        t1^2 - t0 * t2
+    )^2 - 3 # EXCESS kurtosis!
 end
 
 logpdf(d::GeneralizedInverseGaussian, x::Real) =
-	if x >= 0
-		(
-			d.p / 2 * log(d.a / d.b) - log(2 * besselk(d.p, sqrt(d.a * d.b)))
-			+ (d.p - 1) * log(x) - (d.a * x + d.b / x) / 2
-		)
-	else
-		-Inf
-	end
+    if x >= 0
+        (
+            d.p / 2 * log(d.a / d.b) - log(2 * besselk(d.p, sqrt(d.a * d.b)))
+            + (d.p - 1) * log(x) - (d.a * x + d.b / x) / 2
+        )
+    else
+        -Inf
+    end
 
 cdf(d::GeneralizedInverseGaussian, x::Real) =
-	if isinf(x)
-		(x < 0) ? zero(x) : one(x)
-	elseif isnan(x)
-		typeof(x)(NaN)
-	else
-		quadgk(z -> pdf(d, z), 0, x, maxevals=1000)[1]
-	end
+    if isinf(x)
+        (x < 0) ? zero(x) : one(x)
+    elseif isnan(x)
+        typeof(x)(NaN)
+    else
+        quadgk(z -> pdf(d, z), 0, x, maxevals=1000)[1]
+    end
 
 @quantile_newton GeneralizedInverseGaussian
 
 mgf(d::GeneralizedInverseGaussian, t::Number) = begin
-	a, b, p = params(d)
-	sqrt(a / (a - 2t))^p * besselk(p, sqrt(b * (a - 2t))) / besselk(p, sqrt(a * b))
+    a, b, p = params(d)
+    sqrt(a / (a - 2t))^p * besselk(p, sqrt(b * (a - 2t))) / besselk(p, sqrt(a * b))
 end
 
 cf(d::GeneralizedInverseGaussian, t::Number) = mgf(d, 1im * t)
@@ -133,9 +133,9 @@ Sample from the generalized inverse Gaussian distribution based on [1], end of S
 1. Devroye, Luc. 2014. “Random Variate Generation for the Generalized Inverse Gaussian Distribution.” Statistics and Computing 24 (2): 239–46. https://doi.org/10.1007/s11222-012-9367-z.
 """
 rand(rng::AbstractRNG, d::GeneralizedInverseGaussian) = begin
-	# Paper says ω = sqrt(b/a), but Wolfram disagrees
-	ω = sqrt(d.a * d.b)
-	sqrt(d.b / d.a) * rand(rng, _GIG(d.p, ω))
+    # Paper says ω = sqrt(b/a), but Wolfram disagrees
+    ω = sqrt(d.a * d.b)
+    sqrt(d.b / d.a) * rand(rng, _GIG(d.p, ω))
 end
 
 # ===== Private two-parameter version =====
@@ -152,20 +152,20 @@ NOTE: the paper says (Section 1) that the second parameter of `_GIG` should be `
 1. Devroye, Luc. 2014. “Random Variate Generation for the Generalized Inverse Gaussian Distribution.” Statistics and Computing 24 (2): 239–46. https://doi.org/10.1007/s11222-012-9367-z.
 """
 struct _GIG{T1<:Real, T2<:Real} <: ContinuousUnivariateDistribution
-	λ::T1
-	ω::T2
-	function _GIG(λ::T1, ω::T2) where {T1<:Real, T2<:Real}
-		@assert ω >= 0
-		new{T1, T2}(λ, ω)
-	end
+    λ::T1
+    ω::T2
+    function _GIG(λ::T1, ω::T2) where {T1<:Real, T2<:Real}
+        @assert ω >= 0
+        new{T1, T2}(λ, ω)
+    end
 end
 
 logpdf(d::_GIG, x::Real) =
-	if x >= 0
-		-log(2 * besselk(-d.λ, d.ω)) + (d.λ - 1) * log(x) - d.ω/2 * (x + 1/x)
-	else
-		-Inf
-	end
+    if x >= 0
+        -log(2 * besselk(-d.λ, d.ω)) + (d.λ - 1) * log(x) - d.ω/2 * (x + 1/x)
+    else
+        -Inf
+    end
 
 """
     rand(rng::AbstractRNG, d::_GIG)
@@ -177,66 +177,66 @@ Sampling from the _2-parameter_ generalized inverse Gaussian distribution based 
 1. Devroye, Luc. 2014. “Random Variate Generation for the Generalized Inverse Gaussian Distribution.” Statistics and Computing 24 (2): 239–46. https://doi.org/10.1007/s11222-012-9367-z.
 """
 function rand(rng::AbstractRNG, d::_GIG)
-	λ, ω = d.λ, d.ω
-	(λ < 0) && return 1 / rand(rng, _GIG(-λ, ω))
-	
-	α = sqrt(ω^2 + λ^2) - λ
-	ψ(x) = -α * (cosh(x) - 1) - λ * (exp(x) - x - 1)
-	ψprime(x) = -α * sinh(x) - λ * (exp(x) - 1)
+    λ, ω = d.λ, d.ω
+    (λ < 0) && return 1 / rand(rng, _GIG(-λ, ω))
 
-	tmp = -ψ(1)
-	t = if 0.5 <= tmp <= 2
-		1.0
-	elseif tmp > 2
-		sqrt(2 / (α + λ))
-	else
-		log(4 / (α + 2λ))
-	end
+    α = sqrt(ω^2 + λ^2) - λ
+    ψ(x) = -α * (cosh(x) - 1) - λ * (exp(x) - x - 1)
+    ψprime(x) = -α * sinh(x) - λ * (exp(x) - 1)
 
-	tmp = -ψ(-1)
-	s = if 0.5 <= tmp <= 2
-		1.0
-	elseif tmp > 2
-		sqrt(4 / (α * cosh(1) + λ))
-	else
-		min(1/λ, log(1 + 1/α + sqrt(1 / α^2 + 2/α)))
-	end
+    tmp = -ψ(1)
+    t = if 0.5 <= tmp <= 2
+        1.0
+    elseif tmp > 2
+        sqrt(2 / (α + λ))
+    else
+        log(4 / (α + 2λ))
+    end
 
-	eta, zeta, theta, xi = -ψ(t), -ψprime(t), -ψ(-s), ψprime(-s)
-	p, r = 1/xi, 1/zeta
+    tmp = -ψ(-1)
+    s = if 0.5 <= tmp <= 2
+        1.0
+    elseif tmp > 2
+        sqrt(4 / (α * cosh(1) + λ))
+    else
+        min(1/λ, log(1 + 1/α + sqrt(1 / α^2 + 2/α)))
+    end
 
-	t_ = t - r * eta
-	s_ = s - p * theta
-	q = t_ + s_
+    eta, zeta, theta, xi = -ψ(t), -ψprime(t), -ψ(-s), ψprime(-s)
+    p, r = 1/xi, 1/zeta
 
-	chi(x) = if -s_ <= x <= t_
-		1.0
-	elseif x < -s_
-		exp(-theta + xi * (x + s))
-	else # x > t_
-		exp(-eta - zeta * (x - t))
-	end
+    t_ = t - r * eta
+    s_ = s - p * theta
+    q = t_ + s_
 
-	# Generation
-	U, V, W = rand(rng), rand(rng), rand(rng)
-	X = if U < q / (p + q + r)
-		-s_ + q * V
-	elseif U < (q + r) / (p + q + r)
-		t_ - r * log(V)
-	else
-		-s_ + p * log(V)
-	end
-	while W * chi(X) > exp(ψ(X))
-		U, V, W = rand(rng), rand(rng), rand(rng)
-		X = if U < q / (p + q + r)
-			-s_ + q * V
-		elseif U < (q + r) / (p + q + r)
-			t_ - r * log(V)
-		else
-			-s_ + p * log(V)
-		end
-	end
+    chi(x) = if -s_ <= x <= t_
+        1.0
+    elseif x < -s_
+        exp(-theta + xi * (x + s))
+    else # x > t_
+        exp(-eta - zeta * (x - t))
+    end
 
-	tmp = λ/ω
-	(tmp + sqrt(1 + tmp^2)) * exp(X)
+    # Generation
+    U, V, W = rand(rng), rand(rng), rand(rng)
+    X = if U < q / (p + q + r)
+        -s_ + q * V
+    elseif U < (q + r) / (p + q + r)
+        t_ - r * log(V)
+    else
+        -s_ + p * log(V)
+    end
+    while W * chi(X) > exp(ψ(X))
+        U, V, W = rand(rng), rand(rng), rand(rng)
+        X = if U < q / (p + q + r)
+            -s_ + q * V
+        elseif U < (q + r) / (p + q + r)
+            t_ - r * log(V)
+        else
+            -s_ + p * log(V)
+        end
+    end
+
+    tmp = λ/ω
+    (tmp + sqrt(1 + tmp^2)) * exp(X)
 end
--- test/univariate/continuous/generalizedinversegaussian.jl
+++ test/univariate/continuous/generalizedinversegaussian.jl
@@ -1,69 +1,69 @@
 import SpecialFunctions: besselk
 
 @testset "Generalized inverse Gaussian" begin
-	GIG(μ, λ, θ) = GeneralizedInverseGaussian(Val(:Wolfram), μ, λ, θ)
-	# Empirical characteristic function
+    GIG(μ, λ, θ) = GeneralizedInverseGaussian(Val(:Wolfram), μ, λ, θ)
+    # Empirical characteristic function
     cf_empirical(samples::AbstractVector{<:Real}, t::Real) = mean(x->exp(1im * t * x), samples)
-	# Derivative d/dp log(besselk(p, x))
-	dlog_besselk_dp(p::Real, x::Real, h::Real=1e-5) = (log(besselk(p + h, x)) - log(besselk(p - h, x))) / (2h)
+    # Derivative d/dp log(besselk(p, x))
+    dlog_besselk_dp(p::Real, x::Real, h::Real=1e-5) = (log(besselk(p + h, x)) - log(besselk(p - h, x))) / (2h)
 
-	distributions = [
-		# basic inverse Gaussian
-		(
-			d=GIG(3, 8, -1/2),
-			tmean=3,
-			tvar=3.375,
-			tskew=1.83711730708738357364796305603,
-			tkurt=5.625
-		), (
-			d=GIG(3, 8, 1.0),
-			tmean=4.80482214427053451026053858645,
-			tvar=7.53538381114490814886717269870,
-			tskew=1.46944720741022099816138306944,
-			tkurt=3.41236660328772156010103826417
-		), (
-			d=GIG(1, 1, 1.0),
-			tmean=2.69948393559377234389267739977,
-			tvar=4.51072222384624734344698799307,
-			tskew=1.80431858973895795576206086081,
-			tkurt=4.99738802301388098591337070387
-		), (
-			d=GIG(1, 1, -1.0),
-			tmean=0.699483935593772343892677399771,
-			tvar=0.510722223846247343446987993073,
-			tskew=3.52208925791368986307687496050,
-			tkurt=21.7757675389062712238907921144
-		)
-	]
-	NSAMPLES = 10^6
-	for (d, mean_true, var_true, skew_true, kurt_true) in distributions
-		println("\ttesting $d")
+    distributions = [
+        # basic inverse Gaussian
+        (
+            d=GIG(3, 8, -1/2),
+            tmean=3,
+            tvar=3.375,
+            tskew=1.83711730708738357364796305603,
+            tkurt=5.625
+        ), (
+            d=GIG(3, 8, 1.0),
+            tmean=4.80482214427053451026053858645,
+            tvar=7.53538381114490814886717269870,
+            tskew=1.46944720741022099816138306944,
+            tkurt=3.41236660328772156010103826417
+        ), (
+            d=GIG(1, 1, 1.0),
+            tmean=2.69948393559377234389267739977,
+            tvar=4.51072222384624734344698799307,
+            tskew=1.80431858973895795576206086081,
+            tkurt=4.99738802301388098591337070387
+        ), (
+            d=GIG(1, 1, -1.0),
+            tmean=0.699483935593772343892677399771,
+            tvar=0.510722223846247343446987993073,
+            tskew=3.52208925791368986307687496050,
+            tkurt=21.7757675389062712238907921144
+        )
+    ]
+    NSAMPLES = 10^6
+    for (d, mean_true, var_true, skew_true, kurt_true) in distributions
+        println("\ttesting $d")
 
-		@test collect(params(d)) ≈ [d.a, d.b, d.p]
+        @test collect(params(d)) ≈ [d.a, d.b, d.p]
 
-		@test mean(d)     ≈ mean_true
-		@test var(d)      ≈ var_true
-		@test skewness(d) ≈ skew_true
-		@test kurtosis(d) ≈ kurt_true
+        @test mean(d)     ≈ mean_true
+        @test var(d)      ≈ var_true
+        @test skewness(d) ≈ skew_true
+        @test kurtosis(d) ≈ kurt_true
 
-		samples = rand(d, NSAMPLES)
+        samples = rand(d, NSAMPLES)
 
-		@test abs(mean(d) - mean(samples)) < 0.01
-		@test abs(std(d) - std(samples)) < 0.01
+        @test abs(mean(d) - mean(samples)) < 0.01
+        @test abs(std(d) - std(samples)) < 0.01
 
-		@test maximum(t->abs(cf(d, t) - cf_empirical(samples, t)), range(-50, 50, 100)) < 0.005
+        @test maximum(t->abs(cf(d, t) - cf_empirical(samples, t)), range(-50, 50, 100)) < 0.005
 
-		a, b, p = params(d)
-		t = sqrt(a * b)
-		# E[log p(x; a,b,p)]
-		expected_loglik_true = (
-			p/2 * log(a/b) - log(2besselk(p, t))
-			+ (p-1) * (0.5 * log(b/a) + dlog_besselk_dp(p, t))
-			- (t * besselk(p+1, t)/besselk(p, t) - p)
-		)
-		expected_loglik_sample = mean(x->logpdf(d, x), samples)
-		@test abs(expected_loglik_true - expected_loglik_sample) < 0.01
+        a, b, p = params(d)
+        t = sqrt(a * b)
+        # E[log p(x; a,b,p)]
+        expected_loglik_true = (
+            p/2 * log(a/b) - log(2besselk(p, t))
+            + (p-1) * (0.5 * log(b/a) + dlog_besselk_dp(p, t))
+            - (t * besselk(p+1, t)/besselk(p, t) - p)
+        )
+        expected_loglik_sample = mean(x->logpdf(d, x), samples)
+        @test abs(expected_loglik_true - expected_loglik_sample) < 0.01
 
-		test_samples(d, NSAMPLES)
-	end
+        test_samples(d, NSAMPLES)
+    end
 end
