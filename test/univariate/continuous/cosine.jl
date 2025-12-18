6c2e5f9f5af4ee27d6093504a92c6d4ac6f796ca
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-12-19 00:59:07 +0100

    new tests

--- test/univariate/continuous/cosine.jl
+++ test/univariate/continuous/cosine.jl
@@ -2,10 +2,11 @@
     @testset "mgf, cgf, cf" begin
         @test cf(Cosine(1, 1), 1) ≈ cis(1) * sin(1)/(1 - π^-2)
         @test mgf(Cosine(-1, 2), -1) ≈ exp(1) * sinh(-2) / (-2*(1 + 4π^-2))
-        @test cgf(Cosine(-1, 2), 2) ≈ -2 + log(sinh(4)) - log(4) - log(1 + 16π^-2)
+        test_cgf(Cosine(-1, 2), [-1,2, 4])
     end
 
     @testset "affine transformations" begin
+        test_affine_transformations(Cosine, (4, 2))
         @test -Cosine(1, 1) == Cosine(-1, 1)
         @test 3Cosine(1, 2) == Cosine(3, 6)
         @test Cosine(0, 2) + 42 == Cosine(42, 2)
