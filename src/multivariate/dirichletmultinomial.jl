69cb9392453532f8910ed55042a05b5bb77043ac
Tamas K. Papp <tkpapp@gmail.com>
2026-01-31 00:34:06 +0100

    Allow <:AbstractArray{<:Real} for DirichletMultinomial. (#2023)
    * Allow <:AbstractArray{<:Real} for DirichletMultinomial.
    
    Tested with StaticArrays.SVector.
    
    * add back the constructor with explicit T
    
    * Update src/multivariate/dirichletmultinomial.jl
    
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>
    
    * Update src/multivariate/dirichletmultinomial.jl
    
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>

--- src/multivariate/dirichletmultinomial.jl
+++ src/multivariate/dirichletmultinomial.jl
@@ -2,7 +2,7 @@
     DirichletMultinomial
 
 The [Dirichlet-multinomial distribution](https://en.wikipedia.org/wiki/Dirichlet-multinomial_distribution)
-is the distribution of a draw from a multinomial distribution where each sample has a 
+is the distribution of a draw from a multinomial distribution where each sample has a
 slightly different probability vector, drawn from a common Dirichlet distribution.
 
 This contrasts with the multinomial distribution, which assumes that all observations arise
@@ -29,25 +29,26 @@ vector α.
 DirichletMultinomial(n, k) # Dirichlet-multinomial distribution with n trials and parameter
 vector of length k of ones.
 ```
+
+`α` can be any `AbstractVector{<:Real}`.
 """
-struct DirichletMultinomial{T <: Real} <: DiscreteMultivariateDistribution
+struct DirichletMultinomial{T <: Real,V <: AbstractVector{T}} <: DiscreteMultivariateDistribution
     n::Int
-    α::Vector{T}
+    α::V
     α0::T
 
-    function DirichletMultinomial{T}(n::Integer, α::Vector{T}) where T
+    function DirichletMultinomial{T}(n::Integer, α::V) where {T <: Real, V <: AbstractVector{T}}
         α0 = sum(abs, α)
         sum(α) == α0 || throw(ArgumentError("alpha must be a positive vector."))
         n > 0 || throw(ArgumentError("n must be a positive integer."))
-        new{T}(Int(n), α, α0)
+        new{T,V}(Int(n), α, α0)
     end
 end
-DirichletMultinomial(n::Integer, α::Vector{T}) where {T <: Real} = DirichletMultinomial{T}(n, α)
-DirichletMultinomial(n::Integer, α::Vector{T}) where {T <: Integer} = DirichletMultinomial(n, float(α))
+DirichletMultinomial(n::Integer, α::AbstractVector{T}) where {T <: Real} = DirichletMultinomial{T}(n, α)
+DirichletMultinomial(n::Integer, α::AbstractVector{T}) where {T <: Integer} = DirichletMultinomial(n, float(α))
 DirichletMultinomial(n::Integer, k::Integer) = DirichletMultinomial(n, ones(k))
 
 Base.show(io::IO, d::DirichletMultinomial) = show(io, d, (:n, :α,))
-
 
 # Parameters
 ncategories(d::DirichletMultinomial) = length(d.α)
