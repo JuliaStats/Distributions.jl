15ab101cdddc4f49415614662562848f17fc8885
oscarddssmith <oscar.smith@juliahub.com>
2026-02-04 10:35:53 -0500

    test NoncentralT

--- test/univariates.jl
+++ test/univariates.jl
@@ -144,8 +144,6 @@ function verify_and_test(D::Union{Type,Function}, d::UnivariateDistribution, dct
     # generic testing
     if isa(d, Cosine)
         n_tsamples = floor(Int, n_tsamples / 10)
-    elseif isa(d, NoncentralT)
-        n_tsamples = min(n_tsamples, 100)
     end
 
     if !isa(d, Union{Skellam,
