da420cde4fc0df074fa8c54b36bad70c46903a0c
João C. Rodrigues Jr. <joao@qdc.ai>
2026-04-06 17:19:07 -0300

    Implementing Hypergeometric sampling to remove RFunctions dependency (#1994)
    * implementing H2PE/HIN sampling algorithm
    
    * moving logic to a sampler
    
    * using sampler() method
    
    * removing unecessary constructor for sampler
    
    * adding test to cover all branches in H.G. sampler
    
    * excluding Hypergeometric from logpdf(d, Dual(... test
    
    * adding test covering Hypergeometric modes
    
    * using minmax function instead of if block
    
    Co-authored-by: Oscar Smith <oscardssmith@gmail.com>
    
    * using min function instead of if block
    
    Co-authored-by: Oscar Smith <oscardssmith@gmail.com>
    
    * using fld(x, y) instead of floor(Int, x / y)
    
    Co-authored-by: Oscar Smith <oscardssmith@gmail.com>
    
    * Update src/samplers/hypergeometric.jl
    
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>
    
    * notation & typing adjustments
    
    * fixing type bug
    
    * optimizations on H2PE acceptance criteria by @oscardssmidth
    
    * removing 2 fields from the struct
    
    * reverting unrelated changes in distribution file
    
    * reverting unrelated changes on the distribution file
    
    * removing Rmath dependency
    
    * Update src/samplers/hypergeometric.jl
    
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>
    
    * simplify y,p assignments
    
    * changing broadcasts to mapreduce
    
    * reverting logbeta simplification
    
    * fixing name issues
    
    * correcting sign error
    
    * addapting DRSTE for new coefs
    
    * exchanging mapreduce for sum(map()) for performance
    
    * x⁴ -> (x²)² optimization
    
    * Revert "excluding Hypergeometric from logpdf(d, Dual(... test"
    
    This reverts commit f23d2c67715a94f4c297bd37db2dd3c1a781b97a.
    
    * removing hypergeometric from Dual logpdf test
    
    * correcting test exclusion expression
    
    * Revert "adding test covering Hypergeometric modes"
    
    This reverts commit c1e848c76e26d4e6a61e371a9d21af16dbf259d4.
    
    ---------
    
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>
    Co-authored-by: Oscar Smith <oscardssmith@gmail.com>

--- src/samplers.jl
+++ src/samplers.jl
@@ -14,19 +14,20 @@ Samples from the sampler and returns the result.
 """
 rand(::AbstractRNG, ::Sampleable)
 
-for fname in ["aliastable.jl",
-              "binomial.jl",
-              "poissonbinomial.jl",
-              "poisson.jl",
-              "exponential.jl",
-              "gamma.jl",
-              "multinomial.jl",
-              "vonmises.jl",
-              "vonmisesfisher.jl",
-              "discretenonparametric.jl",
-              "categorical.jl",
-              "productnamedtuple.jl",
-             ]
-
+for fname in [
+    "aliastable.jl"
+    "binomial.jl"
+    "poissonbinomial.jl"
+    "poisson.jl"
+    "exponential.jl"
+    "gamma.jl"
+    "multinomial.jl"
+    "vonmises.jl"
+    "vonmisesfisher.jl"
+    "discretenonparametric.jl"
+    "categorical.jl"
+    "productnamedtuple.jl"
+    "hypergeometric.jl"
+]
     include(joinpath("samplers", fname))
 end
